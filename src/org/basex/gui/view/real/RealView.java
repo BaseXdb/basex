98d78b2a72a078df1bafb737b420e90fcaf5ccb6
christian <christian@inf.uni-konstanz.de>
2008-05-28 06:52:40 +0000

    checkstyle issues fixed

--- src/org/basex/gui/view/real/RealView.java
+++ src/org/basex/gui/view/real/RealView.java
@@ -1,67 +1,44 @@
 package org.basex.gui.view.real;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Graphics;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.Vector;
-
-import org.basex.core.proc.List;
 import org.basex.data.Data;
-import org.basex.data.Nodes;
 import org.basex.gui.GUI;
 import org.basex.gui.GUIConstants;
-import org.basex.gui.layout.BaseXBack;
-import org.basex.gui.layout.BaseXBar;
-import org.basex.gui.layout.BaseXLabel;
 import org.basex.gui.layout.BaseXLayout;
-import org.basex.gui.layout.BaseXPanel;
 import org.basex.gui.view.View;
-import org.basex.gui.view.table.TableData;
-import org.basex.index.Names;
 import org.basex.query.fs.DirIterator;
-import org.basex.util.Array;
-import org.basex.util.IntList;
-import org.basex.util.Map;
 import org.basex.util.Performance;
-import org.basex.util.Set;
 import org.basex.util.Token;
-import org.basex.util.TokenIterator;
-import org.basex.util.TokenList;
 
 /**
- * This class offers a real tree view. *
+ * This class offers a real tree view.
  * @author Workgroup DBIS, University of Konstanz 2005-08, ISC License
  * @author Wolfgang Miller, Philipp Ziemer
  */
 public class RealView extends View { 
   /** Var to show the scrollbar. */
-  private BaseXBar scroll = null;
+  //private BaseXBar scroll = null;
   /** Horizontal Coords of the pointer. */
   private int pointerx;
   /** Vertical Coords of the pointer. */
   private int pointery;
-  /** Current font hight. */
+  /** Current font height. */
   private int fontHeight;
   /** Distance from the node to the border. */
   private final int margin = 3;
-  /** Arc Meassures for the box. */
+  /** Arc Measures for the box. */
   private final int arcWH = 10;
   /** Vertical Distance between node levels. */
   private final int lvdistance = 30;
-  /** Color for element node */
+  /** Color for element node. */
   private final Color elementColor = Color.BLACK;
-  /** Color for attribute node */
-  private final Color attributeColor = Color.RED;
-  /** Color for text node */
-  private final Color textColor = Color.BLUE;
-  /** Color for comment node */
-  private final Color commentColor = Color.GREEN;
+  /** Color for attribute node. */
+  //private final Color attributeColor = Color.RED;
+  /** Color for text node. */
+  //private final Color textColor = Color.BLUE;
+  /** Color for comment node. */
+  //private final Color commentColor = Color.GREEN;
   
   //
   //  /**
