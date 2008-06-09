fde7e30b03e70278d7eae2941f59a8cecd2113af
christian <christian@inf.uni-konstanz.de>
2008-06-09 06:49:22 +0000

    - Map class rewritten to handle generic value type

---
+++ src/org/basex/util/Map.java
@@ -0,0 +1,52 @@
+package org.basex.util;
+
+/**
+ * This is a simple hash map, extending the even simpler
+ * {@link Set hash set}.
+ * @param <E> generic value type
+ *
+ * @author Workgroup DBIS, University of Konstanz 2005-08, ISC License
+ * @author Christian Gruen
+ */
+public class Map<E> extends Set {
+  /** Values. */
+  private Object[] values = new Object[CAP];
+
+  /**
+   * Indexes the specified keys and values.
+   * @param key key
+   * @param val value
+   * @return index
+   */
+  public int add(final byte[] key, final E val) {
+    final int i = add(key);
+    if(i > 0) values[i] = val;
+    return i > 0 ? i : -i;
+  }
+
+  /**
+   * Returns the value for the specified key.
+   * @param key key to be found
+   * @return value or null if nothing was found
+   */
+  @SuppressWarnings("unchecked")
+  public E get(final byte[] key) {
+    return key != null ? (E) values[id(key)] : null;
+  }
+
+  /**
+   * Returns the specified value.
+   * @param p value index
+   * @return value
+   */
+  @SuppressWarnings("unchecked")
+  public E value(final int p) {
+    return (E) values[p];
+  }
+
+  @Override
+  protected void rehash() {
+    super.rehash();
+    values = Array.extend(values);
+  }
+}
