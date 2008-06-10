3e538513a6f90c94be552d881bec969339ded705
christian <christian@inf.uni-konstanz.de>
2008-06-10 10:48:45 +0000

    - test classes reorganized - XPathTest -> QueryTest: query tests extended to check XQuery implementation - Pathfinder remainders removed

---
+++ test/org/basex/test/junit/DataUpdateTestBulk.java
@@ -0,0 +1,180 @@
+package org.basex.test.storage;
+
+import org.basex.build.MemBuilder;
+import org.basex.build.xml.XMLParser;
+import org.basex.core.Prop;
+import org.basex.core.proc.Create;
+import org.basex.core.proc.Drop;
+import org.basex.core.proc.Insert;
+import org.basex.data.Data;
+import org.basex.data.DiskData;
+import org.basex.io.IO;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * This class tests the update features of the Data class.
+ *
+ * @author Workgroup DBIS, University of Konstanz 2005-07, ISC License
+ * @author Tim Petrowsky
+ */
+public final class DataUpdateTestBulk {
+  /** Test file size in nodes. */
+  private int size;
+  /** Data. */
+  private Data data;
+  /** InsertData. */
+  private Data insertData;
+  /** Test file we do updates with. */
+  private static final String TESTFILE = "etc/xml/test.xml";
+  /** Test file we do updates with. */
+  private static final String INSERTFILE = "input.xml";
+  /** Test database name. */
+  private static final String DBNAME = "DataUpdateTestBulk";
+
+  /**
+   * Delete the test-database.
+   */
+  @BeforeClass
+  public static void setUpBeforeClass() {
+    Prop.textindex = false;
+    Prop.attrindex = false;
+    Prop.chop = true;
+  }
+
+  /**
+   * Create the database.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Before
+  public void setUp() throws Exception {
+    data = Create.xml(new IO(TESTFILE), DBNAME);
+    size = data.size;
+    final IO file = new IO(INSERTFILE);
+    insertData = new MemBuilder().build(new XMLParser(file), INSERTFILE);
+  }
+
+  /**
+   * Delete the test-database.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @After
+  public void tearDown() throws Exception {
+    data.close();
+    Drop.drop(DBNAME);
+  }
+
+  /**
+   * Reload Data class.
+   * @throws java.lang.Exception in case of problems.
+   */
+  private void reload() throws Exception {
+    data.close();
+    data = new DiskData(DBNAME);
+  }
+
+  /**
+   * Test byte-arrays for equality.
+   * @param expected expected value
+   * @param actual actual value
+   */
+  private void assertByteArraysEqual(
+      final byte[] expected,
+      final byte[] actual) {
+    assertEquals("array lengths don't equal", expected.length, actual.length);
+    for(int i = 0; i < expected.length; i++) {
+      assertEquals(expected[i], actual[i]);
+    }
+  }
+
+  /**
+   * Test nodes for deep equality (including atts/children).
+   * @param data1 first node data
+   * @param pre1 first node pre
+   * @param data2 second node data
+   * @param pre2 second node pre
+   */
+  private void assertNodesDeepEqual(final Data data1, final int pre1,
+      final Data data2, final int pre2) {
+    assertEquals(data1.kind(pre1), data2.kind(pre2));
+    switch(data1.kind(pre1)) {
+      case Data.TEXT:
+        assertByteArraysEqual(data1.text(pre1), data2.text(pre2));
+        break;
+      case Data.ATTR:
+        assertByteArraysEqual(data1.attName(pre1), data2.attName(pre2));
+        assertByteArraysEqual(data1.attValue(pre1), data2.attValue(pre2));
+        break;
+      case Data.ELEM:
+        assertByteArraysEqual(data1.tag(pre1), data2.tag(pre2));
+        assertEquals(data1.size(pre1, Data.ELEM), data2.size(pre2, Data.ELEM));
+        final int siz = data1.size(pre1, Data.ELEM);
+        int pos = 1;
+        while(pos < siz) {
+          assertNodesDeepEqual(data1, pre1 + pos, data2, pre2 + pos);
+          pos += data1.size(pre1 + pos, data1.kind(pre1 + pos));
+        }
+        break;
+      default:
+        fail("Unknown nodekind!");
+    }
+  }
+
+  /**
+   * Test for correct data size.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Test
+  public void testSize() throws Exception {
+    assertEquals("Unexpected size!", size, data.size);
+    reload();
+    assertEquals("Unexpected size!", size, data.size);
+  }
+
+  /**
+   * Test bulk insertion.
+   */
+  @Test
+  public void testBulkInsertSmall() {
+    insert(6, 2, Insert.copy(insertData, 3));
+    assertEquals(size + 4, data.size);
+    assertNodesDeepEqual(insertData, 3, data, 11);
+    assertEquals(11, data.parent(13, Data.ELEM));
+  }
+
+  /**
+   * Test bulk insertion.
+   */
+  @Test
+  public void testBulkInsertLarge() {
+    insert(6, 2, Insert.copy(insertData, 5));
+    assertEquals(size + 2, data.size);
+    assertNodesDeepEqual(insertData, 5, data, 11);
+    assertEquals(21, data.parent(22, Data.ELEM));
+  }
+
+  /**
+   * Inserts a fragment to the database.
+   * @param par parent value
+   * @param pos inserting position
+   * @param tmp temporary data instance
+   */
+  private void insert(final int par, final int pos, final Data tmp) {
+    // find inserting position
+    int pre = par;
+    int k = data.kind(pre);
+    if(pos == 0) {
+      pre += data.size(pre, k);
+    } else {
+      pre += data.attSize(pre, k);
+      for(int p = 1; p < pos && data.parent(pre, k) == par;
+        pre += data.size(pre, k), p++) {
+        k = data.kind(pre);
+      }
+    }
+    data.insert(pre, par, tmp);
+  }
+}
---
+++ test/org/basex/test/junit/DataUpdateTestText.java
@@ -0,0 +1,334 @@
+package org.basex.test.storage;
+
+import static org.basex.util.Token.*;
+import static org.junit.Assert.*;
+import java.io.IOException;
+import org.basex.core.Prop;
+import org.basex.core.proc.Create;
+import org.basex.core.proc.Drop;
+import org.basex.data.Data;
+import org.basex.data.DiskData;
+import org.basex.io.IO;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * This class tests the update features of the Data class.
+ *
+ * @author Workgroup DBIS, University of Konstanz 2005-07, ISC License
+ * @author Tim Petrowsky
+ */
+public final class DataUpdateTestText {
+  /** Test file size in nodes. */
+  private int size;
+  /** Data. */
+  private Data data;
+  /** Test file we do updates with. */
+  private static final String TESTFILE = "etc/xml/test.xml";
+  /** Test database name. */
+  private static final String DBNAME = "DataUpdateTestText";
+
+  /**
+   * Delete the test-database.
+   */
+  @BeforeClass
+  public static void setUpBeforeClass() {
+    Prop.textindex = false;
+    Prop.attrindex = false;
+    Prop.chop = true;
+  }
+
+  /**
+   * Create the database.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Before
+  public void setUp() throws Exception {
+    data = Create.xml(new IO(TESTFILE), DBNAME);
+    size = data.size;
+  }
+
+  /**
+   * Delete the test-database.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @After
+  public void tearDown() throws Exception {
+    data.close();
+    Drop.drop(DBNAME);
+  }
+
+  /**
+   * Reload Data class.
+   * @throws java.lang.Exception in case of problems.
+   */
+  private void reload() throws Exception {
+    data.close();
+    data = new DiskData(DBNAME);
+  }
+
+  /**
+   * Test byte-arrays for equality.
+   * @param expected expected value
+   * @param actual actual value
+   */
+  private void assertByteArraysEqual(
+      final byte[] expected,
+      final byte[] actual) {
+    assertEquals("array lengths don't equal", expected.length, actual.length);
+    for(int i = 0; i < expected.length; i++) {
+      assertEquals(expected[i], actual[i]);
+    }
+  }
+
+  /**
+   * Test for correct data size.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Test
+  public void testSize() throws Exception {
+    assertEquals("Unexpected size!", size, data.size);
+    reload();
+    assertEquals("Unexpected size!", size, data.size);
+  }
+
+  /**
+   * Test insert as last child.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Test
+  public void testInsertTextAsOnly1() throws Exception {
+    final long nextid = data.meta.lastid;
+    insertText(3, 0, token("junit"), Data.TEXT);
+    assertEquals(size + 1, data.size);
+    assertEquals(3, data.parent(4, Data.TEXT));
+    assertEquals((int) Data.ATTR, data.kind(9));
+    assertEquals((int) Data.ELEM, data.kind(5));
+    assertEquals(1, data.parent(5, Data.ELEM));
+    assertEquals(5, data.parent(6, Data.ELEM));
+    assertEquals(nextid + 1, data.meta.lastid);
+    assertByteArraysEqual(token("junit"), data.atom(4));
+    reload();
+    assertEquals(size + 1, data.size);
+    assertEquals(3, data.parent(4, Data.TEXT));
+    assertEquals((int) Data.ATTR, data.kind(9));
+    assertEquals((int) Data.ELEM, data.kind(5));
+    assertEquals(1, data.parent(5, Data.ELEM));
+    assertEquals(5, data.parent(6, Data.ELEM));
+    assertEquals(nextid + 1, data.meta.lastid);
+    assertByteArraysEqual(token("junit"), data.atom(4));
+  }
+
+  /**
+   * Test insert as last child.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Test
+  public void testInsertTextAsOnly2() throws Exception {
+    final long nextid = data.meta.lastid;
+    insertText(3, 1, token("junit"), Data.TEXT);
+    assertEquals(size + 1, data.size);
+    assertEquals(3, data.parent(4, Data.TEXT));
+    assertEquals((int) Data.ATTR, data.kind(9));
+    assertEquals((int) Data.ELEM, data.kind(5));
+    assertEquals(1, data.parent(5, Data.ELEM));
+    assertEquals(5, data.parent(6, Data.ELEM));
+    assertEquals(nextid + 1, data.meta.lastid);
+    assertByteArraysEqual(token("junit"), data.atom(4));
+    reload();
+    assertEquals(size + 1, data.size);
+    assertEquals(3, data.parent(4, Data.TEXT));
+    assertEquals((int) Data.ATTR, data.kind(9));
+    assertEquals((int) Data.ELEM, data.kind(5));
+    assertEquals(1, data.parent(5, Data.ELEM));
+    assertEquals(5, data.parent(6, Data.ELEM));
+    assertEquals(nextid + 1, data.meta.lastid);
+    assertByteArraysEqual(token("junit"), data.atom(4));
+  }
+
+  /**
+   * Test insert as last child.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Test
+  public void testInsertTextAsOnly3() throws Exception {
+    final long nextid = data.meta.lastid;
+    insertText(3, 2, token("junit"), Data.TEXT);
+    assertEquals(size + 1, data.size);
+    assertEquals(3, data.parent(4, Data.TEXT));
+    assertEquals((int) Data.ATTR, data.kind(9));
+    assertEquals((int) Data.ELEM, data.kind(5));
+    assertEquals(1, data.parent(5, Data.ELEM));
+    assertEquals(5, data.parent(6, Data.ELEM));
+    assertEquals(nextid + 1, data.meta.lastid);
+    assertByteArraysEqual(token("junit"), data.atom(4));
+    reload();
+    assertEquals(size + 1, data.size);
+    assertEquals(3, data.parent(4, Data.TEXT));
+    assertEquals((int) Data.ATTR, data.kind(9));
+    assertEquals((int) Data.ELEM, data.kind(5));
+    assertEquals(1, data.parent(5, Data.ELEM));
+    assertEquals(5, data.parent(6, Data.ELEM));
+    assertEquals(nextid + 1, data.meta.lastid);
+    assertByteArraysEqual(token("junit"), data.atom(4));
+  }
+
+  /**
+   * Test insert as last child.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Test
+  public void testInsertTextAfterAttsAsFirst() throws Exception {
+    final long nextid = data.meta.lastid;
+    insertText(6, 1, token("junit"), Data.TEXT);
+    assertEquals(size + 1, data.size);
+    assertEquals((int) Data.TEXT, data.kind(9));
+    assertEquals(6, data.parent(9, Data.TEXT));
+    assertByteArraysEqual(token("junit"), data.atom(9));
+    assertEquals(6, data.parent(10, Data.ELEM));
+    assertEquals(4, data.parent(12, Data.ELEM));
+    assertEquals(nextid + 1, data.meta.lastid);
+    reload();
+    assertEquals(size + 1, data.size);
+    assertEquals((int) Data.TEXT, data.kind(9));
+    assertEquals(6, data.parent(9, Data.TEXT));
+    assertByteArraysEqual(token("junit"), data.atom(9));
+    assertEquals(6, data.parent(10, Data.ELEM));
+    assertEquals(4, data.parent(12, Data.ELEM));
+    assertEquals(nextid + 1, data.meta.lastid);
+  }
+
+  /**
+   * Test insert as last child.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Test
+  public void testInsertTextAfterAttsAsSecond() throws Exception {
+    final long nextid = data.meta.lastid;
+    insertText(6, 2, token("junit"), Data.TEXT);
+    assertEquals(size + 1, data.size);
+    assertEquals((int) Data.ELEM, data.kind(9));
+    assertByteArraysEqual(token("junit"), data.atom(11));
+    assertEquals(6, data.parent(11, Data.TEXT));
+    assertEquals(6, data.parent(9, Data.ELEM));
+    assertEquals(4, data.parent(12, Data.ELEM));
+    assertEquals(nextid + 1, data.meta.lastid);
+
+    reload();
+    assertEquals(size + 1, data.size);
+    assertEquals((int) Data.ELEM, data.kind(9));
+    assertByteArraysEqual(token("junit"), data.atom(11));
+    assertEquals(6, data.parent(11, Data.TEXT));
+    assertEquals(6, data.parent(9, Data.ELEM));
+    assertEquals(4, data.parent(12, Data.ELEM));
+    assertEquals(nextid + 1, data.meta.lastid);
+  }
+
+  /**
+   * Test insert as last child.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Test
+  public void testInsertTextAfterAttsAsLast() throws Exception {
+    final long nextid = data.meta.lastid;
+    insertText(6, 0, token("junit"), Data.TEXT);
+    assertEquals(size + 1, data.size);
+    assertEquals((int) Data.ELEM, data.kind(9));
+    assertByteArraysEqual(token("junit"), data.atom(11));
+    assertEquals(6, data.parent(11, Data.TEXT));
+    assertEquals(6, data.parent(9, Data.ELEM));
+    assertEquals(4, data.parent(12, Data.ELEM));
+    assertEquals(nextid + 1, data.meta.lastid);
+    reload();
+    assertEquals(size + 1, data.size);
+    assertEquals((int) Data.ELEM, data.kind(9));
+    assertByteArraysEqual(token("junit"), data.atom(11));
+    assertEquals(6, data.parent(11, Data.TEXT));
+    assertEquals(6, data.parent(9, Data.ELEM));
+    assertEquals(4, data.parent(12, Data.ELEM));
+    assertEquals(nextid + 1, data.meta.lastid);
+  }
+
+  /**
+   * Test insert text before text.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Test(expected = IOException.class)
+  public void testInsertTextBeforeText() throws Exception {
+    insertText(9, 1, token("foobar"), Data.TEXT);
+  }
+
+  /**
+   * Test insert text before text.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Test(expected = IOException.class)
+  public void testInsertTextAfterTextAsSecond() throws Exception {
+    insertText(9, 2, token("foobar"), Data.TEXT);
+  }
+
+  /**
+   * Test insert text before text.
+   * @throws java.lang.Exception in case of problems.
+   */
+  @Test(expected = IOException.class)
+  public void testInsertTextAfterTextAsLast() throws Exception {
+    insertText(9, 0, token("foobar"), Data.TEXT);
+  }
+
+  /**
+   * Test updateText.
+   * @throws Exception in case of problems
+   */
+  @Test
+  public void testUpdateText() throws Exception {
+    data.update(10, token("JUnit"));
+    assertEquals((int) Data.TEXT, data.kind(10));
+    assertByteArraysEqual(token("JUnit"), data.text(10));
+    reload();
+    assertEquals((int) Data.TEXT, data.kind(10));
+    assertByteArraysEqual(token("JUnit"), data.text(10));
+  }
+
+  /**
+   * Inserts a tag.
+   * @param par parent node
+   * @param pos inserting position
+   * @param name tag name
+   * @param kind node kind
+   * @throws IOException I/O exception
+   */
+  private void insertText(final int par, final int pos,
+      final byte[] name, final byte kind) throws IOException {
+
+    int pre = par;
+    int k = data.kind(pre);
+    if(pos == 0) {
+      pre += data.size(pre, k);
+    } else {
+      pre += data.attSize(pre, k);
+      for(int p = 1; p < pos && data.parent(pre, k) == par;
+        pre += data.size(pre, k), p++) {
+        k = data.kind(pre);
+      }
+    }
+
+    if(kind == Data.TEXT && (data.kind(pre) == Data.TEXT ||
+        data.parent(pre - 1, data.kind(pre - 1)) == par &&
+        data.kind(pre - 1) == Data.TEXT))
+      throw new IOException("May not insert TEXT before/after TEXT!");
+
+    data.insert(pre, par, name, kind);
+  }
+
+  /**
+   * Don't remove.
+   *
+   */
+  @Test
+  public void foo() {
+    return;
+  }
+}
