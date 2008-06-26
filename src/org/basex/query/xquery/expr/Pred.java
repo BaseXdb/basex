623d6d4804214bbc73d2ee4d6bb13c7b044f8865
stratman <stratman@inf.uni-konstanz.de>
2008-06-26 22:21:13 +0000

    pipelined predicate processing (processing more than one predicate is not yet correct)

--- src/org/basex/query/xquery/expr/Pred.java
+++ src/org/basex/query/xquery/expr/Pred.java
@@ -36,6 +36,69 @@ public final class Pred extends Arr {
 
   @Override
   public Iter iter(final XQContext ctx) throws XQException {
+    return new Iter() {
+
+      Iter iter = ctx.iter(root);
+
+      final Item ci = ctx.item;
+      final int cs = ctx.size;
+      final int cp = ctx.pos;
+      
+      boolean firstTime = true;
+      int predCount = 0;
+      int predCountTemp = predCount - 1;
+      int c = 0;
+      int s = 0;
+      
+      final SeqIter sb = new SeqIter();
+      Item i;
+      
+      @Override
+      public Item next() throws XQException {
+
+        // cache results to support last() function
+        if (firstTime) {
+          while((i = iter.next()) != null) sb.add(i);
+          firstTime = false;
+        }
+
+        // evaluates predicates
+        while (predCount < expr.length) {
+          // looks if it's a new predicate
+          if ((predCount - 1) == predCountTemp) {
+            predCountTemp++;
+            ctx.size = sb.size;
+            ctx.pos = 1;
+          }
+
+          while (s < sb.size) {
+            ctx.item = sb.item[s];
+            i = ctx.iter(expr[predCount]).ebv();
+            if(i.n() ? i.dbl() == ctx.pos : i.bool()) {
+              sb.item[c] = sb.item[s];
+              ctx.pos++;
+              s++;
+              return sb.item[c++];
+            }
+            ctx.pos++;
+            s++;
+          }
+          predCount++;
+          s = 0;
+          sb.size = c;
+        }
+
+        ctx.item = ci;
+        ctx.size = cs;
+        ctx.pos = cp;
+        return null;
+      }
+    };
+  } 
+  
+  /*
+  @Override
+  public Iter iter(final XQContext ctx) throws XQException {
     Iter iter = ctx.iter(root);
 
     final Item ci = ctx.item;
@@ -55,7 +118,13 @@ public final class Pred extends Arr {
       for(int s = 0; s < sb.size; s++) {
         ctx.item = sb.item[s];
         i = ctx.iter(p).ebv();
-        if(i.n() ? i.dbl() == ctx.pos : i.bool()) sb.item[c++] = sb.item[s];
+        if(i.n()) {
+          if(i.dbl() == ctx.pos) sb.item[c++] = sb.item[s];
+        } else {
+          if(i.bool()) sb.item[c++] = sb.item[s];
+        }
+//        if(i.n() ? i.dbl() == ctx.pos : i.bool()) sb.item[c++] = sb.item[s];
+
         ctx.pos++;
       }
       sb.size = c;
@@ -65,8 +134,8 @@ public final class Pred extends Arr {
     ctx.size = cs;
     ctx.pos = cp;
     return sb;
-  }
-
+  } */
+  
   @Override
   public boolean uses(final Using u) {
     switch(u) {
