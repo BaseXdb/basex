bf781d1f020f8c93ce0c0298ebb9d9025b9254bc
weiler <weiler@inf.uni-konstanz.de>
2008-07-28 12:42:10 +0000

    - XML:DB API Implementations

---
+++ src/org/basex/api/xmldb/BXResource.java
@@ -0,0 +1,53 @@
+package org.basex.api.xmldb;
+
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Resource;
+
+/**
+ * Implementation of the Resource Interface for the XMLDB:API
+ *
+ * @author Workgroup DBIS, University of Konstanz 2005-08, ISC License
+ * @author Andreas Weiler
+ */
+public class BXResource implements Resource {
+
+  /**
+   * @see org.xmldb.api.base.Resource#getContent()
+   */
+  public Object getContent() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /**
+   * @see org.xmldb.api.base.Resource#getId()
+   */
+  public String getId() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /**
+   * @see org.xmldb.api.base.Resource#getParentCollection()
+   */
+  public Collection getParentCollection() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /**
+   * @see org.xmldb.api.base.Resource#getResourceType()
+   */
+  public String getResourceType() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /**
+   * @see org.xmldb.api.base.Resource#setContent(java.lang.Object)
+   */
+  public void setContent(Object value) {
+  // TODO Auto-generated method stub
+
+  }
+}
