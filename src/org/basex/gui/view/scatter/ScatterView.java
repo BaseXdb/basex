66c3c1785cca2d9f8129d016febe750fdde072b1
kircher <kircher@inf.uni-konstanz.de>
2008-10-26 15:20:58 +0000

    minor

--- src/org/basex/gui/view/scatter/ScatterView.java
+++ src/org/basex/gui/view/scatter/ScatterView.java
@@ -13,7 +13,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
-import java.util.Arrays;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
