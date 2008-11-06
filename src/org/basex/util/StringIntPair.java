4ce7d1853efd3c9113f5d775efd8d7bf9f71d7f8
hauser <hauser@inf.uni-konstanz.de>
2008-11-06 17:49:48 +0000

    simple type needed to build a filesystem stack to accumulate filesizes to parent directory

---
+++ src/org/basex/util/StringIntPair.java
@@ -0,0 +1,66 @@
+package org.basex.util;
+
+/**
+ * Encapsulates a String and an int value.
+ * 
+ * @author joggele
+ */
+
+public class StringIntPair {
+  /** String. */
+  private String str;
+  /** Int value. */
+  private int val;
+  
+  /**
+   * Constructor.
+   * 
+   * @param s String for this Element
+   * @param v Value assigned to this String
+   */
+  public StringIntPair(final String s, final int v) {
+    super();
+    this.str = s;
+    this.val = v;
+  }
+  
+  /**
+   * Getting the String.
+   * 
+   * @return str
+   */
+  public String getStr() {
+    return str;
+  }
+  
+  /**
+   * Sets the String.
+   * @param s String to assign
+   */
+  public void setStr(final String s) {
+    this.str = s;
+  }
+  
+  /**
+   * Returns the Value.
+   * 
+   * @return val
+   */
+  public int getVal() {
+    return val;
+  }
+  
+  /**
+   * Returns the Value.
+   * 
+   * @param v Value to assign
+   */
+  public void setVal(final int v) {
+    this.val = v;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + str + "," + val + "]";  
+  }
+}
