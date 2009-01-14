65f1b36b4585f9a9b19852623ffcdf59975c8cce
miller <miller@inf.uni-konstanz.de>
2009-01-14 01:32:11 +0000

    tidied up the RV source

--- src/org/basex/gui/view/real/RealView.java
+++ src/org/basex/gui/view/real/RealView.java
@@ -1,7 +1,6 @@
 package org.basex.gui.view.real;
 
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Transparency;
 import java.awt.event.MouseEvent;
@@ -30,34 +29,8 @@ import org.basex.util.Token;
  * @author Wolfgang Miller, Philipp Ziemer
  */
 public final class RealView extends View {
-  /** Var to show the scrollbar. */
-  //private BaseXBar scroll = null;
-  /** Horizontal Coords of the pointer. */
-  private int pointerx;
-  /** Vertical Coords of the pointer. */
-  private int pointery;
   /** Current font height. */
   private int fontHeight;
-  /** Distance from the node to the border. */
-  private final int margin = 3;
-  /** Arc Measures for the box. */
-  //private final int arcWH = 10;
-  /** Vertical Distance between node levels. */
-  private final int lvdistance = 30;
-  /** Distance of the tree from the top border. */
-  private final int topdistance = 20;
-  /** Color for element node. */
-  private final Color elementColor = Color.BLACK;
-  /** Color for attribute node. */
-  private final Color attributeColor = Color.GREEN;
-  /** Color for attribute node. */
-  private final Color commentColor = Color.MAGENTA;
-  /** Color for process instruction node. */
-  private final Color piColor = Color.ORANGE;
-  /** Color for text node. */
-  private final Color textColor = Color.CYAN;
-  /** multiplier for pre/post values. */
-  private final int prePostMulti = 17;
   /** the sum of all size values of a node line. */
   private int sumNodeSizeInLine = 0;
   /** the list of all parent nodes of a node line. */
@@ -143,34 +116,21 @@ public final class RealView extends View {
     /** Initialize sizes */
     fontHeight = g.getFontMetrics().getHeight();
 
-    /** Initialize the pointer */
-    pointerx = getWidth() / 2;
-    pointery = topdistance;
-
     if(windowSizeChanged()) {
 
-      switch(3) {
-        case 1:
-          drawTree(g, 0, 0, 0, getWidth());
-          break;
-        case 2:
-          drawPrePost(g, 1, 1);
-          break;
-        case 3:
-          realImage = createImage();
-          Graphics rg = realImage.getGraphics();
-          rects = new ArrayList<ViewRect[]>();
-          Nodes curr = GUI.context.current();
-          for(int i = 0; i < curr.size; i++) {
-            temperature(curr.nodes[i], rg, i);
-          }
+      realImage = createImage();
+      Graphics rg = realImage.getGraphics();
+      rects = new ArrayList<ViewRect[]>();
+      Nodes curr = GUI.context.current();
+      for(int i = 0; i < curr.size; i++) {
+        temperature(curr.nodes[i], rg, i);
 
       }
       focus();
     }
     g.drawImage(realImage, 0, 0, getWidth(), getHeight(), this);
 
-    //highlights focused node
+    // highlights focused node
     if(focusedRect != null) {
       ViewRect r = focusedRect;
       g.drawRect(r.x, r.y, r.w, r.h);
@@ -198,7 +158,7 @@ public final class RealView extends View {
 
     }
 
-    //highlights marked nodes
+    // highlights marked nodes
     if(!rects.isEmpty() && GUI.context.marked().size > 0) {
 
       g.setColor(Color.GREEN);
@@ -233,7 +193,8 @@ public final class RealView extends View {
 
   }
 
-  /** creates a new translucent BufferedImage.
+  /**
+   * creates a new translucent BufferedImage.
    * @return new translucent BufferedImage
    */
   private BufferedImage createImage() {
@@ -277,15 +238,16 @@ public final class RealView extends View {
     else notifyHist(false);
   }
 
-  /**Finds rectangle at cursor position.
+  /**
+   * Finds rectangle at cursor position.
    * @return focused rectangle
    */
   public boolean focus() {
-    //    int level = mousePosY / (fontHeight * 2);
+    // int level = mousePosY / (fontHeight * 2);
 
     if(rects == null) return false;
 
-    //    final Data data = GUI.context.data();
+    // final Data data = GUI.context.data();
     final Iterator<ViewRect[]> it = rects.iterator();
 
     while(it.hasNext()) {
@@ -311,8 +273,8 @@ public final class RealView extends View {
    * @param g the graphics reference
    * @param rootNum number of current root
    */
-  private void temperature(final int root, 
-      final Graphics g, final int rootNum) {
+  private void temperature(final int root, final Graphics g, 
+      final int rootNum) {
     final Data data = GUI.context.data();
     int level = 0;
     sumNodeSizeInLine = data.meta.size;
@@ -335,7 +297,7 @@ public final class RealView extends View {
     final int l = parentList.size;
     final IntList temp = new IntList();
     int sumNodeSize = 0;
-    int rCount = 0; //counts nodes
+    int rCount = 0; // counts nodes
 
     for(int i = 0; i < l; i++) {
       final int p = parentList.list[i];
@@ -352,10 +314,10 @@ public final class RealView extends View {
 
       while(iterator.more()) {
         final int pre = iterator.next();
-        //        if(data.kind(pre) == Data.ELEM) {
+        // if(data.kind(pre) == Data.ELEM) {
         temp.add(pre);
         ++rCount;
-        //        }
+        // }
         sumNodeSize += data.size(pre, data.kind(pre));
       }
     }
@@ -401,10 +363,10 @@ public final class RealView extends View {
       final double nodePercent = nodeSize / (double) sumNodeSizeInLine;
       g.setColor(Color.black);
 
-      final int l = Math.max(2, (int) ratio); //rectangle length
-      final int h = fontHeight; //rectangle height
+      final int l = Math.max(2, (int) ratio); // rectangle length
+      final int h = fontHeight; // rectangle height
 
-      //g.drawRect((int) x, y, l, h);
+      // g.drawRect((int) x, y, l, h);
       tRect[r++] = new ViewRect((int) x, y, l, h, pre, 0);
 
       int c = (int) Math.rint(255 * nodePercent * 40);
@@ -457,12 +419,12 @@ public final class RealView extends View {
         g.drawLine((int) boxMiddle, y - 1, (int) parentMiddle, y - fontHeight
             + 1);
 
-        //        final int line = Math.round(fontHeight / 4f);
-        //        g.drawLine((int) boxMiddle, y, (int) boxMiddle, y - line);
-        //        g.drawLine((int) boxMiddle, y - line, 
-        //        (int) parentMiddle, y - line);
-        //        g.drawLine((int) parentMiddle, y - line, (int) parentMiddle, y
-        //            - fontHeight);
+        // final int line = Math.round(fontHeight / 4f);
+        // g.drawLine((int) boxMiddle, y, (int) boxMiddle, y - line);
+        // g.drawLine((int) boxMiddle, y - line,
+        // (int) parentMiddle, y - line);
+        // g.drawLine((int) parentMiddle, y - line, (int) parentMiddle, y
+        // - fontHeight);
 
       }
 
@@ -474,7 +436,8 @@ public final class RealView extends View {
     parentPos = temp;
   }
 
-  /** Returns true if window-size has changed.
+  /**
+   * Returns true if window-size has changed.
    * @return window-size has changed
    */
   boolean windowSizeChanged() {
@@ -486,213 +449,4 @@ public final class RealView extends View {
     return true;
   }
 
-  /**
-   * Draws the node at the given Coordinates.
-   * @param g graphics reference
-   * @param pre pre value
-   * @param x horizontal coordinate
-   * @param y vertical coordinate
-   * @param color node-color for different types
-   */
-  private void drawNode(final Graphics g, final int pre, final int x,
-      final int y, final Color color) {
-    final Data data = GUI.context.data();
-    final String node = Token.string(data.tag(pre));
-    final int textWidth = BaseXLayout.width(g, node);
-    final int xstart = x - textWidth / 2;
-
-    g.setColor(color);
-    /** Defines the start of the painting */
-
-    /** Draw String and Box */
-    g.drawString(node, xstart, y);
-    g.drawRect(xstart - margin, y - fontHeight, textWidth + 2 * margin,
-        fontHeight + margin);
-
-    //    g.drawRoundRect(xstart - margin, y - fontHeight, textWidth
-    //    + 2 * margin,
-    //        fontHeight + margin, arcWH, arcWH);
-  }
-
-  /**
-   * Draws the Tree.
-   * @param g graphics reference
-   * @param root pre value of the current virtual root
-   * @param lv current level
-   * @param frameleft left border of the parent frame
-   * @param space width of the parent frame
-   */
-  void drawTree(final Graphics g, final int root, final int lv,
-      final int frameleft, final int space) {
-    final Data data = GUI.context.data();
-    final ChildIterator iterator = new ChildIterator(data, root);
-
-    final int level = lv + 1;
-    final int nodetype = Data.ELEM;
-    int rootsize = data.size(root, nodetype);
-    int pre;
-    /** The x-value of the left frame border. */
-    int border = frameleft;
-    /** To engage inaccuracies during the measure method, this measures
-     * the size that is left after a child got it's space. */
-    int sizeleft = space;
-    int childframewidth = -1;
-
-    while(iterator.more()) {
-      pre = iterator.next();
-
-      if(data.kind(pre) == nodetype) {
-        /** Measures the required space by the size value. */
-        final double percent = (double) (data.size(pre, nodetype) + 1)
-            / (double) rootsize;
-        childframewidth = (int) (sizeleft * percent);
-
-        pointerx = border + childframewidth / 2;
-        pointery = topdistance + level * lvdistance;
-        drawNode(g, pre, pointerx, pointery, elementColor);
-
-        //        checkOverlap(g, pre, pointerx, pointery, elementColor);
-        drawTree(g, pre, level, border, childframewidth);
-
-        border += childframewidth;
-        rootsize = rootsize - data.size(pre, nodetype) + 1;
-        sizeleft = sizeleft - childframewidth;
-      }
-
-    }
-  }
-
-  /**
-   * Calculates the post value of a node.
-   * @param pre the pre value of the node
-   * @param level the level of the node
-   * @return calculated post value
-   */
-  private int calcPost(final int pre, final int level) {
-    final Data data = GUI.context.data();
-    return (data.size(pre, data.kind(pre)) - level) + pre;
-  }
-
-  /**
-   * Draws pre and post values next to the node they belong to.
-   * @param g graphics reference
-   * @param pre the pre value
-   * @param post the post value
-   * @param textWidth the textWidth
-   */
-  void drawPreAndPostValues(final Graphics g, final int pre, final int post,
-      final int textWidth) {
-    final int x = pre * prePostMulti;
-    final int y = post * prePostMulti;
-    final int preStrLen = BaseXLayout.width(g, Integer.toString(pre) + " ");
-    final int postStrLen = BaseXLayout.width(g, " ") + textWidth;
-    final Font temp = getFont();
-    final Font little = new Font(temp.getFontName(), Font.TRUETYPE_FONT,
-        (int) (fontHeight / 1.5f));
-    g.setFont(little);
-    g.setColor(Color.BLUE);
-    g.drawString(Integer.toString(pre), x - preStrLen, y + fontHeight / 4);
-    g.setColor(Color.RED);
-    g.drawString(Integer.toString(post), x + postStrLen, y + fontHeight / 4);
-    g.setFont(temp);
-
-  }
-
-  /**
-   * Draws the nodes for given pre and post value.
-   * @param g graphics reference
-   * @param pre the pre value for the node to draw
-   * @param post the par value for the node to draw
-   * @param level the level for the node to draw
-   */
-  private void drawPrePostNode(final Graphics g, final int pre, final int post,
-      final int level) {
-    final Data data = GUI.context.data();
-    final int x = pre * prePostMulti;
-    final int y = post * prePostMulti;
-    int preTextWidth = -1;
-    //int parTextWidth = -1;
-
-    switch(data.kind(pre)) {
-      case Data.ELEM:
-        final String s = Token.string(data.tag(pre));
-        g.drawString(s, x, y);
-        preTextWidth = BaseXLayout.width(g, s);
-        break;
-      case Data.ATTR:
-        g.setColor(attributeColor);
-        g.drawString("A", x, y);
-        preTextWidth = BaseXLayout.width(g, "A");
-        break;
-      case Data.COMM:
-        g.setColor(commentColor);
-        g.drawString("C", x, y);
-        preTextWidth = BaseXLayout.width(g, "C");
-        break;
-      case Data.PI:
-        g.setColor(piColor);
-        g.drawString("PI", x, y);
-        preTextWidth = BaseXLayout.width(g, "PI");
-        break;
-      case Data.TEXT:
-        g.setColor(textColor);
-        g.drawString("T", x, y);
-        preTextWidth = BaseXLayout.width(g, "T");
-    }
-    drawPreAndPostValues(g, pre, post, preTextWidth);
-    g.setColor(Color.BLACK);
-
-    final int par = data.parent(pre, data.kind(pre));
-
-    if(par > 0) {
-      final int parPost = calcPost(par, level - 1);
-
-      switch(data.kind(par)) {
-        case Data.ELEM:
-          //String s = Token.string(data.tag(pre));
-          //parTextWidth = BaseXLayout.width(g, s);
-          break;
-        case Data.ATTR:
-          //parTextWidth = BaseXLayout.width(g, "A");
-          break;
-        case Data.COMM:
-          //parTextWidth = BaseXLayout.width(g, "C");
-          break;
-        case Data.PI:
-          //parTextWidth = BaseXLayout.width(g, "PI");
-          break;
-        case Data.TEXT:
-          //parTextWidth = BaseXLayout.width(g, "T");
-      }
-
-      final int parX = par * prePostMulti;
-      final int parY = parPost * prePostMulti;
-      // [WM] improve line connection between nodes.
-      g.drawLine(parX, parY, x, y);
-    }
-
-  }
-
-  /**
-   * Draws pre-post-tree.
-   * @param g graphics reference
-   * @param pre the pre value of the current node
-   * @param level the level of the current node
-   */
-  private void drawPrePost(final Graphics g, final int pre, final int level) {
-    final Data data = GUI.context.data();
-    final int post = calcPost(pre, level);
-    final int lv = level;
-
-    drawPrePostNode(g, pre, post, lv);
-
-    final ChildIterator iterator = new ChildIterator(data, pre);
-
-    while(iterator.more()) {
-      final int newPre = iterator.next();
-
-      drawPrePost(g, newPre, lv + 1);
-    }
-
-  }
 }
