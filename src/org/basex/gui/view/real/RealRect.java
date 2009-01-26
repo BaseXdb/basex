66b365e4ec79996d593f1f525f823c61361d38f9
miller <miller@inf.uni-konstanz.de>
2009-01-26 16:11:56 +0000

    RealRect is back :D

---
+++ src/org/basex/gui/view/real/RealRect.java
@@ -0,0 +1,57 @@
+package org.basex.gui.view.real;
+
+import org.basex.gui.view.ViewRect;
+
+/**
+ * @author nofix
+ *
+ */
+public class RealRect extends ViewRect{
+  
+  /** mPre array with multi pre values. */
+  public int[] mPre = null;
+  /** multiPre boolean, true if multi pres exist. */
+  boolean multiPre = false;
+  
+  /** rectangle constructor, specifying additional rectangle
+   * with multi pres.
+  * @param xx x position
+  * @param yy y position
+  * @param ww width
+  * @param hh height
+  * @param pres rectangle multi pre values
+  * @param l level
+  */
+  
+  public RealRect(final int xx, final int yy, final int ww, final int hh, 
+      final int [] pres, final int l) {
+    x = xx;
+    y = yy;
+    w = ww;
+    h = hh;
+    mPre = pres;
+    level = l;
+    multiPre = true;
+  }
+  
+  
+  /** rectangle constructor, specifying additional rectangle
+  * attributes such as id, level and orientation.
+  * @param xx x position
+  * @param yy y position
+  * @param ww width
+  * @param hh height
+  * @param p rectangle pre value
+  * @param l level
+   */
+  public RealRect(final int xx, final int yy, final int ww, 
+      final int hh, final int p, final int l) {
+    x = xx;
+    y = yy;
+    w = ww;
+    h = hh;
+    pre = p;
+    level = l;
+  }
+
+}
