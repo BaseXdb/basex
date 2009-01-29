666b2f42e4ee6768489b6526504333dbb87260bd
christian <christian@inf.uni-konstanz.de>
2009-01-29 18:53:06 +0000

    BDB stuff moved to separate SVN package.

--- test/org/basex/test/examples/XQueryAPIExample.java
+++ test/org/basex/test/examples/XQueryAPIExample.java
@@ -5,7 +5,6 @@ import javax.xml.xquery.XQDataSource;
 import javax.xml.xquery.XQPreparedExpression;
 import javax.xml.xquery.XQResultSequence;
 
-
 /**
  * This class serves an example for executing XQuery requests using the XQuery API.
  * @author Workgroup DBIS, University of Konstanz 2005-08, ISC License
@@ -26,17 +25,17 @@ public final class XQueryAPIExample {
    * @throws Exception exception
    */
   public static void main(final String[] args) throws Exception {
-             //Gets the XQConnection for the specified Driver.
-         final XQConnection conn = ((XQDataSource)Class.forName(DRIVER).newInstance()).getConnection();
-             //Prepares the Expression with the Document and the Query.
-         final XQPreparedExpression expr = conn.prepareExpression(DOC + QUERY);
-             //Executes the XQuery query.
-         final XQResultSequence result = expr.executeQuery();
-             //Gets all results of the execution.
-         while (result.next()) {
-             //Prints the results to the console.
-          System.out.println(result.getItemAsString(null));
-         }
+    // Gets the XQConnection for the specified Driver.
+    final XQConnection conn = ((XQDataSource) Class.forName(DRIVER)
+        .newInstance()).getConnection();
+    // Prepares the Expression with the Document and the Query.
+    final XQPreparedExpression expr = conn.prepareExpression(DOC + QUERY);
+    // Executes the XQuery query.
+    final XQResultSequence result = expr.executeQuery();
+    // Gets all results of the execution.
+    while(result.next()) {
+      // Prints the results to the console.
+      System.out.println(result.getItemAsString(null));
     }
+  }
 }
-
