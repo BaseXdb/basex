d1de9ad1a01927a134f7d88ec4bc7d2daff39979
gath <gath@inf.uni-konstanz.de>
2009-09-25 10:00:35 +0000

    TF/IDF added for INEX Queries

--- src/org/basex/util/ScoringTokenizer.java
+++ src/org/basex/util/ScoringTokenizer.java
@@ -1,5 +1,8 @@
 package org.basex.util;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import org.basex.core.Prop;
 
 /**
@@ -13,6 +16,8 @@ public final class ScoringTokenizer extends Tokenizer{
   private IntMap token;
   /** Maximum score. */
   private int max = 1;
+  /** Container for frequency. */
+  private IntMap freq;
 
   /**
    * Empty constructor.
@@ -20,13 +25,14 @@ public final class ScoringTokenizer extends Tokenizer{
    */
   public ScoringTokenizer(final Prop pr) {
     super(Token.EMPTY, pr);
+    readFrequency();
   }
 
   @Override
   public void init(final byte[] txt) {
     super.init(txt);    
     initScoring();
-    init();
+    init();    
   }
 
   /**
@@ -37,9 +43,37 @@ public final class ScoringTokenizer extends Tokenizer{
   public int score(final byte[] key) {
     final int c = token.get(key);
     token.set(key, -1);
+    if (c > 0 && freq != null) {
+      final int f = freq.get(key);
+      if (f > 0) {
+        return (int) (Math.log(1313 / f) * c * 1000 / max);
+//        return (int) (Math.log(2666130 / f) * c * 1000 / max);
+      }
+    }
+    
     return c == 0 ? 0 : c * 1000 /  max;
   }
 
+  /**
+   * Read frequency out of an external file.
+   */
+  public void readFrequency() {
+    try {
+      final BufferedReader br = new BufferedReader(new FileReader("words.freq"));
+      String l;
+      freq = new IntMap();
+      while((l = br.readLine()) != null) {
+        final int i = l.indexOf(';');
+        freq.add(l.substring(0, i).getBytes(), 
+            Integer.valueOf(l.substring(i + 1)));
+      }
+                
+      br.close();
+    } catch (Exception e) {
+      
+    }
+  }
+  
   /**
    * Initializes the scoring process.
    */
