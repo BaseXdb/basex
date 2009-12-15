97da29803b3deac2f9eadbfd3cd99c6c2da76c3d
seiferle <seiferle@inf.uni-konstanz.de>
2009-12-15 15:38:29 +0000

    Still work in progress. Subclasses probably need to be split in one file per use-case / example category.

--- test/org/basex/test/examples/DBExample.java
+++ test/org/basex/test/examples/DBExample.java
@@ -1,11 +1,19 @@
 package org.basex.test.examples;
 
+import java.io.IOException;
+
 import org.basex.core.*;
 import org.basex.core.Commands.CmdIndex;
 import org.basex.core.proc.*;
+import org.basex.data.XMLSerializer;
+import org.basex.io.PrintOutput;
+import org.basex.query.QueryException;
+import org.basex.query.QueryProcessor;
+import org.basex.query.item.Item;
+import org.basex.query.iter.Iter;
 
 /**
- * This class demonstrates how new are created and deleted.
+ * This class demonstrates how new databases are created, deleted and queried.
  *
  * @author Workgroup DBIS, University of Konstanz 2005-09, ISC License
  * @author BaseX Team
@@ -13,7 +21,7 @@ import org.basex.core.proc.*;
 @SuppressWarnings("unused")
 public final class DBExample {
   /** The current database Context. */
-  private static Context context;
+  static final Context CONTEXT = new Context();
 
   /** Private constructor. */
   private DBExample() { }
@@ -21,47 +29,22 @@ public final class DBExample {
   /**
    * Main method of the example class.
    * @param args (ignored) command-line arguments
-   * @throws BaseXException exception
+   * @throws Exception exception
    */
-  public static void main(final String[] args) throws BaseXException {
+  public static void main(final String[] args) throws Exception {
     // Creates a new database context, referencing the database.
-    context = new Context();
+
 
     System.out.println("\n=== Create a database from a file.");
 
     // Creates a database from the specified file.
-   new CreateDB("input.xml", "Example1").exec(context, System.out);
+   new CreateDB("input.xml", "Example1").exec(CONTEXT, System.out);
     // Do some database stuff
    
-   indexExample();
-
-   queryExample();
-   System.out.println("\n=== III information on the specified " +
-       "database context.");
-   // Dumps information on the specified database context
-   new InfoDB().exec(context, System.out);
- 
-    // Closes the database.
-    new Close().exec(context, System.out);
-//    new DropDB("Example1").exec(context, System.out);
-
-//    System.out.println("\n=== Create a database from an input string.");
-//
-//    // XML string.
-//    String xml = "<xml>This is a test</xml>";
-//    // Creates a database for the specified input.
-//    new CreateDB(xml, "Example2").exec(context, System.out);
-//    // Closes the database.
-//    new Close().exec(context, System.out);
-
-//    System.out.println("\n=== Open a database and show database info:");
-//
-//    // Opens an existing database
-//    new Open("Example1").exec(context, System.out);
-//    // Dumps information on the specified database context
-//    new InfoDB().exec(context, System.out);
-//    // Closes the database.
-//    new Close().exec(context, System.out);
+   MaintenanceExamples.run();
+   
+   // Runs some example Queries
+   QueryExample.run();
     
     
 //    // You may also create Databases from a collection of files.
@@ -79,28 +62,175 @@ public final class DBExample {
 
   /**
    * Some Examples in XQuery Evaluation.
-   * @throws BaseXException 
-   * For further information on BaseX Client-Side abilities on 
+   * This class contains several variants of XQuery Processing in BaseX.
+   * For further information on BaseX Client-Side abilities in 
    * XQuery Processing please see:
    * @see XQueryExample#main(String[])
    */
-  private static void queryExample() throws BaseXException {
-    // II Evaluate XQueries for the given context
-    System.out.println("\n=== II Evaluating queries.");
+   static final class QueryExample {
+     /** XMLSerializer that generates valid XML Output. */
+    static XMLSerializer xmlSer;
 
-      new XQuery("for $x in .//body//li return $x").exec(context, System.out);
+    /**
+     * PrintOutput Context. Point the PrintOutput to whatever file
+     * you like to store the serializing results in a file.
+     * You may as well point it to System.out.
+     */
+    static PrintOutput out = new PrintOutput(System.out);
+
+    /**
+     * Runs the Example Queries and sets up the XMLSerializer.
+     * @throws Exception *TODO*
+     */
+    public static void run() throws Exception {
+      // point the serializer to System.out
+      xmlSer = new XMLSerializer(out);
+      // II Evaluate XQueries for the given context
+      System.out.println("\n=== II Evaluating queries.");
+      // Evaluate XQuery and output the result to System.out
+      System.out.println("===== XQuery Proc: direct output.");
+      directOutputExample();
+      System.out.println("\n===== XQuery Node Iteration and XML Serializing.");
+
+      iterateExample();
+      xmlSer.close();
+      out.close();
+
+    }
+
+    /**
+     * This method exexcutes an XQuery Process for the given database context.
+     * The results are automatically serialized and 
+     * printed to an arbitrary OutputStream.
+     * @throws BaseXException in case your *TODO*
+     */
+    private static void directOutputExample() throws BaseXException {
+      new XQuery("for $x in .//body//li return $x").exec(CONTEXT, System.out);
+      
+    }
+
+    /**
+     * Shows how Results can be iterated and serialized one after another
+     * using the {@link QueryProcessor} class.
+     * This is especially useful if you happen to have very big results,
+     * as you will not have to process all resulting nodes at once.
+     * Please note the use of {@link XMLSerializer} to generate 
+     * valid XML output.
+     * 
+     * @throws BaseXException in case something went wrong.
+     * @throws QueryException in case your query was wrong. 
+     */
+    private static void iterateExample() 
+      throws BaseXException, QueryException {
+      QueryProcessor qp = new QueryProcessor("for $x in .//body//li return $x",
+          CONTEXT);
+      // Returns a query iterator
+      Iter iter = qp.iter();
+      Item item;
+      try {
+        while(null != (item = iter.next())) {
+          // 
+          item.serialize(xmlSer);
+        }
+      } catch(IOException e) {
+        e.printStackTrace();
+        return;
+      }// Closes the serializer
+      // Closes the query processor
+    }
     
+    /**
+     * Use XQuery update to ....
+     * *TODO* [MSe]
+     */
+    private static void updateExample(){
+      // *TODO*
+    }
+    /** Private constructor to avoid class creation.*/
+    private QueryExample() { }
+
   }
 
   /**
-   * Some Examples on index creation.
-   * @throws BaseXException on error.
+   * Examples on Database maintenance.
+   * @author michael
+   * Topics covered: <br />
+   * Index creation & maintenance: 
+   * {@link MaintenanceExamples#createIndices} 
+   * <br /> <br />
+   * Index creation & maintenance: 
+   * {@link MaintenanceExamples#createIndices} 
    */
-  private static void indexExample() throws BaseXException {
-    // I Index Creation & Maintenance
-    new CreateIndex("fulltext").exec(context, System.out);
+  static final class MaintenanceExamples {
+    /**
+     * Runs the exmaples.
+     * @throws BaseXException on error.
+     */
+    static void run() throws BaseXException {
+      createIndices();
+      optimizeDB();
+    }
+    /**
+     * Method that shows how to invoke the optimization of the index structure. 
+     * @throws BaseXException *TODO*
+     */
+    private static void optimizeDB() throws BaseXException {
+      new Optimize().exec(CONTEXT, System.out);
+      
+    }
+    /**
+     * This method shows operations to create and drop indices.
+     * @throws BaseXException on error.
+     */
+    private static void createIndices() throws BaseXException{
+      // I Index Creation & Maintenance
+      new CreateIndex("fulltext").exec(CONTEXT, System.out);
+
+    }
     
-    // Optimize the index structure 
-    new Optimize().exec(context, System.out);
+    /**
+     * Information on the currently open database.
+     * @throws BaseXException *TODO*
+     */
+    private static void info() throws BaseXException{
+      System.out.println("\n=== III information on the specified "
+          + "database context.");
+      // Dumps information on the specified database context
+      new InfoDB().exec(CONTEXT, System.out);
+
+    }
+    /**
+     * This method shows how to create, drop and modify databases in BaseX.
+     * @throws BaseXException ex *TODO*
+     */
+    private static void creationAndDropping() throws BaseXException {
+
+      
+      // Closes the database.
+      new Close().exec(CONTEXT, System.out);
+      // new DropDB("Example1").exec(context, System.out);
+
+      // System.out.println("\n=== Create a database from an input string.");
+      //
+      // // XML string.
+      // String xml = "<xml>This is a test</xml>";
+      // // Creates a database for the specified input.
+      // new CreateDB(xml, "Example2").exec(context, System.out);
+      // // Closes the database.
+      // new Close().exec(context, System.out);
+
+      // System.out.println("\n=== Open a database and show database info:");
+      //
+      // // Opens an existing database
+      // new Open("Example1").exec(context, System.out);
+      // // Dumps information on the specified database context
+      // new InfoDB().exec(context, System.out);
+      // // Closes the database.
+      // new Close().exec(context, System.out);
+
+    }
+
+    /** Private constructor. */
+    private MaintenanceExamples() { }
   }
 }
