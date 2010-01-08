39bbf0364aeea83d0aa83de9726071210395551f
michael seiferle <michael.seiferle @uni-konstanz.de>
2010-01-08 14:56:35 +0000

    MINOR: Example methods will be added.

---
+++ test/org/basex/test/examples/AdministrationExample.java
@@ -0,0 +1,13 @@
+package org.basex.test.examples;
+
+/**
+ * This class presents methods to directly access
+ * core maintenance and administration features.
+ *
+ * @author Workgroup DBIS, University of Konstanz 2005-10, ISC License
+ * @author BaseX Team
+ * [MS] add examples.
+ */
+public class AdministrationExample {
+
+}
