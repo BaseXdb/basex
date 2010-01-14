c3341661b02851d6c57c600722c3d83db0dfed4b
sebastian gath <sebastian.gath @uni-konstanz.de>
2010-01-14 22:04:59 +0000

    Full text index (for wildcards optimized) creation process optimized - main memory usage further reduced.

--- src/org/basex/index/FTTrieBuilder.java
+++ src/org/basex/index/FTTrieBuilder.java
@@ -4,6 +4,7 @@ import static org.basex.data.DataText.*;
 import static org.basex.util.Token.*;
 
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 import org.basex.core.proc.DropDB;
 import org.basex.data.Data;
@@ -17,6 +18,7 @@ import org.basex.util.Performance;
 import org.basex.util.TokenBuilder;
 import org.basex.util.TokenList;
 
+
 /**
  * This class builds an index for text contents in a compressed trie.
  *
@@ -26,9 +28,11 @@ import org.basex.util.TokenList;
  */
 public final class FTTrieBuilder extends FTBuilder {
   /** Trie index. */
-  private final FTArray index = new FTArray(128);
+  private FTArray index = new FTArray(128);
   /** Hash structure for temporarily saving the tokens. */
   private FTHash hash = new FTHash();
+  /** Offset for joining subtrees. */
+  private int o = 0;
 
   /**
    * Constructor.
@@ -48,6 +52,8 @@ public final class FTTrieBuilder extends FTBuilder {
 
   @Override
   void index(final byte[] tok) throws IOException {
+//    merge = true;
+//    if (ntok % 6 == 0) {
     if((ntok & 0xFFF) == 0 && scm == 0 && memFull()) {
       // currently no frequency support for tfidf based scoring
       writeSortedList(csize++);
@@ -70,11 +76,15 @@ public final class FTTrieBuilder extends FTBuilder {
   
   @Override
   public void write() throws IOException {
+    if(!merge) {
+      writeCompleteTrie();
+      return;
+    }
+    
     writeSortedList(csize++);
-    if(!merge) return;
-
     final DataOutput outb = new DataOutput(data.meta.file(DATAFTX + 'b'));
     final DataOutput outt = new DataOutput(data.meta.file(DATAFTX + 't'));
+    final IntList root = new IntList();
 
     final byte[][] tok = new byte[csize][];
     final int[][] pres = new int[csize][];
@@ -107,6 +117,8 @@ public final class FTTrieBuilder extends FTBuilder {
         }
       }
       
+      if (root.size() == 0 || root.get(root.size() - 1) != tok[min][0]) 
+        root.add(tok[min][0]);
       outt.write(tok[min].length);
       outt.write(tok[min]);
 //      tokens.add(tok[min]);
@@ -134,14 +146,14 @@ public final class FTTrieBuilder extends FTBuilder {
       // write compressed pre and pos arrays
       final byte[] p = tbp.finish();
       Num.size(p, p.length);
-      final byte[] o = tbo.finish();
-      Num.size(o, o.length);
-      writeFTData(outb, p, o);
+      final byte[] op = tbo.finish();
+      Num.size(op, op.length);
+      writeFTData(outb, p, op);
     }
     outt.write(0);
     outt.close();
     outb.close();
-    writeTrie();
+    writeSplittedTrie(root);
   }
 
   /**
@@ -162,23 +174,257 @@ public final class FTTrieBuilder extends FTBuilder {
 
   /**
    * Write trie structure to disk.
+   * @param roots root nodes
    * @throws IOException
    */
-  void writeTrie() throws IOException {
+  void writeSplittedTrie(final IntList roots) throws IOException {
     DataAccess t = new DataAccess(data.meta.file(DATAFTX + 't'));
+    // save each node: l, t1, ..., tl, n1, v1, ..., nu, vu, s, p
+    // l = length of the token t1, ..., tl
+    // u = number of next nodes n1, ..., nu
+    // v1= the first byte of each token n1 points, ...
+    // s = size of pre values saved at pointer p
+    // [byte, byte[l], byte, int, byte, ..., int, long]
+    final DataOutput outN = new DataOutput(data.meta.file(DATAFTX + 'a'));
+    // ftdata is stored here, with pre1, ..., preu, pos1, ..., posu
+    // each node entries size is stored here
+    final DataOutput outS = new DataOutput(data.meta.file(DATAFTX + 'c'));
+    final int[] root = new int[roots.size()];
+    int rp = 0;
+    
+    // write tmp root node first
+    outN.write((byte) 1);
+    outN.write((byte) -1);
+    // write tmp next pointer
+    for(int j = 0; j < roots.size(); j++) {
+     outN.writeInt(0); // dummy pointer
+     outN.write(roots.get(j)); // first char of next node
+    }
+    outN.writeInt(0); // data size
+    outN.write5(-1); // pointer on data - root has no data
+    outS.writeInt(0);
+    int siz = (int) (2L + roots.size() * 5L + 9L);
     
     byte tl = t.read1();
     while (tl > 0) {
       final long pos = t.pos();
       final byte[] tok = t.readBytes(pos, pos + tl);
-      final int siz = t.read4();
-      final long off = t.read5();
-      index.insertSorted(tok, siz, off);
-//      System.out.println(new String(tok) + ":" + siz + ":" + off);
+      final int s = t.read4();
+      final long off = t.read5();      
+      if (rp < roots.size() && tok[0] != roots.get(rp)) {
+        // write subtree to disk
+        siz = writeSubTree(root, outN, outS, rp, siz);
+        rp++;
+        /*
+        final TokenList tokens = index.tokens;
+        final IntArrayList next = index.next;
+        root[rp] = next.get(0)[1] + o;
+        rp++;
+        
+        final int il = next.size();
+        for(int i = 1; i < il; i++) {
+          final int[] nxt = next.get(i);
+          // check pointer on data needs 1 or 2 ints
+          final int lp = nxt[nxt.length - 1] > -1 ? 0 : -1;
+          // write token size as byte
+          outN.write((byte) tokens.get(nxt[0]).length);
+          // write token
+          outN.write(tokens.get(nxt[0]));
+          // write next pointer
+          int j = 1;
+          for(; j < nxt.length - 2 + lp; j++) {
+            outN.writeInt(nxt[j] + o); // pointer
+            // first char of next node
+            outN.write(tokens.get(next.get(nxt[j])[0])[0]);
+          }
+          outN.writeInt(nxt[j]); // data size
+          if(nxt[j] == 0 && nxt[j + 1] == 0) {
+            // node has no data
+            outN.write5(nxt[j + 1]);
+          } else {
+            // write pointer on data
+            if(lp == 0) {
+              outN.write5(nxt[j + 1]);
+            } else {
+              outN.write5(toLong(nxt, nxt.length - 2));
+            }
+          }
+          outS.writeInt(siz);
+          siz += 1L + tokens.get(nxt[0]).length * 1L + (nxt.length - 3 + lp)
+              * 5L + 9L;
+        }      
+        
+        outS.writeInt(siz);
+        o += next.size();
+        */
+        index = new FTArray(128);
+      }
+      
+      index.insertSorted(tok, s, off);
+//      System.out.println(new String(tok) + ":" + siz + ":" + off);      
       tl = t.read1();
     }
+    
+    writeSubTree(root, outN, outS, rp, siz);
 
     t.close();
+    outN.close();
+    outS.close();
+
+    // update root node
+    final RandomAccessFile a = 
+      new RandomAccessFile(data.meta.file(DATAFTX + 'a'),"rw");
+    a.seek(2);
+    for(int i = 0; i < root.length; i++) {
+      a.writeInt(root[i]);
+      a.seek(a.getFilePointer() + 1);
+    }
+    a.close();
+//    
+//    final TokenList tokens = index.tokens;
+//    final IntArrayList next = index.next;
+//
+//    if(index.count != 1) {
+//      // index.next[i] : [p, n1, ..., s, d]
+//      // index.tokens[p], index.next[n1], ..., index.pre[d]
+//
+//      // first root node
+//      // write token size as byte
+//      outN.write((byte) 1);
+//      // write token
+//      outN.write((byte) -1);
+//      // write next pointer
+//      int j = 1;
+//      final int js = next.get(0).length - 2;
+//      for(; j < js; j++) {
+//        outN.writeInt(next.get(0)[j]); // pointer
+//        // first char of next node
+//        outN.write(tokens.get(next.get(next.get(0)[j])[0])[0]);
+//      }
+//
+//      outN.writeInt(next.get(0)[j]); // data size
+//      outN.write5(-1); // pointer on data - root has no data
+//      outS.writeInt(siz);
+//      siz += 2L + (next.get(0).length - 3) * 5L + 9L;
+//      // all other nodes
+//      final int il = next.size();
+//      for(int i = 1; i < il; i++) {
+//        final int[] nxt = next.get(i);
+//        // check pointer on data needs 1 or 2 ints
+//        final int lp = nxt[nxt.length - 1] > -1 ? 0 : -1;
+//        // write token size as byte
+//        outN.write((byte) tokens.get(nxt[0]).length);
+//        // write token
+//        outN.write(tokens.get(nxt[0]));
+//        // write next pointer
+//        j = 1;
+//        for(; j < nxt.length - 2 + lp; j++) {
+//          outN.writeInt(nxt[j]); // pointer
+//          // first char of next node
+//          outN.write(tokens.get(next.get(nxt[j])[0])[0]);
+//        }
+//        outN.writeInt(nxt[j]); // data size
+//        if(nxt[j] == 0 && nxt[j + 1] == 0) {
+//          // node has no data
+//          outN.write5(nxt[j + 1]);
+//        } else {
+//          // write pointer on data
+//          if(lp == 0) {
+//            outN.write5(nxt[j + 1]);
+//          } else {
+//            outN.write5(toLong(nxt, nxt.length - 2));
+//          }
+//        }
+//        outS.writeInt(siz);
+//        siz += 1L + tokens.get(nxt[0]).length * 1L + (nxt.length - 3 + lp)
+//            * 5L + 9L;
+//      }
+//    }
+//    outS.writeInt(siz);
+//    outN.close();
+//    outS.close();
+    
+    DropDB.delete(data.meta.name, DATAFTX + "\\d+." + IO.BASEXSUFFIX,
+    data.meta.prop);
+  }
+  
+  /**
+   * Writes subtree to disk.
+   * @param root Array with root offsets
+   * @param outN DataOutput
+   * @param outS DataOutput
+   * @param rp pointer on root offsets
+   * @param siz size
+   * @return new size
+   * @throws IOException
+   */
+  private int writeSubTree(final int[] root, final DataOutput outN, 
+      final DataOutput outS, final int rp, final int siz) throws IOException {
+    int s = siz;
+    // write subtree to disk
+    final TokenList tokens = index.tokens;
+    final IntArrayList next = index.next;
+    if (root != null) root[rp] = next.get(0)[1] + o;
+//    rp++;
+    
+    final int il = next.size();
+    for(int i = 1; i < il; i++) {
+      final int[] nxt = next.get(i);
+      // check pointer on data needs 1 or 2 ints
+      final int lp = nxt[nxt.length - 1] > -1 ? 0 : -1;
+      // write token size as byte
+      outN.write((byte) tokens.get(nxt[0]).length);
+      // write token
+      outN.write(tokens.get(nxt[0]));
+      // write next pointer
+      int j = 1;
+      for(; j < nxt.length - 2 + lp; j++) {
+        outN.writeInt(nxt[j] + o); // pointer
+        // first char of next node
+        outN.write(tokens.get(next.get(nxt[j])[0])[0]);
+      }
+      outN.writeInt(nxt[j]); // data size
+      if(nxt[j] == 0 && nxt[j + 1] == 0) {
+        // node has no data
+        outN.write5(nxt[j + 1]);
+      } else {
+        // write pointer on data
+        if(lp == 0) {
+          outN.write5(nxt[j + 1]);
+        } else {
+          outN.write5(toLong(nxt, nxt.length - 2));
+        }
+      }
+      outS.writeInt(s);
+      s += 1L + tokens.get(nxt[0]).length * 1L + 
+          (nxt.length - 3 + lp) * 5L + 9L;
+      }
+
+    outS.writeInt(s);
+    o += next.size();
+
+    return s;
+  }
+  
+  /**
+   * Write trie structure to disk.
+   * @throws IOException
+   */
+  void writeCompleteTrie() throws IOException {
+    if(scm == 0) hash.init();
+    else hash.initIter();
+    final DataOutput outb = new DataOutput(data.meta.file(DATAFTX + 'b'));
+    while(hash.more()) {
+      final int p = hash.next();
+      final byte[] tok = hash.key();
+      final int ds = hash.ns[p];
+      final long cpre = outb.size();
+      // write compressed pre and pos arrays
+      writeFTData(outb, hash.pre[p], hash.pos[p]);
+      index.insertSorted(tok, ds, cpre);
+    }
+    outb.close();
+    hash = null;
     
     final TokenList tokens = index.tokens;
     final IntArrayList next = index.next;
@@ -220,48 +466,13 @@ public final class FTTrieBuilder extends FTBuilder {
       outS.writeInt(siz);
       siz += 2L + (next.get(0).length - 3) * 5L + 9L;
       // all other nodes
-      final int il = next.size();
-      for(int i = 1; i < il; i++) {
-        final int[] nxt = next.get(i);
-        // check pointer on data needs 1 or 2 ints
-        final int lp = nxt[nxt.length - 1] > -1 ? 0 : -1;
-        // write token size as byte
-        outN.write((byte) tokens.get(nxt[0]).length);
-        // write token
-        outN.write(tokens.get(nxt[0]));
-        // write next pointer
-        j = 1;
-        for(; j < nxt.length - 2 + lp; j++) {
-          outN.writeInt(nxt[j]); // pointer
-          // first char of next node
-          outN.write(tokens.get(next.get(nxt[j])[0])[0]);
-        }
-        outN.writeInt(nxt[j]); // data size
-        if(nxt[j] == 0 && nxt[j + 1] == 0) {
-          // node has no data
-          outN.write5(nxt[j + 1]);
-        } else {
-          // write pointer on data
-          if(lp == 0) {
-            outN.write5(nxt[j + 1]);
-          } else {
-            outN.write5(toLong(nxt, nxt.length - 2));
-          }
-        }
-        outS.writeInt(siz);
-        siz += 1L + tokens.get(nxt[0]).length * 1L + (nxt.length - 3 + lp)
-            * 5L + 9L;
-      }
-    }
-    outS.writeInt(siz);
-    outN.close();
-    outS.close();
-    
+      siz = writeSubTree(null, outN, outS, 0, siz);
+      outN.close();
+      outS.close();
+    }    
     DropDB.delete(data.meta.name, DATAFTX + "\\d+." + IO.BASEXSUFFIX,
     data.meta.prop);
   }
-  
-  
 
 /*  
   void writeOld() throws IOException {
