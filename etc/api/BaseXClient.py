c28e0a523e92aee23c4fa3eac87be20517744830
andreas weiler <andreas.weiler @uni-konstanz.de>
2010-02-08 10:46:05 +0000

    Reworked Python BaseXClient 6.0

--- etc/api/BaseXClient.py
+++ etc/api/BaseXClient.py
@@ -1,83 +1,76 @@
-#!/usr/bin/python
-# Fabrice Issac
-# LDI 2009
-
-import socket
-import sys
+"""/**
+ * This is the starter class for the client console mode.
+ * It sends all commands to the server instance.
+ * Add the '-h' option to get a list on all available command-line arguments.
+ *
+ * @author Workgroup DBIS, University of Konstanz 2005-10, ISC License
+ * @author Andeas Weiler
+ */"""
+ 
+import ClientSession, getopt, sys
 
 class BaseXClient(object):
-	def __init__(self,host,port):
-		self.host = host
-		self.port = port
-		self.sesid = ""
-		self.verb = True
-		self.data = ' '
-		
-	def connect(self):
-		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.s.connect((self.host,self.port))
+    # Initializes the client.
+    def __init__(self,host,port):
+        self.host = host
+        self.port = port
+        print 'BaseX 6.01 [Client]'
+        print 'Try "help" to get some information.'           
+    
+    # Creates a session.   
+    def session(self):
+        user = raw_input('Username: ');
+        pw = raw_input('Password: ');
+        global session
+        session = ClientSession.ClientSession(self.host, self.port, user, pw)
+        return session.connect()
+    
+    # Reads commands from the console.    
+    def readCommand(self):
+        self.com = raw_input('> ')
+        if self.com == "":
+            self.readCommand()
+        return self.com
+    
+    # Runs the console.
+    def console(self):
+        if self.session() == True:
+            session.sendCommand("SET INFO ON")
+            session.s.recv(1024)
+            while self.readCommand() != "exit":
+                session.sendCommand(self.com)
+                data = session.receive()
+                print data
+            try: 
+                session.sendCommand("exit")
+            except:
+                session.close()
+            print "See you."
+        else:
+            print "Access denied."
+            session.close()
 
-	def close(self):
-		self.s.close()
-		
-	def formatCommand(self,com):
-		# integer to hex e.g. 4 -> '\x00\x04'
-		i2h = chr(len(com)>>8)+chr(len(com)%16384)
-		fcom = i2h+com
-		return fcom
-		
-	def sendCommand(self,com):
-		fcom = self.formatCommand(com)
-		if self.verb:
-			print "C=>"+repr(fcom)
-		self.connect()
-		self.s.send(fcom)
-		data = self.s.recv(1024)
-		if self.verb:
-			print "R=>"+repr(data)
-		if len(data)==4:
-			self.sesid = str(ord(data[2])*256+ord(data[3]))
-		else:
-			sys.stdout.write("Error\n")
-		self.close()
-		
-	def getResult(self,com):
-		fcom = self.formatCommand(com+" "+self.sesid)
-		if self.verb:
-			print "c=>"+repr(fcom)
-		self.connect()
-		self.s.send(fcom)
-		self.data = self.s.recv(10000000)
-		if self.verb:
-			print "R=>"+repr(self.data)
-		try:
-			self.sesid = str(ord(self.data[2])*256+ord(self.data[3]))
-		except:
-			self.data = self.data + ""
-		self.close()
-	
-	def write(self):
-		sys.stdout.write(self.data)
-		
-	def console(self):
-		print "Python BaseX client"
-		print "v0.1"
-		self.sendCommand("SET INFO ON")
-		self.getResult("INTOUTPUT")
-		self.write()	
-		self.data = ' '
-		while self.data != '':
-			sys.stdout.write('>')
-			com = sys.stdin.readline().rstrip()
-			if com != '':
-				self.sendCommand(com)
-				self.getResult("INTOUTPUT")
-				self.write()
-				self.getResult("INTINFO")
-		print "Profitez de la vie !!!"
-		
-		
+# Reads arguments -p and -h.
+def opts():
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], "-p:-h", ["port", "host"])
+        except getopt.GetoptError, err:
+            print str(err)
+            sys.exit()
+        global host
+        global port
+        host = "localhost"
+        port = 1984
+        
+        for o, a in opts:
+            if o == "-p":
+                port = int(a)
+            if o == "-h":
+                host = a
+
+# Main method.
 if __name__ == '__main__':
-	bxc = BaseXClient("localhost",1984)
-	bxc.console()
-	
+    opts()
+    bxc = BaseXClient(host,port)
+    bxc.console()
+     
---
+++ etc/api/ClientSession.py
@@ -0,0 +1,81 @@
+"""/**
+ * This wrapper sends commands to the server instance over a socket
+ * connection. It extends the {@link Session} class:
+ *
+ * <ul>
+ * <li> A socket instance is created by the constructor.</li>
+ * <li> The {@link #execute} method sends database commands to the server.
+ * All strings are encoded as UTF8 and suffixed by a zero byte.</li>
+ * <li> If the command has been successfully processed,
+ * the result string is read.</li>
+ * <li> Next, the processing info string is read.</li>
+ * <li> A last byte is next sent to indicate if command execution
+ * was successful (0) or not (1).</li>
+ * <li> {@link #close} closes the session by sending the {@link Cmd#EXIT}
+ * command to the server.</li>
+ * </ul>
+ *
+ * @author Workgroup DBIS, University of Konstanz 2005-10, ISC License
+ * @author Andreas Weiler
+ */"""
+ 
+import hashlib, socket
+ 
+class ClientSession(object):
+    
+    # Initializes the ClientSession.
+    def __init__(self,host,port,user,pw):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.pw = pw
+        
+    # Connects to the server.    
+    def connect(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((self.host,self.port)) 
+        return self.login()
+    
+    # Login of user at the server.
+    def login(self):
+        ts = self.getIt()
+        pwmd5 = hashlib.md5(self.pw).hexdigest()
+        m = hashlib.md5()
+        m.update(pwmd5)
+        m.update(ts)
+        complete = m.hexdigest()
+        self.sendCommand(self.user)
+        self.sendCommand(complete)
+        data = self.s.recv(1)
+        return "\0" == data
+    
+    # sends command to the server        
+    def sendCommand(self,com):
+        self.s.send(str.encode(com))
+        self.s.send("\0")
+    
+    # Gets the parts of the answer.    
+    def getIt(self):
+        com = ""
+        while True:
+            data = self.s.recv(1)
+            if(data == "\0"):
+                return com
+            else:
+                com += data
+                      
+    # Receives data.    
+    def receive(self):
+        part1 = self.getIt()
+        part2 = self.getIt()
+        part3 = self.s.recv(1)
+        recv = ""
+        if part1 != "\0":
+            recv += part1 + part2
+        else:
+            recv = part2
+        return recv
+    
+    # Closes the connection.       
+    def close(self):
+        self.s.close()
