0c4d9d71b9fb99e2b1f707f3c958dd6401cb2b03
andreas weiler <andreas.weiler @uni-konstanz.de>
2010-03-17 15:17:37 +0000

    C# Api Version started...to be continued soon

---
+++ etc/c#/BaseX.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * ----------------------------------------------------------------------------- 
+ * 
+ * This C# module provides methods to connect to and communicate with the
+ * BaseX Server.
+ *
+ * The Constructor of the class expects a hostname, port, username and password
+ * for the connection. The socket connection will then be established via the
+ * hostname and the port.
+ *
+ * For the execution of commands you need to call the execute() method with the
+ * database command as argument. The method returns a boolean, indicating if
+ * the command was successful. The result can be requested with the result()
+ * method, and the info() method returns additional processing information
+ * or error output.
+ *
+ * -----------------------------------------------------------------------------
+ * Example:
+ * 
+ * -----------------------------------------------------------------------------
+ * (C) Workgroup DBIS, University of Konstanz 2005-10, ISC License
+ * -----------------------------------------------------------------------------
+ */
+using System;
+using System.Net.Sockets;
+
+namespace BaseX
+{
+	class Session
+	{
+		public Session(String host, int port, String username, String pw) 
+		{
+			TcpClient socket = new TcpClient(host, port);
+		}
+	}
+}
---
+++ etc/c#/Example.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * -----------------------------------------------------------------------------
+ *
+ * This example shows how BaseX commands can be performed via the Ruby API.
+ * The execution time will be printed along with the result of the command.
+ *
+ * -----------------------------------------------------------------------------
+ * (C) Workgroup DBIS, University of Konstanz 2005-10, ISC License
+ * -----------------------------------------------------------------------------
+ */
+using System;
+
+namespace BaseX
+{
+	public class Example
+	{	
+		public static void Main(string[] args)
+		{
+			new Session("localhost", 1984, "admin", "admin");
+		}
+	}
+}
