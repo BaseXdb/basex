7f7e460265a6872fa812ca2594fb91732b8c265a
andreas weiler <andreas.weiler @uni-konstanz.de>
2010-03-21 15:19:39 +0000

    C# Api Version finished.

--- etc/c#/BaseX.cs
+++ etc/c#/BaseX.cs
@@ -17,20 +17,127 @@
  * -----------------------------------------------------------------------------
  * Example:
  * 
+ * using System;
+ * 
+ * namespace BaseX
+ *	{
+ *	public class Example
+ *	{	
+ *		public static void Main(string[] args)
+ *		{	
+ *			try {
+ *				Session session = new Session("localhost", 1984, "admin", "admin");
+ *				if ((session.execute("xquery 1 to 10")) == "\0") {
+ *					Console.WriteLine(s.res());
+ * 				} else {
+ *					Console.WriteLine(s.inf());
+ *				}
+ *				session.close();
+ *			} catch (Exception e) {
+ *				Console.WriteLine(e.Message);
+ *		  }
+ *		}
+ *	  }
+ *	} 
+ * 
  * -----------------------------------------------------------------------------
  * (C) Workgroup DBIS, University of Konstanz 2005-10, ISC License
  * -----------------------------------------------------------------------------
  */
 using System;
 using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace BaseX
 {
 	class Session
 	{
-		public Session(String host, int port, String username, String pw) 
+		public NetworkStream stream = null;
+		public TcpClient socket = null;
+		public byte[] outStream = null;
+		public byte[] inStream = null;
+		public string result = "";
+		public string info = "";
+		
+		/** Constructor, creating a new socket connection. */
+		public Session(string host, int port, string username, string pw) 
 		{
-			TcpClient socket = new TcpClient(host, port);
+			socket = new TcpClient(host, port);
+			stream = socket.GetStream();
+			string ts = readString();
+			string h = md5(pw);
+			string hts = h + ts;
+			string end = md5(hts);
+			send(username + "\0");
+			send(end + "\0");
+			if (read() != "\0") {
+			 	throw new Exception("Access denied.");
+			}
+		}
+		
+		/** Executes the specified command. */
+		public string execute(string com) {
+			send(com + "\0");
+			result = readString();
+			info = readString();
+			return read();
+		}
+		
+		/** Returns the result. */
+		public string res() {
+			return result;
+		}
+		
+		/** Returns the processing information. */
+		public string inf() {
+			return info;
+		}
+		
+		/** Closes the connection. */
+		public void close() {
+			send("exit");
+			socket.Close();
+		}
+		
+		/** Receives a single byte from the socket. */
+		private string read() {
+			inStream = new byte[1];
+			stream.Read(inStream, 0, 1);
+			string test = System.Text.Encoding.ASCII.GetString(inStream);
+            return test;
+		}
+		
+		/** Returns the received string. */
+		private string readString() {
+			string complete = "";
+			string t = "";
+			while ((t = read()) != "\0") {
+				complete += t;
+			}
+			return complete;
+		}
+		
+		/** Sends strings to server. */
+		private void send(string message) {
+			outStream = System.Text.Encoding.ASCII.GetBytes(message);
+			stream.Write(outStream, 0, outStream.Length);
+            stream.Flush();
+		}
+		
+		/** Returns the md5 hash of a string. */
+		private string md5(string input) {
+		var MD5 = new MD5CryptoServiceProvider();
+		var hashValue = new Byte[1];
+        byte[] bytes = Encoding.ASCII.GetBytes(input);
+        hashValue = MD5.ComputeHash(bytes);
+        var sb = new StringBuilder();
+
+		for (var i = 0; i <= hashValue.Length - 1; i++)
+            {
+                sb.Append(hashValue[i].ToString("x2"));
+            }
+            return sb.ToString();
 		}
 	}
 }
--- etc/c#/Example.cs
+++ etc/c#/Example.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * -----------------------------------------------------------------------------
  *
- * This example shows how BaseX commands can be performed via the Ruby API.
+ * This example shows how BaseX commands can be performed via the C# API.
  * The execution time will be printed along with the result of the command.
  *
  * -----------------------------------------------------------------------------
@@ -9,14 +9,41 @@
  * -----------------------------------------------------------------------------
  */
 using System;
+using System.Diagnostics;
 
 namespace BaseX
 {
 	public class Example
 	{	
 		public static void Main(string[] args)
-		{
-			new Session("localhost", 1984, "admin", "admin");
+		{	
+			// initialize timer
+			Stopwatch watch = new Stopwatch();
+			watch.Start();
+			
+			// command to be performed
+			string com = "xquery 1 to 10";
+			try {
+				// create session
+				Session session = new Session("localhost", 1984, "admin", "admin");
+				
+				// perform command and show result or error output
+				if ((session.execute(com)) == "\0") {
+					Console.WriteLine(session.res());
+				} else {
+					Console.WriteLine(session.inf());
+				}
+				
+				// close session
+				session.close();
+				
+				// print time needed
+				watch.Stop();
+				Console.WriteLine(watch.ElapsedMilliseconds + " ms.");
+				
+			} catch (Exception e) {
+				Console.WriteLine(e.Message);
+			}
 		}
 	}
 }
