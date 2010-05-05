71a2fb472a4f789a21f58310ed09a8d37a1ea7eb
andreas weiler <andreas.weiler @uni-konstanz.de>
2010-05-05 17:59:32 +0000

    started to implement iterate mode for clients

---
+++ etc/python/experimental/Example.py
@@ -0,0 +1,34 @@
+# -----------------------------------------------------------------------------
+#
+# This example shows how BaseX commands can be performed via the Python API.
+# The execution time will be printed along with the result of the command.
+#
+# -----------------------------------------------------------------------------
+# (C) Workgroup DBIS, University of Konstanz 2005-10, ISC License
+# -----------------------------------------------------------------------------
+
+import BaseXClient, time
+
+# initialize timer
+start = time.clock()
+
+# command to be performed
+cmd = "1 to 10";
+
+try:
+  # create session
+  session = BaseXClient.Session('localhost', 1984, 'admin', 'admin')
+  
+  query = BaseXClient.Query(session)
+  print query.run(cmd)
+
+  # close session
+  session.close()
+
+  # print time needed
+  time = (time.clock() - start) * 1000
+  print time, "ms."
+
+except IOError as e:
+  # print exception
+  print e
