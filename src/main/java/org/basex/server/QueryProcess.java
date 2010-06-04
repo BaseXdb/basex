c2e4dd41ec44bb8b015718c9a46ea800d56c13b7
andreas weiler <andreas.weiler @uni-konstanz.de>
2010-06-04 12:40:15 +0000

    timeout added to iterative mode, php updated soon

--- src/main/java/org/basex/server/QueryProcess.java
+++ src/main/java/org/basex/server/QueryProcess.java
@@ -5,25 +5,27 @@ import static org.basex.core.Text.*;
 import java.io.IOException;
 
 import org.basex.core.Context;
+import org.basex.core.Prop;
 import org.basex.data.XMLSerializer;
 import org.basex.io.PrintOutput;
 import org.basex.query.QueryException;
 import org.basex.query.QueryProcessor;
 import org.basex.query.item.Item;
 import org.basex.query.iter.Iter;
+import org.basex.util.Performance;
 
 /**
  * Container for processes executing a query with iterative results.
- *
+ * 
  * @author Workgroup DBIS, University of Konstanz 2005-10, ISC License
  * @author Andreas Weiler
  */
 public class QueryProcess {
-  
+
   /** Id. */
   public int id;
   /** Output. */
-  private PrintOutput out;
+  PrintOutput out;
   /** Processor. */
   private QueryProcessor processor;
   /** Iterator. */
@@ -36,7 +38,11 @@ public class QueryProcess {
   private boolean updating;
   /** Context. */
   private Context ctx;
-  
+  /** Timeout thread. */
+  private Thread timeout;
+  /** Flag for timeout. */
+  boolean running = true;
+
   /**
    * Constructor.
    * @param i id
@@ -49,7 +55,7 @@ public class QueryProcess {
     this.out = o;
     this.serverProc = sp;
   }
-  
+
   /**
    * Starts the query process.
    * @param s input string
@@ -62,10 +68,11 @@ public class QueryProcess {
     updating = processor.ctx.updating;
     ctx.sema.before(updating);
     try {
-    iter = processor.iter();
-    serializer = new XMLSerializer(out);
-    out.print(String.valueOf(id));
-    send(true);
+      iter = processor.iter();
+      serializer = new XMLSerializer(out);
+      out.print(String.valueOf(id));
+      send(true);
+      startTimer();
     } catch(QueryException ex) {
       // invalid command was sent by a client; create error feedback
       serverProc.log.write(this, s, INFOERROR + ex.extended());
@@ -75,7 +82,7 @@ public class QueryProcess {
       send(true);
     }
   }
-  
+
   /**
    * Returns the next item to the client.
    * @throws IOException Exception
@@ -83,20 +90,30 @@ public class QueryProcess {
   public void more() throws IOException {
     Item item;
     try {
-    if((item = iter.next()) != null) {
-      send(true);
-      item.serialize(serializer);
-      send(true);
-    } else {
-      send(false);
-      close();
-    }
+      if(!running) {
+        send(false);
+        send(true);
+        out.print(SERVERTIME);
+        send(true);
+        close();
+      } else {
+        if((item = iter.next()) != null) {
+          send(true);
+          item.serialize(serializer);
+          send(true);
+        } else {
+          send(false);
+          send(false);
+          close();
+        }
+      }
     } catch(Exception ex) {
+      send(false);
       send(false);
       close();
     }
   }
-  
+
   /**
    * Closes the query process.
    * @throws IOException Exception
@@ -105,15 +122,40 @@ public class QueryProcess {
     ctx.sema.after(updating);
     serializer.close();
     processor.close();
+    stopTimer();
     serverProc.queries.remove(this);
   }
-  
+
   /**
    * Sends the success flag to the client.
    * @param ok success flag
    * @throws IOException Exception
    */
-  private void send(final boolean ok) throws IOException {
-      serverProc.send(ok);
+  void send(final boolean ok) throws IOException {
+    serverProc.send(ok);
+  }
+
+  /**
+   * Starts a timeout thread for the specified process.
+   */
+  private void startTimer() {
+    final long to = ctx.prop.num(Prop.TIMEOUT);
+    if(to == 0) return;
+
+    timeout = new Thread() {
+      @Override
+      public void run() {
+        Performance.sleep(10);
+        running = false;
+      }
+    };
+    timeout.start();
+  }
+
+  /**
+   * Stops the current timeout thread.
+   */
+  private void stopTimer() {
+    if(timeout != null) timeout.interrupt();
   }
 }
