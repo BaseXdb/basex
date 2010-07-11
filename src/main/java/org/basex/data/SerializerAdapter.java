b74c1b11abb9fe96fa4fcf29bbcb4c860353044c
leonard woerteler <leonard.woerteler @uni-konstanz.de>
2010-07-11 12:06:54 +0000

    Adapter for custom serializers.

---
+++ src/main/java/org/basex/data/SerializerAdapter.java
@@ -0,0 +1,55 @@
+package org.basex.data;
+
+import java.io.IOException;
+
+/**
+ * This class subclasses {@link Serializer} and provides empty methods for all
+ * abstract methods.
+ * 
+ * @author Workgroup DBIS, University of Konstanz 2005-10, ISC License
+ * @author Josua Krause
+ * @author Leo Woerteler
+ */
+@SuppressWarnings("unused")
+public class SerializerAdapter extends Serializer {
+
+  @Override
+  public void attribute(final byte[] n, final byte[] v) throws IOException { }
+
+  @Override
+  protected void close(final byte[] t) throws IOException { }
+
+  @Override
+  public void closeResult() throws IOException { }
+
+  @Override
+  protected void cls() throws IOException { }
+
+  @Override
+  public void comment(final byte[] b) throws IOException { }
+
+  @Override
+  protected void empty() throws IOException { }
+
+  @Override
+  protected void finish() throws IOException { }
+
+  @Override
+  public void item(final byte[] b) throws IOException { }
+
+  @Override
+  public void openResult() throws IOException { }
+
+  @Override
+  public void pi(final byte[] n, final byte[] v) throws IOException { }
+
+  @Override
+  protected void start(final byte[] t) throws IOException { }
+
+  @Override
+  public void text(final byte[] b) throws IOException { }
+
+  @Override
+  public void text(final byte[] b, final FTPos ftp) throws IOException { }
+
+}
