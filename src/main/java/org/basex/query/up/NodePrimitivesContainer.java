3c355ef7c9032b91136d3965b2138a3c48a956c4
lukas kircher <lukas.kircher @uni-konstanz.de>
2010-09-29 09:44:48 +0000

    *XQUF: fixed XQUFTS applyUpdates-007 issue that resulted from yesterdays 'optimization'

--- src/main/java/org/basex/query/up/NodePrimitivesContainer.java
+++ src/main/java/org/basex/query/up/NodePrimitivesContainer.java
@@ -38,8 +38,8 @@ final class NodePrimitivesContainer implements NodePrimitives {
    */
   private void add(final int i, final UpdatePrimitive p) throws QueryException {
     final PrimitiveType pt = p.type();
-    del = pt == DELETE;
-    rep = pt == REPLACENODE;
+    del = pt == DELETE || del;
+    rep = pt == REPLACENODE || rep;
     textAdjacency = del || rep || pt == INSERTBEFORE || pt == INSERTAFTER;
     if(i < primitives.size()) {
       final UpdatePrimitive a = primitives.get(i);
