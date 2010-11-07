2ced856d4581ab55b1cc5c54b3baf31872082046
dimitar popov <dimitar.popov @uni-konstanz.de>
2010-11-07 16:25:57 +0000

    Unit tests for org.basex.util.ft.FTBitapSearch

---
+++ src/test/java/org/basex/test/util/FTBitapSearchTest.java
@@ -0,0 +1,176 @@
+package org.basex.test.util;
+
+import static org.basex.util.Token.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+import org.basex.query.QueryException;
+import org.basex.util.TokenList;
+import org.basex.util.ft.FTBitapSearch;
+import org.basex.util.ft.FTBitapSearch.TokenComparator;
+import org.basex.util.ft.FTIterator;
+import org.basex.util.ft.Span;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test {@link FTBitapSearch} methods.
+ * @author Dimitar Popov
+ */
+public class FTBitapSearchTest {
+
+  /** Simple comparator. */
+  private static final TokenComparator CMP = new TokenComparator() {
+    @Override
+    public boolean equal(final byte[] o1, final byte[] o2) {
+      return eq(o1, o2);
+    }
+  };
+
+  /**
+   * Test data.
+   * @author Dimitar Popov
+   */
+  public static class TestData {
+    /** Set to search in. */
+    public final FTIterator haystack;
+    /** Set to search for. */
+    public final ArrayList<byte[][]> needles;
+    /** Indices of expected hits. */
+    public final int[] expected;
+
+    /**
+     * Constructor.
+     * @param h haystack
+     * @param n needle
+     * @param e expected hits
+     */
+    public TestData(final String[] h, final String[][] n, final int[] e) {
+      expected = e;
+      final byte[][] hs = new byte[h.length][];
+      for(int i = 0; i < h.length; i++)
+        hs[i] = token(h[i]);
+      
+      haystack = new FTIterator() {
+        /** Index of current element. */
+        private int cnt;
+
+        @Override
+        public boolean hasNext() {
+          return cnt < hs.length;
+        }
+
+        @Override
+        public byte[] nextToken() {
+          if(hasNext()) return hs[cnt++];
+          throw new NoSuchElementException();
+        }
+
+        @Override
+        public Span next() {
+          return null;
+        }
+
+        @Override
+        public void init(final byte[] text) { }
+      };
+      needles = new ArrayList<byte[][]>(n.length);
+      for(int i = 0; i < n.length; i++) {
+        final TokenList needle = new TokenList(n[i].length);
+        for(int j = 0; j < n[i].length; j++)
+          needle.add(token(n[i][j]));
+        needles.add(needle.toArray());
+      }
+    }
+  }
+
+  /** Test data. */
+  private static final TestData[] TESTS = new TestData[] {
+      new TestData(
+          new String[] { "token1", "token1", "token2", "token3", "token4",
+              "token1", "token2", "token3", "token3" },
+          new String[][] { { "token1", "token2" } },
+          new int[] { 1, 5 }),
+      new TestData(
+          new String[] { "token1" },
+          new String[][] { { "token1" } },
+          new int[] { 0 }),
+      new TestData(
+          new String[] { "token1"},
+          new String[][] { { "token2" } },
+          new int[] { }),
+      new TestData(
+          new String[] { "token1" },
+          new String[][] { { } },
+          new int[] { }),
+      new TestData(
+          new String[] { "token1", "token2", "token1", "token2", "token1" },
+          new String[][] { { "token1", "token2", "token1" } },
+          new int[] { 0, 2 }),
+      new TestData(
+          new String[] { "token", "token", "token" },
+          new String[][] { { "token", "token" } },
+          new int[] { 0, 1}),
+      new TestData(
+          new String[] { },
+          new String[][] { { "token", "token"} },
+          new int[] { }),
+      new TestData(
+          new String[] { },
+          new String[][] { { } },
+          new int[] { }),
+      new TestData(
+          new String[] { "token" },
+          new String[][] { },
+          new int[] { }),
+      new TestData(
+          new String[] { "token1", "token2", "token3" },
+          new String[][] { { "token2", "token3"}, { "token2" } },
+          new int[] { 1, 1}),
+      new TestData(
+          new String[] { "token1", "token2", "token3" },
+          new String[][] { { "token2" }, { "token2", "token3" } },
+          new int[] { 1, 1 }),
+      new TestData(
+          new String[] { "token1", "token2", "token3" },
+          new String[][] { { "token2" }, { "token1", "token2" } },
+          new int[] { 0})};
+
+  /** Pre-initialized {@link FTBitapSearch} objects. */
+  private FTBitapSearch[] searches;
+
+  /** Set up method. */
+  @Before
+  public void setUp() {
+    searches = new FTBitapSearch[TESTS.length];
+    for(int i = 0; i < searches.length; i++) {
+      searches[i] = new FTBitapSearch(TESTS[i].haystack, TESTS[i].needles, CMP);
+    }
+  }
+
+  /** Test search. */
+  @Test
+  public void testSearchIter() {
+    try {
+      for(int i = 0; i < TESTS.length; i++) {
+        final FTBitapSearch s = searches[i];
+        for(int j = 0; j < TESTS[i].expected.length; j++) {
+          if(!s.hasNext())
+            fail("Test " + i + ": expected " + TESTS[i].expected.length +
+                " hits, got only " + (j + 1));
+          final int pos = s.next();
+          if(pos != TESTS[i].expected[j])
+            fail("Test " + i + ", result " + j + ": expected " +
+                TESTS[i].expected[j] + ", got " + pos);
+        }
+        if (s.hasNext())
+          fail("Test " + i + ": expected " + TESTS[i].expected.length +
+              " hits, got more!");
+      }
+    } catch(QueryException e) {
+      fail(e.getMessage());
+    }
+  }
+}
