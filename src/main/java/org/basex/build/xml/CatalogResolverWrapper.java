1e9ed0d62233b92aecec47444cced214065d3c79
michael seiferle <michael.seiferle @uni-konstanz.de>
2010-11-10 13:16:21 +0000

    [FIX] Reconfigured the wrapper to either use the internal com.sun.org.apache.xml.internal.resolver or the external org.apache.xml.resolver.

--- src/main/java/org/basex/build/xml/CatalogResolverWrapper.java
+++ src/main/java/org/basex/build/xml/CatalogResolverWrapper.java
@@ -12,13 +12,64 @@ import org.xml.sax.XMLReader;
  * @author Michael Seiferle
  */
 public final class CatalogResolverWrapper {
+  /** Package declaration for either internal or external CatalogManager. */
+  private static final String CMP = cminitpackage();
+  /** Package declaration for either internal or external CatalogResolver. */
+  private static final String CRP = crinitpackage();
+
   /** Resolver if availiable. */
   private static final Object CM = init();
 
-
   /** private Constructor, no instantiation. */
-  private CatalogResolverWrapper() {
+  private CatalogResolverWrapper() {}
 
+  /**
+   * Searches for presence of the xml resolver packages.
+   * com.sun.org.apache.xml.internal.resolver.tools.CatalogResolver
+   * org.apache.xml.resolver.tools.CatalogResolver
+   * @return found package or default (internal) package on unsupported
+   *         platforms.
+   */
+  private static String crinitpackage() {
+    try {
+      Class.forName("org.apache.xml.resolver.tools.CatalogResolver");
+      return "org.apache.xml.resolver.tools.CatalogResolver";
+    } catch(Exception e) {
+      try {
+        Class.forName("com.sun.org.apache.xml.internal."
+            + "resolver.tools.CatalogResolver");
+        return "com.sun.org.apache.xml.internal.resolver.tools.CatalogResolver";
+      } catch(Exception e2) {
+
+      }
+    }
+
+    return "com.sun.org.apache.xml.internal.resolver.tools.CatalogResolver";
+  }
+
+  /**
+   * Searches for presence of the xml resolver packages.
+   * org.apache.xml.resolver.tools.CatalogManager
+   * com.sun.org.apache.xml.internal.resolver.CatalogManager
+   * @return found package or default (internal) package on unsupported
+   *         platforms.
+   */
+  private static String cminitpackage() {
+    // TODO Auto-generated method stub
+    // "com.sun.org.apache.xml.internal.resolver.CatalogManager";
+    try {
+      Class.forName("org.apache.xml.resolver.CatalogManager");
+      return "org.apache.xml.resolver.CatalogManager";
+    } catch(Exception e) {
+      try {
+        Class.forName("com.sun.org.apache.xml.internal."
+            + "resolver.CatalogManager");
+        return "com.sun.org.apache.xml.internal.resolver.CatalogManager";
+      } catch(Exception e2) {
+
+      }
+    }
+    return "com.sun.org.apache.xml.internal.resolver.CatalogManager";
   }
 
   /**
@@ -28,12 +79,11 @@ public final class CatalogResolverWrapper {
   private static Object init() {
     Object cmm = null;
     try {
-      cmm = Class.forName("org.apache.xml.resolver.CatalogManager").
-      newInstance();
+      cmm = Class.forName(CMP).newInstance();
     } catch(Exception e) { }
     return cmm;
-  }  
-  
+  }
+
   /**
    * Returns a CatalogResolver instance or null if it could not be found.
    * @return CatalogResolver if availiable
@@ -43,33 +93,29 @@ public final class CatalogResolverWrapper {
   }
 
   /**
-   * Decorates the XMLReader with the catalog resolver 
-   * if it has been found on the classpath. Does nothing otherwise.
+   * Decorates the XMLReader with the catalog resolver if it has been found on
+   * the classpath. Does nothing otherwise.
    * @param r XMLReader
    * @param cat path.
    */
-  public static void set(final XMLReader r, final String cat)  {
+  public static void set(final XMLReader r, final String cat) {
     if(null == CM) return;
-      try {
-        Class<?> clazz = Class.
-          forName("org.apache.xml.resolver.CatalogManager");
-        Method m = clazz.getMethod("setCatalogFiles", String.class);
-        m.invoke(CM, cat);
-        m = clazz.getMethod("setIgnoreMissingProperties", boolean.class);
-        m.invoke(CM, true);
-        m = clazz.getMethod("setPreferPublic", boolean.class);
-        m.invoke(CM, true);
-        m = clazz.getMethod("setUseStaticCatalog", boolean.class);
-        m.invoke(CM, false);
-        m = clazz.getMethod("setVerbosity", int.class);
-        m.invoke(CM, 0);
-        r.setEntityResolver((EntityResolver) Class.forName(
-            "org.apache.xml.resolver.tools.CatalogResolver").getConstructor(
-            new Class[] {
-                Class.forName("org.apache.xml.resolver.CatalogManager")}).
-                newInstance(CM));
+    try {
+      Class<?> clazz = Class.forName(CMP);
+      Method m = clazz.getMethod("setCatalogFiles", String.class);
+      m.invoke(CM, cat);
+      m = clazz.getMethod("setIgnoreMissingProperties", boolean.class);
+      m.invoke(CM, true);
+      m = clazz.getMethod("setPreferPublic", boolean.class);
+      m.invoke(CM, true);
+      m = clazz.getMethod("setUseStaticCatalog", boolean.class);
+      m.invoke(CM, false);
+      m = clazz.getMethod("setVerbosity", int.class);
+      m.invoke(CM, 0);
+      r.setEntityResolver((EntityResolver) Class.forName(CRP).getConstructor(
+          new Class[] { Class.forName(CMP)}).newInstance(CM));
 
-      } catch(Exception e) { }
+    } catch(Exception e) { }
   }
 
 }
