ae62544053c466fba3a8cee1bbc9dd96df48cbf2
christian gruen <christian.gruen @uni-konstanz.de>
2011-02-14 17:29:07 +0000

    [MOD] Release scripts updated.

--- src/main/java/org/basex/build/xml/SAXWrapper.java
+++ src/main/java/org/basex/build/xml/SAXWrapper.java
@@ -87,8 +87,8 @@ public final class SAXWrapper extends FileParser {
 
       sax = new SAXHandler(builder);
       final String cat = prop.get(Prop.CATFILE);
-
       if(!cat.isEmpty()) CatalogResolverWrapper.set(r, cat);
+
       r.setDTDHandler(sax);
       r.setContentHandler(sax);
       r.setProperty("http://xml.org/sax/properties/lexical-handler", sax);
