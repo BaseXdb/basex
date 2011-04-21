dd944da0119b5897fbb74662e5f4c582fd907da7
Christian Grün <'christian.gruen@gmail.com'>
2011-04-21 16:35:02 +0200

    [ADD] Example added for create database instance with file hierarchy

---
+++ src/main/java/org/basex/examples/create/SimpleFSParser.java
@@ -0,0 +1,75 @@
+package org.basex.examples.create;
+import static org.basex.util.Token.*;
+import java.io.File;
+import java.io.IOException;
+import org.basex.build.Builder;
+import org.basex.build.Parser;
+
+/**
+ * This class serves as a simple filesystem parser and creates an XML
+ * representation, which resembles the DeepFS markup.
+ *
+ * @author Workgroup DBIS, University of Konstanz 2005-10, ISC License
+ * @author Christian Gruen
+ */
+public final class SimpleFSParser extends Parser {
+  /** FS Token. */
+  private static final byte[] DIR = token("dir");
+  /** FS Token. */
+  private static final byte[] FILE = token("file");
+  /** FS Token. */
+  private static final byte[] FSML = token("fsml");
+  /** FS Token. */
+  private static final byte[] MTIME = token("mtime");
+  /** FS Token. */
+  private static final byte[] NAME = token("name");
+  /** FS Token. */
+  private static final byte[] SIZE = token("size");
+  /** FS Token. */
+  private static final byte[] SUFFIX = token("suffix");
+
+  /**
+   * Constructor.
+   * @param path file path
+   */
+  public SimpleFSParser(final String path) {
+    super(path);
+  }
+
+  @Override
+  public void parse(final Builder b) throws IOException {
+    b.startDoc(token(file.name()));
+    b.startElem(FSML, atts.reset());
+    parse(new File(file.path()), b);
+    b.endElem(FSML);
+    b.endDoc();
+  }
+
+  /**
+   * Recursively parses the specified directory.
+   * @param dir directory to be parsed
+   * @param b builder instance
+   * @throws IOException I/O exception
+   */
+  private void parse(final File dir, final Builder b) throws IOException {
+    atts.reset();
+    atts.add(NAME, token(dir.getName()));
+    atts.add(MTIME, token(dir.lastModified()));
+    b.startElem(DIR, atts);
+    for(final File f : dir.listFiles()) {
+      if(f.isDirectory()) {
+        parse(f, b);
+      } else {
+        atts.reset();
+        final String name = f.getName();
+        atts.add(NAME, token(name));
+        atts.add(SIZE, token(f.length()));
+        atts.add(MTIME, token(f.lastModified()));
+        int i = name.lastIndexOf('.');
+        if(i != -1) atts.add(SUFFIX, token(name.substring(i + 1)));
+        b.emptyElem(FILE, atts);
+      }
+    }
+    b.endElem(DIR);
+  }
+}
