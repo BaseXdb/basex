2f99871b2db4c866d1804ae8e8c895ed1c69d0dc
Leo Woerteler <leo@woerteler.de>
2011-06-02 21:10:17 +0200

    [ADD] tests for relocations in FLWOR clauses

--- src/main/java/org/basex/core/cmd/OptimizeAll.java
+++ src/main/java/org/basex/core/cmd/OptimizeAll.java
@@ -1,7 +1,6 @@
 package org.basex.core.cmd;
 
 import static org.basex.core.Text.*;
-import static org.basex.util.Token.*;
 import java.io.IOException;
 import org.basex.build.Builder;
 import org.basex.build.DiskBuilder;
@@ -10,13 +9,12 @@ import org.basex.core.CommandBuilder;
 import org.basex.core.Prop;
 import org.basex.core.User;
 import org.basex.core.Commands.Cmd;
+import org.basex.data.BuilderSerializer;
 import org.basex.data.DiskData;
-import org.basex.data.FTPos;
 import org.basex.data.MetaData;
 import org.basex.data.Serializer;
 import org.basex.index.IndexToken.IndexType;
 import org.basex.io.IO;
-import org.basex.util.Atts;
 import org.basex.util.Util;
 
 /**
@@ -116,112 +114,17 @@ public final class OptimizeAll extends ACreate {
 
     @Override
     public void parse(final Builder build) throws IOException {
-      final Serializer ser = new Serializer() {
-        /** Current tag's name. */
-        private byte[] tag;
-        /** True while being in an open tag. */
-        private boolean open;
-        /** Attribute cache. */
-        private final Atts att = new Atts();
-
-        @Override
-        public void text(final byte[] b, final FTPos ftp) throws IOException {
-          text(b);
-        }
-
-        @Override
-        public void text(final byte[] b) throws IOException {
-          finish();
-          build.text(b);
-        }
-
+      final Serializer ser = new BuilderSerializer(build) {
         @Override
         protected void start(final byte[] t) throws IOException {
+          super.start(t);
           pre++;
-          tag = t;
-          open = true;
-        }
-
-        @Override
-        public void pi(final byte[] n, final byte[] v) throws IOException {
-          build.pi(concat(n, new byte[]{ ' ' }, v));
-        }
-
-        @Override
-        public void item(final byte[] b) throws IOException {
-          text(b);
-        }
-
-        @Override
-        protected void finish() throws IOException {
-          if(open) {
-            build.startElem(tag, att);
-            att.reset();
-            open = false;
-          }
-        }
-
-        @Override
-        protected void empty() throws IOException {
-          if(open) {
-            build.emptyElem(tag, att);
-            open = false;
-          } else {
-            close(tag);
-          }
-          tag = null;
-          att.reset();
-        }
-
-        @Override
-        public void comment(final byte[] b) throws IOException {
-          build.comment(b);
-        }
-
-        @Override
-        protected void close(final byte[] t) throws IOException {
-          build.endElem(t);
-          tag = null;
-        }
-
-        @Override
-        public void attribute(final byte[] n, final byte[] v)
-            throws IOException {
-          if(startsWith(n, XMLNS)) {
-            if(n.length == 5) {
-              build.startNS(EMPTY, v);
-            } else if(n[5] == ':') {
-              build.startNS(substring(n, 6), v);
-            } else att.add(n, v);
-          } else {
-            att.add(n, v);
-          }
         }
 
         @Override
         protected void openDoc(final byte[] name) throws IOException {
+          super.openDoc(name);
           pre++;
-          build.startDoc(name);
-        }
-
-        @Override
-        protected void closeDoc() throws IOException {
-          build.endDoc();
-        }
-
-        @Override
-        public void openResult() throws IOException {
-          // ignore this
-        }
-
-        @Override
-        public void closeResult() throws IOException {
-          // ignore this
-        }
-
-        @Override
-        protected void cls() throws IOException {
-          // ignore this
         }
       };
       for(final int root : old.doc()) {
