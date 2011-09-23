e29a54f656d85c394c080e4521c66c160bb56143
dimitarp <dimitar.popov@uni-konstanz.de>
2011-09-24 00:00:31 +0200

    [MOD] XQuery full-text: new class for parsing and matching full-text wild-card queries + unit test

---
+++ src/main/java/org/basex/query/ft/FTWildcard.java
@@ -0,0 +1,125 @@
+package org.basex.query.ft;
+
+import static org.basex.query.util.Err.*;
+
+import org.basex.query.QueryException;
+import org.basex.util.InputInfo;
+
+/**
+ * Wild-card expression.
+ *
+ * @author BaseX Team 2005-11, BSD License
+ * @author Christian Gruen
+ * @author Dimitar Popov
+ */
+public class FTWildcard {
+  /** Value encoding the wild-card dot. */
+  private static final int DOT = -1;
+  /** Characters. */
+  private final int[] chars;
+  /** Minimum number of occureence. */
+  private final int[] min;
+  /** Maximum number of occureence. */
+  private final int[] max;
+
+  /**
+   * Parse and construct a new wild card expression.
+   * @param q input wild card expression
+   * @param ii input info
+   * @throws QueryException {@link org.basex.query.util.Err#FTREG}, if the wild
+   * card expression is not valid
+   */
+  public FTWildcard(final byte[] q, final InputInfo ii) throws QueryException {
+    final int[] tmpchars = new int[q.length];
+    final int[] tmpmin = new int[q.length];
+    final int[] tmpmax = new int[q.length];
+
+    int pos = -1;
+    int ql = 0;
+    while(ql < q.length) {
+      // parse wildcards
+      if(q[ql] == '.') {
+        byte c = ++ql < q.length ? q[ql] : 0;
+        // minimum/maximum number of occurrence
+        int n = 0;
+        int m = Integer.MAX_VALUE;
+        if(c == '?') { // .?
+          ++ql;
+          m = 1;
+        } else if(c == '*') { // .*
+          ++ql;
+        } else if(c == '+') { // .+
+          ++ql;
+          n = 1;
+        } else if(c == '{') { // .{m,n}
+          m = 0;
+          while(true) {
+            c = ++ql < q.length ? q[ql] : 0;
+            if(c >= '0' && c <= '9') n = (n << 3) + (n << 1) + c - '0';
+            else if(c == ',') break;
+            else FTREG.thrw(ii, q);
+          }
+          while(true) {
+            c = ++ql < q.length ? q[ql] : 0;
+            if(c >= '0' && c <= '9') m = (m << 3) + (m << 1) + c - '0';
+            else if(c == '}') break;
+            else FTREG.thrw(ii, q);
+          }
+          ++ql;
+        } else { // .
+          m = 1;
+          n = 1;
+        }
+        tmpmin[++pos] = n;
+        tmpmax[pos] = m;
+        tmpchars[pos] = DOT;
+      } else {
+        if(q[ql] == '\\' && ++ql == q.length) FTREG.thrw(ii, q);
+        tmpchars[++pos] = q[ql++];
+      }
+    }
+
+    chars = new int[pos + 1];
+    max = new int[chars.length];
+    min = new int[chars.length];
+
+    System.arraycopy(tmpchars, 0, chars, 0, chars.length);
+    System.arraycopy(tmpmax, 0, max, 0, max.length);
+    System.arraycopy(tmpmin, 0, min, 0, min.length);
+  }
+
+  /**
+   * Check if the wild-card can match a sub-string in a string.
+   * @param t token to search for match
+   * @return <code>true</code> if a match is found
+   */
+  public boolean match(final byte[] t) {
+    return match(t, 0, 0);
+  }
+
+  /**
+   * Check if the wild-card can match a sub-string in a string.
+   * @param t token to search for match
+   * @param tp input position
+   * @param qp query position
+   * @return <code>true</code> if a match is found
+   */
+  private boolean match(final byte[] t, final int tp, final int qp) {
+    int ql = qp;
+    int tl = tp;
+    while(ql < chars.length) {
+      if(chars[ql] == DOT) {
+        int n = min[ql];
+        final int m = max[ql++];
+        // recursively evaluates wildcards (non-greedy)
+        while(!match(t, tl + n, ql))
+          if(tl + ++n > t.length) return false;
+        if(n > m) return false;
+        tl += n;
+      } else {
+        if(tl >= t.length || t[tl++] != chars[ql++]) return false;
+      }
+    }
+    return tl == t.length;
+  }
+}
---
+++ src/test/java/org/basex/test/query/FTWildcardTest.java
@@ -0,0 +1,98 @@
+package org.basex.test.query;
+
+import static org.basex.util.Token.*;
+import static org.junit.Assert.*;
+
+import org.basex.query.QueryException;
+import org.basex.query.ft.FTWildcard;
+import org.junit.Test;
+
+/**
+ * Wild-card parsing and matching tests.
+ *
+ * @author BaseX Team 2005-11, BSD License
+ * @author Dimitar Popov
+ */
+public class FTWildcardTest {
+  /** Sample texts which should be matched. */
+  private static final String[][] TEXTS_GOOD = {
+    { "well", "wall", "will", "w.ll", /* "wüll" */},
+    { "site", "asite" },
+    { "improv", "improve", "improvement" },
+    { "wisdomness", "witches" },
+    { "site" },
+    { "Usability\\", "Usable\\" }
+  };
+  /** Sample texts which should NOT be matched. */
+  private static final String[][] TEXTS_BAD = new String[][] {
+    { "wll", "wel", "ell" },
+    { "bite", "abite", "sit", "asit" },
+    { "impro", "mprove" },
+    { "witch", "wiskey", "wisdomnetts" },
+    { "\\s\\i\\t\\e" },
+    { "Usability", "Usab\\", "usability\\" }
+  };
+
+  /** Valid wild-card expressions. */
+  private static final String[] VALIDWC = new String[] {
+    "w.ll",
+    ".?site",
+    "improv.*",
+    "wi.{4,7}s",
+    "\\s\\i\\t\\e",
+    "Usab.+\\\\"
+  };
+
+  /** Valid wild card expressions. */
+  private static final String[] INVALIDWC = new String[] {
+    "wi.{5,7]",
+    "will\\"
+  };
+
+  /** Test if wild-card expressions are correctly parsed. */
+  @Test
+  public void testParse() {
+    for(final String wc : VALIDWC)
+      try {
+        new FTWildcard(token(wc), null);
+      } catch(final Exception e) {
+        e.printStackTrace();
+        fail("Parsing failed: " + wc);
+      }
+
+    for(final String wc : INVALIDWC)
+      try {
+        new FTWildcard(token(wc), null);
+        fail("Parsing did NOT failed: " + wc);
+      } catch(final QueryException e) {
+      } catch(final Exception e) {
+        e.printStackTrace();
+        fail("Error while parsing: " + wc);
+      }
+  }
+
+  /**
+   * Test wild-card matching.
+   * @throws QueryException wild-card expression is not parsed
+   */
+  @Test
+  public void testMatch() throws QueryException {
+    for(int i = 0; i < VALIDWC.length; i++) {
+
+      final String q = VALIDWC[i];
+      final FTWildcard wc = new FTWildcard(token(q), null);
+
+      final String[] good = TEXTS_GOOD[i];
+      for(int j = 0; j < good.length; j++) {
+        assertTrue("\"" + q + "\" did NOT match \"" + good[j] + "\"",
+            wc.match(token(good[j])));
+      }
+
+      final String[] bad = TEXTS_BAD[i];
+      for(int j = 0; j < bad.length; j++) {
+        assertFalse("\"" + q + "\" matched \"" + bad[j] + "\"",
+            wc.match(token(bad[j])));
+      }
+    }
+  }
+}
