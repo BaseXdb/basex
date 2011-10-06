d18f557d0d16f356d4f0f23d3a6610a75d1eaeb3
Christian Grün <'christian.gruen@gmail.com'>
2011-10-06 13:27:33 +0200

    [MOD] Full-text: Japanese tokenizer updated.

--- src/main/java/org/basex/util/ft/JapaneseTokenizer.java
+++ src/main/java/org/basex/util/ft/JapaneseTokenizer.java
@@ -13,8 +13,8 @@ import java.util.Iterator;
 
 import org.basex.core.Prop;
 import org.basex.util.Reflect;
+import org.basex.util.TokenBuilder;
 import org.basex.util.Util;
-import org.basex.util.list.StringList;
 
 /**
  * Japanese lexer using igo (http://igo.sourceforge.jp/).
@@ -30,7 +30,10 @@ class JapaneseTokenizer extends Tokenizer {
   private static final String PATTERN = "net.reduls.igo.Tagger";
   /** Name of Japanese dictionary. */
   private static final String LANG = "ja";
-
+  /** A part of speech, KIGOU(Mark). */
+  private static final int HINSHI_MARK = 1;
+  /** A part of speech, Others(only a mark is distinguished). */
+  private static final int HINSHI_OTHERS = 0;
   /** Igo constructor. */
   private static Constructor<?> tgr;
   /** Igo instance. */
@@ -39,11 +42,13 @@ class JapaneseTokenizer extends Tokenizer {
   private static Method parse;
   /** Surface field. */
   private static Field surface;
+  /** feature field. */
+  private static Field feature;
   /** Start field. */
   private static Field start;
 
   /** Token iterator. */
-  private Iterator<String> tokens;
+  private Iterator<Morpheme> tokens;
   /** Sensitivity flag. */
   private final boolean cs;
   /** Uppercase flag. */
@@ -74,13 +79,19 @@ class JapaneseTokenizer extends Tokenizer {
       } else {
         tgr = Reflect.find(clz, String.class);
         tagger = Reflect.get(tgr, dic.toString());
-        parse = Reflect.method(clz, "parse", CharSequence.class);
-        if(parse == null) {
-          Util.errln("Could not initialize Igo lexer method.");
+        if(tagger == null) {
+          available = false;
+          Util.errln("Could not initialize Igo Japanese lexer.");
+        } else {
+          parse = Reflect.method(clz, "parse", CharSequence.class);
+          if(parse == null) {
+            Util.errln("Could not initialize Igo lexer method.");
+          }
+          clz = Reflect.find("net.reduls.igo.Morpheme");
+          surface = Reflect.field(clz, "surface");
+          feature = Reflect.field(clz, "feature");
+          start = Reflect.field(clz, "start");
         }
-        clz = Reflect.find("net.reduls.igo.Morpheme");
-        surface = Reflect.field(clz, "surface");
-        start = Reflect.field(clz, "start");
       }
     }
   }
@@ -114,26 +125,30 @@ class JapaneseTokenizer extends Tokenizer {
     final ArrayList<?> morpheme =
       (ArrayList<?>) Reflect.invoke(parse, tagger, source);
 
-    final StringList sl = new StringList();
+    final ArrayList<Morpheme> list = new ArrayList<Morpheme>();
     try {
       int prev = 0;
       for(int i = 0; i < morpheme.size(); i++) {
         final Object m = morpheme.get(i);
         final String srfc = surface.get(m).toString();
+        final String ftr = feature.get(m).toString();
+        final int hinshi = getHinshi(srfc, ftr);
         final int s = start.getInt(m);
         if(i != 0) {
           final int l = s - prev;
           if(l != 0) {
-            sl.add(source.substring(s - 1, s + l - 1));
+            list.add(new Morpheme(
+                source.substring(s - 1, s + l - 1), HINSHI_MARK)
+            );
           }
         }
         prev = srfc.length() + s;
-        sl.add(srfc);
+        list.add(new Morpheme(srfc, hinshi));
       }
     } catch(final Exception ex) {
       Util.errln(Util.name(this) + ": " + ex);
     }
-    tokens = sl.iterator();
+    tokens = list.iterator();
 
     return this;
   }
@@ -150,11 +165,18 @@ class JapaneseTokenizer extends Tokenizer {
 
   @Override
   public byte[] nextToken() {
-    String n = tokens.next();
+    final Morpheme m = tokens.next();
+    String n = m.mSurface;
     pos++;
+    if(special) {
+      return token(n);
+    }
+    if(m.mHinshi == HINSHI_MARK) {
+      return EMPTY;
+    }
     if(uc) n = n.toUpperCase();
-    if(lc || !cs) n.toLowerCase();
-    return token(n);
+    if(lc || !cs) n = n.toLowerCase();
+    return toHankaku(token(n));
   }
 
   @Override
@@ -165,5 +187,133 @@ class JapaneseTokenizer extends Tokenizer {
   @Override
   Collection<Language> languages() {
     return collection(LANG);
+  }
+
+  /**
+   * Return the mark or not.
+   * @param srfc Morpheme surface
+   * @param ftr Morpheme feature
+   * @return mark or not
+   */
+  private static int getHinshi(final String srfc, final String ftr) {
+    int hinshi = 0;
+    //morphological analyzer certainly returns
+    //the single ascii char as a "noun".
+    final byte[] s = token(srfc);
+    if(s.length == 1 && !letter(s[0]) && !digit(s[0])) {
+      hinshi = HINSHI_MARK;
+    } else {
+      final String[] parts = ftr.split(",");
+      if(parts[0].equals("\u8A18\u53F7")) {
+        hinshi = HINSHI_MARK;
+      } else {
+        hinshi = HINSHI_OTHERS;
+      }
+    }
+    return hinshi;
+  }
+
+  /**
+   * Converts to HANKAKU characters.
+   * @param s Japanese text
+   * @return result of conversion(->HANKAKU)
+   */
+  private static byte[] toHankaku(final byte[] s) {
+    if(ascii(s)) return s;
+    final TokenBuilder tb = new TokenBuilder(s.length);
+    for(int p = 0; p < s.length; p += cl(s, p)) {
+      final int c = cp(s, p);
+      if(c >= 0xFF10 && c <= 0xFF19 || c >= 0xFF21 && c <= 0xFF3A
+          || c >= 0xFF41 && c <= 0xFF5A) {
+        tb.add(c - 0xFEE0);
+      } else if(c == 0x3000) { // IDEOGRAPHIC SPACE
+        tb.add(0x0020);
+      } else if(c == 0xFF01) { // !
+        tb.add(0x0021);
+      } else if(c == 0x201D) { // "
+        tb.add(0x0022);
+      } else if(c == 0xFF03) { // #
+        tb.add(0x0023);
+      } else if(c == 0xFF04) { // $
+        tb.add(0x0024);
+      } else if(c == 0xFF05) { // %
+        tb.add(0x0025);
+      } else if(c == 0xFF06) { // &
+        tb.add(0x0026);
+      } else if(c == 0x2019) { // '
+        tb.add(0x0027);
+      } else if(c == 0xFF08) { // (
+        tb.add(0x0028);
+      } else if(c == 0xFF09) { // )
+        tb.add(0x0029);
+      } else if(c == 0xFF0A) { // *
+        tb.add(0x002A);
+      } else if(c == 0xFF0B) { // +
+        tb.add(0x002B);
+      } else if(c == 0xFF0C) { // ,
+        tb.add(0x002C);
+      } else if(c == 0xFF0D) { // -
+        tb.add(0x002D);
+      } else if(c == 0xFF0E) { // .
+        tb.add(0x002E);
+      } else if(c == 0xFF0F) { // /
+        tb.add(0x002F);
+      } else if(c == 0xFF1A) { // :
+        tb.add(0x003A);
+      } else if(c == 0xFF1B) { // ;
+        tb.add(0x003B);
+      } else if(c == 0xFF1C) { // <
+        tb.add(0x003C);
+      } else if(c == 0xFF1D) { // =
+        tb.add(0x003D);
+      } else if(c == 0xFF1E) { // >
+        tb.add(0x003E);
+      } else if(c == 0xFF1F) { // ?
+        tb.add(0x003F);
+      } else if(c == 0xFF20) { // @
+        tb.add(0x0040);
+      } else if(c == 0xFF3B) { // [
+        tb.add(0x005B);
+      } else if(c == 0xFFE5) { // \
+        tb.add(0x005C);
+      } else if(c == 0xFF3D) { // ]
+        tb.add(0x005D);
+      } else if(c == 0xFF3E) { // ^
+        tb.add(0x005E);
+      } else if(c == 0xFF3F) { // _
+        tb.add(0x005F);
+      } else if(c == 0xFF40) { // `
+        tb.add(0x0060);
+      } else if(c == 0xFF5B) { // {
+        tb.add(0x007B);
+      } else if(c == 0xFF5C) { // |
+        tb.add(0x007C);
+      } else if(c == 0xFF5D) { // }
+        tb.add(0x007D);
+      } else if(c == 0xFF5E) { // ~
+        tb.add(0x007E);
+      } else {
+        tb.add(c);
+      }
+    }
+    return tb.finish();
+  }
+
+  /** Morpheme class. */
+  private static class Morpheme {
+    /** Surface of Morpheme. */
+    final String mSurface;
+    /** Feature of Morpheme. */
+    final int mHinshi;
+
+    /**
+     * Constructor.
+     * @param srfc surface
+     * @param hinshi a part of speech
+    */
+    Morpheme(final String srfc, final int hinshi) {
+      mSurface = srfc;
+      mHinshi = hinshi;
+    }
   }
 }
