8665b4df104e13428ca28c4eaeb036bd017e9cec
Christian Grün <'christian.gruen@gmail.com'>
2011-10-22 17:01:47 +0200

    [MOD] Version 7.0.1 RC1: start and release scripts updated

--- etc/basexhttp.bat
+++ etc/basexhttp.bat
@@ -4,13 +4,13 @@ setlocal
 REM Path to this script
 set PWD=%~dp0
 
-REM Paths to JAR archives or class files
-set BASEX=%PWD%/../../basex/target/classes
-set BASEXAPI=%PWD%/../target/classes
-
-REM Classpath
+REM Core and library classes
+set CP=%PWD%/../target/classes
 set LIB=%PWD%/../lib
-set CP=%BASEX%;%BASEXAPI%;%LIB%/commons-codec-1.4.jar;%LIB%/commons-fileupload-1.2.2.jar;%LIB%/commons-io-1.4.jar;%LIB%/igo-0.4.3.jar;%LIB%/jdom-1.1.jar;%LIB%/jetty-6.1.26.jar;%LIB%/jetty-util-6.1.26.jar;%LIB%/lucene-stemmers-3.4.0.jar;%LIB%/milton-api-1.6.5.jar;%LIB%/mime-util-2.1.3.jar;%LIB%/xml-resolver-1.2.jar;%LIB%/servlet-api-2.5-20081211.jar;%LIB%/slf4j-api-1.5.8.jar;%LIB%/slf4j-nop-1.5.8.jar;%LIB%/tagsoup-1.2.jar;%LIB%/xmldb-api-1.0.jar;%LIB%/xqj-api-1.0.jar
+set CP=%CP%;%LIB%/commons-codec-1.4.jar;%LIB%/commons-fileupload-1.2.2.jar;%LIB%/commons-io-1.4.jar;%LIB%/jdom-1.1.jar;%LIB%/jetty-6.1.26.jar;%LIB%/jetty-util-6.1.26.jar;%LIB%/milton-api-1.6.5.jar;%LIB%/mime-util-2.1.3.jar;%LIB%/servlet-api-2.5-20081211.jar;%LIB%/slf4j-api-1.5.8.jar;%LIB%/slf4j-nop-1.5.8.jar;%LIB%/xmldb-api-1.0.jar;%LIB%/xqj-api-1.0.jar
+set CP=%CP%;%PWD%/../../basex/target/classes
+set LIB=%PWD%/../../basex/lib
+set CP=%CP%;%LIB%/igo-0.4.3.jar;%LIB%/lucene-stemmers-3.4.0.jar;%LIB%/tagsoup-1.2.jar;%LIB%/xml-resolver-1.2.jar
 
 REM Options for virtual machine
 set VM=-Xmx512m
