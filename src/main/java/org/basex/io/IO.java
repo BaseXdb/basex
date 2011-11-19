0642bd7ec16196bdc852841123e62238c9527920
Lukas Kircher <lukaskircher1@gmail.com>
2011-11-19 19:40:41 +0100

    [FIX] pattern matching in Restore CMD + some more functionality. Almost there

--- src/main/java/org/basex/io/IO.java
+++ src/main/java/org/basex/io/IO.java
@@ -38,6 +38,9 @@ public abstract class IO {
   /** Date format which is appended to backups. */
   public static final SimpleDateFormat DATE =
     new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+  /** Date pattern. */
+  public static final String DATEPATTERN =
+    "-\\d{4}-\\d{2}-\\d{2}-\\d{2}-\\d{2}-\\d{2}";
 
   /** XQuery suffixes. */
   public static final String[] XQSUFFIXES =
