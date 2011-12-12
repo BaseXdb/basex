9d9bc474e62db8a0e6825085a6726369ccd305bd
Christian Grün <'christian.gruen@gmail.com'>
2011-12-12 23:45:15 +0100

    [MOD] XQuery: moving static context properties to own class

---
+++ src/main/java/org/basex/query/StaticContext.java
@@ -0,0 +1,120 @@
+package org.basex.query;
+
+import static org.basex.query.QueryText.*;
+import static org.basex.util.Token.*;
+
+import org.basex.io.IO;
+import org.basex.query.item.SeqType;
+import org.basex.query.item.Uri;
+import org.basex.query.util.NSContext;
+import org.basex.query.util.format.DecFormatter;
+import org.basex.util.Util;
+import org.basex.util.hash.TokenObjMap;
+
+/**
+ * This class contains the static context of an expression.
+ *
+ * @author BaseX Team 2005-11, BSD License
+ * @author Christian Gruen
+ */
+public final class StaticContext {
+  /** Static and dynamic namespaces. */
+  public NSContext ns = new NSContext();
+  /** Default element/type namespace. */
+  public byte[] nsElem;
+  /** Default function namespace. */
+  public byte[] nsFunc = FNURI;
+  /** Context item static type. */
+  public SeqType initType;
+  /** Default collation. */
+  public Uri collation = Uri.uri(URLCOLL);
+
+  /** Construction mode. */
+  public boolean construct;
+  /** Ordering mode. */
+  public boolean ordered;
+  /** Default order for empty sequences. */
+  public boolean orderGreatest;
+  /** Boundary-space policy. */
+  public boolean spaces;
+  /** Copy-namespaces mode: (no-)preserve. */
+  public boolean nsPreserve = true;
+  /** Copy-namespaces mode: (no-)inherit. */
+  public boolean nsInherit = true;
+
+  /** Static Base URI. */
+  private byte[] baseURI = EMPTY;
+
+  /** Decimal formats. */
+  public final TokenObjMap<DecFormatter> decFormats =
+    new TokenObjMap<DecFormatter>();
+
+  /**
+   * Adopts values of the specified static context.
+   * @param sc static context
+   */
+  public void copy(final StaticContext sc) {
+    ns = sc.ns;
+    nsElem = sc.nsElem;
+    nsFunc = sc.nsFunc;
+    // vars, ctxItem, funcs
+    collation = sc.collation;
+    construct = sc.construct;
+    ordered = sc.ordered;
+    orderGreatest = sc.orderGreatest;
+    spaces = sc.spaces;
+    nsPreserve = sc.nsPreserve;
+    nsInherit = sc.nsInherit;
+    baseURI = sc.baseURI;
+    // decFormats
+  }
+
+  /**
+   * Declares a namespace.
+   * A namespace is undeclared if the {@code uri} is an empty string.
+   * The default element namespaces is set if the {@code prefix} is empty.
+   * @param prefix namespace prefix
+   * @param uri namespace uri
+   * @throws QueryException query exception
+   */
+  public void namespace(final String prefix, final String uri)
+      throws QueryException {
+
+    if(prefix.isEmpty()) {
+      nsElem = uri.isEmpty() ? null : token(uri);
+    } else if(uri.isEmpty()) {
+      ns.delete(token(prefix));
+    } else {
+      ns.add(token(prefix), token(uri), null);
+    }
+  }
+
+  /**
+   * Returns an IO representation of the base URI, or {@code null}.
+   * @return IO reference
+   */
+  public IO baseIO() {
+    return baseURI.length != 0 ? IO.get(string(baseURI)) : null;
+  }
+
+  /**
+   * Returns a URI representation of the base URI.
+   * @return IO reference
+   */
+  public Uri baseURI() {
+    return Uri.uri(baseURI);
+  }
+
+  /**
+   * Sets the base URI.
+   * @param uri uri to be set
+   */
+  public void baseURI(final String uri) {
+    baseURI = token(uri);
+  }
+
+  @Override
+  public String toString() {
+    return Util.name(this) + '[' + baseIO() + ']';
+  }
+}
---
+++ src/main/java/org/basex/query/util/VarStack.java
@@ -0,0 +1,121 @@
+package org.basex.query.util;
+
+import java.io.IOException;
+import java.util.Arrays;
+import org.basex.core.Text;
+import org.basex.data.ExprInfo;
+import org.basex.io.serial.Serializer;
+import org.basex.query.QueryException;
+import org.basex.query.item.QNm;
+import org.basex.util.Array;
+
+/**
+ * Variable stack.
+ *
+ * @author BaseX Team 2005-11, BSD License
+ * @author Christian Gruen
+ */
+public final class VarStack extends ExprInfo {
+  /** Variable expressions. */
+  public Var[] vars;
+  /** Number of stored variables. */
+  public int size;
+
+  /**
+   * Default constructor.
+   */
+  public VarStack() {
+    this(4);
+  }
+
+  /**
+   * Default constructor.
+   * @param c initial capacity
+   */
+  public VarStack(final int c) {
+    vars = new Var[c];
+  }
+
+  /**
+   * Adds or replaces the specified variable.
+   * @param v variable
+   */
+  public void update(final Var v) {
+    final int i = indexOf(v);
+    if(i == -1) add(v);
+    else vars[i] = v;
+  }
+
+  /**
+   * Adds the specified variable.
+   * @param v variable
+   */
+  public void add(final Var v) {
+    if(size == vars.length) vars = Arrays.copyOf(vars, Array.newSize(size));
+    vars[size++] = v;
+  }
+
+  /**
+   * Returns a variable with the specified name; should only be
+   * used while parsing because it ignores ids of variables.
+   * @param name variable name
+   * @return variable
+   */
+  public Var get(final QNm name) {
+    for(int i = size; i-- > 0;) if(name.eq(vars[i].name)) return vars[i];
+    return null;
+  }
+
+  /**
+   * Returns a variable with the same id.
+   * @param v variable
+   * @return variable
+   */
+  public Var get(final Var v) {
+    final int i = indexOf(v);
+    return i == -1 ? null : vars[i];
+  }
+
+  /**
+   * Returns the index of a variable with the same id, or {@code -1}.
+   * @param v variable
+   * @return index
+   */
+  private int indexOf(final Var v) {
+    for(int s = size - 1; s >= 0; s--) if(v.is(vars[s])) return s;
+    return -1;
+  }
+
+  /**
+   * Checks if the given variable is in this list.
+   * @param v variable
+   * @return {@code true} if the variable was found, {@code false} otherwise
+   */
+  public boolean contains(final Var v) {
+    return indexOf(v) != -1;
+  }
+
+  /**
+   * Checks if none of the variables contains an updating expression.
+   * @throws QueryException query exception
+   */
+  public void checkUp() throws QueryException {
+    for(int i = 0; i < size; ++i) vars[i].checkUp();
+  }
+
+  @Override
+  public void plan(final Serializer ser) throws IOException {
+    if(size == 0) return;
+    ser.openElement(this);
+    for(int i = 0; i < size; ++i) vars[i].plan(ser);
+    ser.closeElement();
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < size; ++i)
+      sb.append((i == 0 ? "" : Text.NL) + i + Text.COLS + vars[i]);
+    return sb.toString();
+  }
+}
