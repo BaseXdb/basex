c5fd526c0cc81dc314004e764efeb63739e223b8
Christian Grün <'christian.gruen@gmail.com'>
2012-02-07 03:40:41 +0100

    [FIX] GUI, GH-369: OK button did not close the dialog window

--- src/main/java/org/basex/gui/dialog/DialogProps.java
+++ src/main/java/org/basex/gui/dialog/DialogProps.java
@@ -54,7 +54,7 @@ public final class DialogProps extends Dialog {
     panel.setLayout(new BorderLayout(5, 0));
 
     optimize = new BaseXButton(OPTIMIZE_D, this);
-    buttons = newButtons(optimize, OK, CANCEL);
+    buttons = newButtons(optimize, B_OK, CANCEL);
 
     // resource tree
     resources = new DialogResources(this);
