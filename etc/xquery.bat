8c6957670a0fcdd171632434604e3ec0405f2e7e
Christian Grün <'christian.gruen@gmail.com'>
2012-03-29 15:00:23 +0200

    [MOD] CLI: Windows start scripts updated

--- etc/xquery.bat
+++ etc/xquery.bat
@@ -7,7 +7,7 @@ set PWD=%~dp0
 REM Core and library classes
 set CP=%PWD%/../target/classes
 set LIB=%PWD%/../lib
-set CP=%CP%;%LIB%/igo-0.4.3.jar;%LIB%/lucene-stemmers-3.4.0.jar;%LIB%/xml-resolver-1.2.jar;%LIB%/tagsoup-1.2.jar
+set CP=%CP%;%LIB%/igo-0.4.3.jar;%LIB%/lucene-stemmers-3.4.0.jar;%LIB%/xml-resolver-1.2.jar;%LIB%/tagsoup-1.2.jar;%LIB%/jline-1.0.jar
 
 REM Options for virtual machine
 set VM=-Xmx512m
