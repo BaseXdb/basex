2466bc0ea23d36987a6fbf310a7a650ef84fdb34
Christian Grün <christian.gruen@gmail.com>
2012-07-29 17:10:07 +0200

    [FIX] remaining stack trace output removed

--- src/main/java/org/basex/query/func/FNInfo.java
+++ src/main/java/org/basex/query/func/FNInfo.java
@@ -101,7 +101,6 @@ public final class FNInfo extends StandardFunc {
    * @param ctx query context
    */
   static void dump(final byte[] value, final byte[] label, final QueryContext ctx) {
-    Util.stack(12);
     final TokenBuilder tb = new TokenBuilder();
     if(label != null) tb.add(label);
     tb.add(value);
