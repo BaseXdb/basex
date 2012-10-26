4bde0f2e7285efc3f85cdd7db1ca0f7bc163f43a
Christian Grün <christian.gruen@gmail.com>
2012-10-26 13:56:06 +0200

    [MOD] XQuery, db:add(): adopt original document path from original node

--- src/main/java/org/basex/query/up/primitives/DBAdd.java
+++ src/main/java/org/basex/query/up/primitives/DBAdd.java
@@ -86,23 +86,30 @@ public final class DBAdd extends BasicOperation {
    * Creates a {@link Data} instance for the specified document.
    * @param doc item representing document(s)
    * @param pth target path
-   * @return {@link Data} instance from the parsed document(s)
-   * @throws QueryException if {@code doc} does not represent valid document(s)
+   * @return database instance
+   * @throws QueryException query exception
    */
   private Data docData(final Item doc, final byte[] pth) throws QueryException {
-    final MemData mdata;
-    String name = string(pth);
-    if(name.endsWith(".")) RESINV.thrw(info, pth);
+    if(doc instanceof AStr) return docData((AStr) doc, pth);
+    if(doc instanceof ANode) return docData((ANode) doc, pth);
+    throw STRNODTYPE.thrw(info, this, doc.type);
+  }
+
+  /**
+   * Creates a {@link Data} instance from the specified string.
+   * @param doc item representing document(s)
+   * @param pth target path
+   * @return database instance
+   * @throws QueryException query exception
+   */
+  private Data docData(final AStr doc, final byte[] pth) throws QueryException {
+    final QueryInput qi = new QueryInput(string(doc.string(info)));
+    if(!qi.io.exists()) WHICHRES.thrw(info, qi.original);
 
     // add slash to the target if the addressed file is an archive or directory
-    IO io = null;
-    if(doc instanceof AStr) {
-      final byte[] cont = doc.string(info);
-      io = IO.get(string(cont));
-      if(!io.exists()) WHICHRES.thrw(info, cont);
-      if(!name.endsWith("/") && (io.isDir() || io.isArchive())) name += "/";
-    }
-
+    String name = string(pth);
+    if(name.endsWith(".")) RESINV.thrw(info, pth);
+    if(!name.endsWith("/") && (qi.io.isDir() || qi.io.isArchive())) name += "/";
     String target = "";
     final int s = name.lastIndexOf('/');
     if(s != -1) {
@@ -110,36 +117,55 @@ public final class DBAdd extends BasicOperation {
       name = name.substring(s + 1);
     }
 
-    if(doc instanceof AStr) {
-      // set name of document
-      if(!name.isEmpty()) io.name(name);
-      // get name from io reference
-      else if(!(io instanceof IOContent)) name = io.name();
-    }
+    // set name of document
+    if(!name.isEmpty()) qi.io.name(name);
+    // get name from io reference
+    else if(!(qi.io instanceof IOContent)) name = qi.io.name();
 
     // ensure that the final name is not empty
     if(name.isEmpty()) RESINV.thrw(info, pth);
 
-    if(doc instanceof AStr) {
-      final Parser p = new DirParser(io, ctx.prop, data.meta.path).target(target);
-      final MemBuilder b = new MemBuilder(data.meta.name, p);
-      try {
-        mdata = b.build();
-      } catch(final IOException ex) {
-        throw IOERR.thrw(info, ex);
-      }
-    } else if(doc instanceof ANode) {
-      // adding a document node
-      ANode nd = (ANode) doc;
-      if(nd.type != NodeType.DOC) {
-        if(nd.type == NodeType.ATT) UPDOCTYPE.thrw(info, nd);
-        nd = new FDoc().add(nd);
-      }
-      mdata = (MemData) nd.dbCopy(data.meta.prop).data;
-      mdata.update(0, Data.DOC, pth);
-    } else {
-      throw STRNODTYPE.thrw(info, this, doc.type);
+    final Parser p = new DirParser(qi.io, ctx.prop, data.meta.path).target(target);
+    final MemBuilder b = new MemBuilder(data.meta.name, p);
+    try {
+      return b.build();
+    } catch(final IOException ex) {
+      throw IOERR.thrw(info, ex);
     }
+  }
+
+  /**
+   * Creates a {@link Data} instance from the specified node.
+   * @param node node to be added
+   * @param pth target path
+   * @return database instance
+   * @throws QueryException query exception
+   */
+  private Data docData(final ANode node, final byte[] pth) throws QueryException {
+    if(endsWith(pth, '.') || endsWith(pth, '/')) RESINV.thrw(info, pth);
+
+    // adding a document node
+    ANode nd = node;
+    if(nd.type != NodeType.DOC) {
+      if(nd.type == NodeType.ATT) UPDOCTYPE.thrw(info, nd);
+      nd = new FDoc().add(nd);
+    }
+
+    // ensure that the final name is not empty
+    byte[] name = pth;
+    if(name.length == 0) {
+      // adopt name from document node
+      name = nd.baseURI();
+      final Data d = node.data();
+      // adopt path if node is part of database. otherwise, only adopt file name
+      int i = d == null || d.inMemory() ? lastIndexOf(name, '/') : indexOf(name, '/');
+      if(i != -1) name = substring(name, i + 1);
+      if(name.length == 0) RESINV.thrw(info, name);
+    }
+
+    // adding a document node
+    final MemData mdata = (MemData) nd.dbCopy(data.meta.prop).data;
+    mdata.update(0, Data.DOC, name);
     return mdata;
   }
 
