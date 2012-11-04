5cc60ae09c0e06e6ce21423cadda7e6b6c399789
Christian Grün <christian.gruen@gmail.com>
2012-11-04 05:20:32 +0100

    [MIN] GUI: Font sizes in font dialog revised

--- src/main/java/org/basex/gui/dialog/DialogFonts.java
+++ src/main/java/org/basex/gui/dialog/DialogFonts.java
@@ -16,7 +16,8 @@ import org.basex.gui.layout.*;
 public final class DialogFonts extends BaseXDialog {
   /** Predefined font sizes. */
   private static final String[] FTSZ =
-    { "8", "10", "12", "14", "16", "18", "20", "22", "24", "32" };
+    { "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+      "22", "24", "26", "28", "30", "33", "36", "40" };
 
   /** Font name chooser. */
   private final BaseXList font;
