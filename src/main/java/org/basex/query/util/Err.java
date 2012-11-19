390b98bf691cb0cfe4dad1e2b150dd9bbd5430d3
Christian Grün <christian.gruen@gmail.com>
2012-11-19 22:46:11 +0100

    [MOD] Errors from session module moved out of core package.

--- src/main/java/org/basex/query/util/Err.java
+++ src/main/java/org/basex/query/util/Err.java
@@ -129,17 +129,6 @@ public enum Err {
   /** BXRE0011. */
   BXRE_JARFAIL(BXRE, 11, "Reading JAR descriptor failed: %."),
 
-  // Session module
-
-  /** BXSE0001. */
-  BXSE_FITEM(BXSE, 1, "Function items cannot be stored in sessions."),
-  /** BXSE0002. */
-  BXSE_GET(BXSE, 2, "Stored attribute cannot be retrieved: %"),
-  /** BXSE0003. */
-  BXSE_SESS(BXSE, 3, "Servlet context required."),
-  /** BXSE0004. */
-  BXSE_WHICH(BXSE, 4, "Session not found: %"),
-
   // SQL module
 
   /** BXSQ0001. */
