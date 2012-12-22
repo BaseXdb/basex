10d8a0d08717645baff8596436295781f7ab7550
Christian Grün <christian.gruen@gmail.com>
2012-12-22 17:03:26 +0100

    [FIX] HTML Options: public declaration of HTMLProp class

--- src/main/java/org/basex/build/file/HTMLProp.java
+++ src/main/java/org/basex/build/file/HTMLProp.java
@@ -10,7 +10,7 @@ import org.basex.core.*;
  * @author BaseX Team 2005-12, BSD License
  * @author Christian Gruen
  */
-final class HTMLProp extends AProp {
+public final class HTMLProp extends AProp {
   /** TagSoup option: html. */
   public static final Object[] HTML = { "html", false };
   /** TagSoup option: omit-xml-declaration. */
