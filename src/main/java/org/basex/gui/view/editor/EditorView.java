c985b6a58f5e83ce3d0de2c82ccd9d43fbed83a6
Christian Grün <christian.gruen@gmail.com>
2013-02-02 21:26:21 +0100

    [MIN] GUI, Editor: use WORKPATH for "Save As..." command

--- src/main/java/org/basex/gui/view/editor/EditorView.java
+++ src/main/java/org/basex/gui/view/editor/EditorView.java
@@ -306,7 +306,8 @@ public final class EditorView extends View {
   public boolean saveAs() {
     // open file chooser for XML creation
     final EditorArea edit = getEditor();
-    final String path = edit.opened() ? edit.file.path() : "";
+    final String path = edit.opened() ? edit.file.path() :
+      gui.gprop.get(GUIProp.WORKPATH);
     final BaseXFileChooser fc = new BaseXFileChooser(SAVE_AS, path, gui);
     fc.filter(XQUERY_FILES, IO.XQSUFFIXES);
     fc.filter(BXS_FILES, IO.BXSSUFFIX);
