d74a94b7f8397e90f74f52e3209990f6102941dd
Christian Gruen <christian.gruen@gmail.com>
2013-06-15 16:15:21 +0200

    [FIX] Server: delay ping when stopping server

--- src/main/java/org/basex/BaseXServer.java
+++ src/main/java/org/basex/BaseXServer.java
@@ -330,8 +330,8 @@ public final class BaseXServer extends Main implements Runnable {
       stop.touch();
       new Socket(LOCALHOST, eport).close();
       new Socket(LOCALHOST, port).close();
-      // check if server was really stopped
-      while(ping(LOCALHOST, port)) Performance.sleep(50);
+      // wait and check if server was really stopped
+      do Performance.sleep(100); while(ping(LOCALHOST, port));
     } catch(final IOException ex) {
       stop.delete();
       throw ex;
