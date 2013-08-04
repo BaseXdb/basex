7a39b23d0f3173d5019f1a5109cd2fe8db9fb1dc
Leo Woerteler <leo@woerteler.de>
2013-08-04 09:38:53 +0200

    [FIX] XQuery, GH-738: Variable uses not counted correctly in `case`.

--- src/test/java/org/basex/test/query/ast/InlineTest.java
+++ src/test/java/org/basex/test/query/ast/InlineTest.java
@@ -18,4 +18,23 @@ public class InlineTest extends QueryPlanTest {
 
     check("let $x := <x>21</x> return $x + 21", "42", "exists(//GFLWOR)");
   }
+
+  /** Tests if variable uses in {@link Switch} are counted correctly. */
+  @Test public void switchTest() {
+    // all paths use $x only once
+    check("let $x := 1 ! 42 return switch(42) case 23 return $x case 84 return $x" +
+        " case $x return 123 default return 1337", "123", "empty(//GFLWOR)");
+    // $x is used twice
+    check("let $x := 1 ! 42 return switch(23) case $x return 123 case 23 return $x" +
+        " default return 1337", "42", "exists(//GFLWOR)");
+  }
+
+  /** Regression test for Issue GH-738, "switch with contains". */
+  @Test public void gh738() {
+    check("let $item:=<item>blah blah</item> " +
+        "let $type:= switch (fn:true())" +
+        "  case $item contains text \"blah\" return <type>a</type>" +
+        "  default return ()" +
+        "return $type", "<type>a</type>", "count(//Let) = 2");
+  }
 }
