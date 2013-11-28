3c3d289948c45cc021afa7362aa5d5ec4d69ee1f
Christian Gruen <christian.gruen@gmail.com>
2013-11-28 13:10:25 +0100

    [MIN] GUI, Editor, Project View: mix native and custom appearance

--- basex-core/src/main/java/org/basex/gui/view/project/ProjectCellRenderer.java
+++ basex-core/src/main/java/org/basex/gui/view/project/ProjectCellRenderer.java
@@ -4,10 +4,12 @@ import java.awt.*;
 import java.util.*;
 
 import javax.swing.*;
+import javax.swing.filechooser.*;
 import javax.swing.tree.*;
 
 import org.basex.gui.layout.*;
 import org.basex.io.*;
+import org.basex.util.*;
 
 /**
  * Custom tree cell renderer to distinguish between raw and xml leaf nodes.
@@ -16,14 +18,22 @@ import org.basex.io.*;
  * @author Christian Gruen
  */
 final class ProjectCellRenderer extends DefaultTreeCellRenderer {
-  /** File chooser (used for retrieving system icons). */
-  private static final JFileChooser CHOOSER = new JFileChooser();
   /** Icon cache. */
   private static final HashMap<String, Icon> ICONS = new HashMap<String, Icon>();
+  /** System icons. */
+  private static final FileSystemView FS = FileSystemView.getFileSystemView();
   /** Icon for closed directories. */
   private static final Icon DIR1 = BaseXLayout.icon("file-dir1");
   /** Icon for opened directories. */
   private static final Icon DIR2 = BaseXLayout.icon("file-dir2");
+  /** Icon for textual files. */
+  private static final Icon TEXT = BaseXLayout.icon("file-text");
+  /** Icon for XML/XQuery file types. */
+  private static final Icon XML = BaseXLayout.icon("file-xml");
+  /** Icon for XML/XQuery file types. */
+  private static final Icon XQUERY = BaseXLayout.icon("file-xquery");
+  /** Icon for BaseX file types. */
+  private static final Icon BASEX = BaseXLayout.icon("file-basex");
   /** Icon for unknown file types. */
   private static final Icon UNKNOWN = BaseXLayout.icon("file-unknown");
 
@@ -54,15 +64,27 @@ final class ProjectCellRenderer extends DefaultTreeCellRenderer {
    */
   static Icon fileIcon(final IOFile file) {
     if(file == null) return UNKNOWN;
+
+    // fallback code for displaying icons
     final String path = file.path();
-    final int i = path.lastIndexOf(path, '.');
-    final String suffix = i != -1 ? path.substring(i + 1) : null;
-    Icon icon = null;
-    if(suffix != null) icon = ICONS.get(suffix);
-    if(icon == null) {
-      icon = CHOOSER.getIcon(file.file());
-      if(suffix != null) ICONS.put(suffix, icon);
+    final String mime = MimeTypes.get(path);
+    if(MimeTypes.isXML(mime)) return XML;
+    if(MimeTypes.isXQuery(mime)) return XQUERY;
+    if(path.contains(IO.BASEXSUFFIX)) return BASEX;
+
+    if(Prop.WIN) {
+      // retrieve system icons (only supported on Windows)
+      final int p = path.lastIndexOf(path, '.');
+      final String suffix = p != -1 ? path.substring(p + 1) : null;
+      Icon icon = null;
+      if(suffix != null) icon = ICONS.get(suffix);
+      if(icon == null) {
+        icon = FS.getSystemIcon(file.file());
+        if(suffix != null) ICONS.put(suffix, icon);
+      }
+      return icon;
     }
-    return icon;
+    // default icon chooser
+    return MimeTypes.isText(mime) ? TEXT : UNKNOWN;
   }
 }
