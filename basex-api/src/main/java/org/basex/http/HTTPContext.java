49f0c132b5c0781da060b2e6a647eedb1e93710e
Christian Gruen <christian.gruen@gmail.com>
2014-01-20 12:36:08 +0100

    [FIX] HTTP Services: correct handling of "Accept" header.

--- basex-api/src/main/java/org/basex/http/HTTPContext.java
+++ basex-api/src/main/java/org/basex/http/HTTPContext.java
@@ -205,7 +205,10 @@ public final class HTTPContext {
    * @return database
    */
   public String[] produces() {
-    final String[] acc = req.getHeader("Accept").split("\\s*,\\s*");
+    final String accept = req.getHeader("Accept");
+    if(accept == null) return new String[0];
+
+    final String[] acc = accept.split("\\s*,\\s*");
     final int as = acc.length;
     for(int a = 0; a < as; a++) {
       if(acc[a].indexOf(';') != -1) acc[a] = acc[a].replaceAll("\\w*;.*", "");
