c26b8c8449e4d1fc5286bf9d892ae47d06ebdade
Christian Gruen <christian.gruen@gmail.com>
2014-02-12 16:12:36 +0100

    VERSION 7.8 (February 12, 2014)
     GUI
     - new project view for organizing and opening project files
     - realtime search of project files and contents
     - various new editor shortcuts and code completions, code formatting
    
     UPDATES
     - improved performance of delete and insert operations
     - faster in-place value updates
     - 'update': convenience keyword for transform expressions
    
     XQUERY OPTIMIZATIONS
     - XQuery functions are now inlined and further optimized
     - closure optimizations, better static typing
     - improved detection and rewriting of tail calls
     - faster processing of (sub)sequences
    
     XQUERY MODULES
     - JSON and CSV Module: updated and enhanced conversion rules
     - Unit Module: enhanced test report output
     - Map Module: map:serialize added; syntax aligned with latest spec.
     - File Module: aligned with changes in EXPath spec.
     - XQuery Module: xquery:evaluate (operates on opened databases)
     - Full-Text Module: ft:contains added, ft:search: more options
     - EXPath Binary Module added
     - Java modules: support for locking annotations
    
     API
     - New options: INLINELIMIT, TAILCALLS, DEFAULTDB, RUNQUERY
     - New command-line flag: -R only parse query
     - Russian translation added (thanks to Oleksandr Shpak!)
     - Spanish translation added (thanks to Carlos Marcos!)

--- basex-core/src/main/java/org/basex/util/Prop.java
+++ basex-core/src/main/java/org/basex/util/Prop.java
@@ -40,7 +40,7 @@ public final class Prop {
   /** Project name. */
   public static final String NAME = "BaseX";
   /** Code version (may contain major, minor and optional patch number). */
-  public static final String VERSION = version("7.8 RC3");
+  public static final String VERSION = version("7.8");
   /** Main author. */
   public static final String AUTHOR = "Christian Gr\u00FCn";
   /** Co-authors (1). */
