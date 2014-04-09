d6f7868458b2e61626bedd18c74746ea3294271f
Christian Gruen <christian.gruen@gmail.com>
2014-04-09 20:11:48 +0200

    [FIX] RESTXQ: returning instead of raising error

--- basex-api/src/main/java/org/basex/http/restxq/RestXqModules.java
+++ basex-api/src/main/java/org/basex/http/restxq/RestXqModules.java
@@ -77,8 +77,9 @@ public final class RestXqModules {
           if(first.compareTo(rxf) != 0) break;
           tb.add(Prop.NL).add(rxf.function.info.toString());
         }
-        if(first.path != null) first.error(PATH_CONFLICT, first.path, tb);
-        first.error(ERROR_CONFLICT, first.error, tb);
+        throw first.path == null ?
+          first.error(ERROR_CONFLICT, first.error, tb) :
+          first.error(PATH_CONFLICT, first.path, tb);
       }
     }
     // choose most specific function
