1e3567ad37378ca9f632aad9cef140127ad5c72f
Christian Gruen <christian.gruen@gmail.com>
2014-04-29 19:07:27 +0200

    [FIX] HTTP Module: empty response messages

--- basex-core/src/main/java/org/basex/query/util/http/HTTPClient.java
+++ basex-core/src/main/java/org/basex/query/util/http/HTTPClient.java
@@ -201,21 +201,21 @@ public final class HTTPClient {
   }
 
   /**
-   * Writes the payload of a body or part in the output stream of the
-   * connection.
+   * Writes the payload of a body or part in the output stream of the connection.
    * @param payload body/part payload
    * @param payloadAtts payload attributes
    * @param out output stream
    * @throws IOException I/O exception
    */
   private static void writePayload(final ValueBuilder payload, final TokenMap payloadAtts,
-                                   final OutputStream out) throws IOException {
+      final OutputStream out) throws IOException {
 
     // detect method (specified by @method or derived from @media-type)
     final byte[] m = payloadAtts.get(METHOD);
     final String method;
     if(m == null) {
-      final String type = string(payloadAtts.get(MEDIA_TYPE));
+      final byte[] tp = payloadAtts.get(MEDIA_TYPE);
+      final String type = tp == null ? "" : string(tp);
       if(eq(type, APP_HTML_XML)) {
         method = SerialMethod.XHTML.toString();
       } else if(eq(type, TEXT_HTML)) {
