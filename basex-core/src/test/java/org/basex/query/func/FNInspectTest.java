7718afb51140549dbf3810ab25445ce1fcbacde4
Christian Gruen <christian.gruen@gmail.com>
2014-05-19 15:55:51 +0200

    [ADD] XQuery: inspect:functions(uri). Closes #872

--- basex-core/src/test/java/org/basex/query/func/FNInspectTest.java
+++ basex-core/src/test/java/org/basex/query/func/FNInspectTest.java
@@ -99,8 +99,10 @@ public final class FNInspectTest extends AdvancedQueryTest {
     query("declare function local:x() { 2 }; " +
         _INSPECT_FUNCTIONS.args() + "()", "2");
     query("import module namespace hello='world' at 'src/test/resources/hello.xqm';" +
-        "inspect:functions()[last()] instance of function(*)",
-        "true"
-        );
+        "inspect:functions()[last()] instance of function(*)", "true");
+
+    query("for $f in " + _INSPECT_FUNCTIONS.args("src/test/resources/hello.xqm")
+        + "where local-name-from-QName(function-name($f)) = 'world' "
+        + "return $f()", "hello world");
   }
 }
