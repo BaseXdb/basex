fa67d2cbafcc9608f2d812d2dea2da101958317e
Christian Gruen <christian.gruen@gmail.com>
2014-05-21 14:31:46 +0200

    [MOD] Switching to 7.9 beta

--- basex-core/src/main/java/org/basex/util/Prop.java
+++ basex-core/src/main/java/org/basex/util/Prop.java
@@ -40,7 +40,7 @@ public final class Prop {
   /** Project name. */
   public static final String NAME = "BaseX";
   /** Code version (may contain major, minor and optional patch number). */
-  public static final String VERSION = version("7.8.3 beta");
+  public static final String VERSION = version("7.9 beta");
   /** Main author. */
   public static final String AUTHOR = "Christian Gr\u00FCn";
   /** Co-authors (1). */
