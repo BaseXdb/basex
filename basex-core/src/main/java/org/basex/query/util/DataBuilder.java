963cad17ec1a2fde29476ea3a3718affc53a4c0e
Christian Gruen <christian.gruen@gmail.com>
2014-05-25 18:40:51 +0200

    [FIX] XQuery: adding attributes with namespaces. Closes #956.

--- basex-core/src/main/java/org/basex/query/util/DataBuilder.java
+++ basex-core/src/main/java/org/basex/query/util/DataBuilder.java
@@ -122,7 +122,7 @@ public final class DataBuilder {
     final int n = data.atnindex.index(q.string(), null, false);
     // usually, attributes don't have a namespace flag.
     // this is different here, because a stand-alone attribute has no parent element.
-    data.attr(ds, pre - par, n, node.string(), u, u != 0);
+    data.attr(ds, pre - par, n, node.string(), u, par == -1 && u != 0);
     data.insert(ds);
     return pre + 1;
   }
