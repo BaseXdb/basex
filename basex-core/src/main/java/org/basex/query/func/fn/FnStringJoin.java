01111e2992fbb0113e29d988e9310e967c3f3e2e
Christian Gruen <christian.gruen@gmail.com>
2014-08-12 19:24:10 +0200

    [MIN] XQuery, string-join optimizations

--- basex-core/src/main/java/org/basex/query/func/fn/FnStringJoin.java
+++ basex-core/src/main/java/org/basex/query/func/fn/FnStringJoin.java
@@ -16,15 +16,17 @@ import org.basex.util.*;
  */
 public final class FnStringJoin extends StandardFunc {
   @Override
-  public Item item(final QueryContext qc, final InputInfo ii) throws QueryException {
-    final byte[] sep = exprs.length == 2 ? toToken(exprs[1], qc) : EMPTY;
-    final TokenBuilder tb = new TokenBuilder();
+  public Str item(final QueryContext qc, final InputInfo ii) throws QueryException {
     final Iter iter = exprs[0].atomIter(qc, info);
-    int c = 0;
-    for(Item it; (it = iter.next()) != null;) {
-      if(c++ != 0) tb.add(sep);
-      tb.add(toToken(it));
-    }
+    final byte[] token = exprs.length == 2 ? toToken(exprs[1], qc) : EMPTY;
+    // no results: empty string
+    Item it = iter.next();
+    if(it == null) return Str.ZERO;
+    // single result
+    if((it = iter.next()) == null) return Str.get(toToken(it));
+    // join multiple strings
+    final TokenBuilder tb = new TokenBuilder(toToken(it));
+    do tb.add(token).add(toToken(it)); while((it = iter.next()) != null);
     return Str.get(tb.finish());
   }
 
