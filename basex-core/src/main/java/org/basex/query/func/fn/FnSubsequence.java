88b8e8bcad2f4fa1ca9b3fcc1326b79666c0b3c9
Christian Gruen <christian.gruen@gmail.com>
2014-11-22 11:33:58 +0100

    [MOD] XQuery, subsequence: value and iter methods unified

--- basex-core/src/main/java/org/basex/query/func/fn/FnSubsequence.java
+++ basex-core/src/main/java/org/basex/query/func/fn/FnSubsequence.java
@@ -18,40 +18,27 @@ import org.basex.query.var.*;
  * @author Christian Gruen
  */
 public final class FnSubsequence extends StandardFunc {
+  /** Return all values. */
+  private static final long[] ALL = {};
+
   @Override
   public Iter iter(final QueryContext qc) throws QueryException {
-    final double ds = toDouble(exprs[1], qc);
-    if(Double.isNaN(ds)) return Empty.ITER;
-    final long s = StrictMath.round(ds);
-    final boolean si = s == Long.MIN_VALUE;
-
-    long l = Long.MAX_VALUE;
-    if(exprs.length > 2) {
-      final double dl = toDouble(exprs[2], qc);
-      if(Double.isNaN(dl)) return Empty.ITER;
-      if(si && dl == Double.POSITIVE_INFINITY) return Empty.ITER;
-      l = StrictMath.round(dl);
-    }
-    final boolean linf = l == Long.MAX_VALUE;
-    if(si) return linf ? exprs[0].iter(qc) : Empty.ITER;
+    final long[] range = range(qc);
+    if(range == null) return Empty.ITER;
+    if(range == ALL) return exprs[0].iter(qc);
+    final long start = range[0], len = range[1];
 
     // optimization: return subsequence
     final Iter iter = qc.iter(exprs[0]);
-    if(iter instanceof ValueIter) {
-      final Value val = iter.value();
-      final long rs = val.size();
-      final long from = Math.max(1, s) - 1;
-      final long len = Math.min(rs - from, l + Math.min(0, s - 1));
-      return SubSeq.get(val, from, len).iter();
-    }
-    final long max = iter.size();
-    final long e = linf ? l : s + l;
+    if(iter instanceof ValueIter) return sub((ValueIter) iter, start, len).iter();
 
     // fast route if the size is known
+    final long max = iter.size();
+    final long e = len == Long.MAX_VALUE ? len : start + len;
     if(max >= 0) return new Iter() {
       // directly access specified items
       final long m = Math.min(e, max + 1);
-      long c = Math.max(1, s);
+      long c = Math.max(1, start);
       @Override
       public Item next() throws QueryException { return c < m ? iter.get(c++ - 1) : null; }
       @Override
@@ -66,9 +53,9 @@ public final class FnSubsequence extends StandardFunc {
       @Override
       public Item next() throws QueryException {
         while(true) {
-          final Item i = iter.next();
-          if(i == null || ++c >= e) return null;
-          if(c >= s) return i;
+          final Item it = iter.next();
+          if(it == null || ++c >= e) return null;
+          if(c >= start) return it;
         }
       }
     };
@@ -76,49 +63,71 @@ public final class FnSubsequence extends StandardFunc {
 
   @Override
   public Value value(final QueryContext qc) throws QueryException {
-    final double dstart = toDouble(exprs[1], qc);
-    if(Double.isNaN(dstart)) return Empty.SEQ;
-    final long start = StrictMath.round(dstart);
-    final boolean sinf = start == Long.MIN_VALUE;
-
-    long l = Long.MAX_VALUE;
-    if(exprs.length > 2) {
-      final double dlength = toDouble(exprs[2], qc);
-      if(Double.isNaN(dlength)) return Empty.SEQ;
-      if(sinf && dlength == Double.POSITIVE_INFINITY) return Empty.SEQ;
-      l = StrictMath.round(dlength);
-    }
-    final boolean linf = l == Long.MAX_VALUE;
-    if(sinf) return linf ? exprs[0].value(qc) : Empty.SEQ;
+    final long[] range = range(qc);
+    if(range == null) return Empty.SEQ;
+    if(range == ALL) return exprs[0].value(qc);
+    final long start = range[0], len = range[1];
 
     // optimization: return subsequence
     final Iter iter = qc.iter(exprs[0]);
-    if(iter instanceof ValueIter) {
-      final Value val = iter.value();
-      final long rs = val.size();
-      final long from = Math.max(1, start) - 1;
-      final long len = Math.min(rs - from, l + Math.min(0, start - 1));
-      return SubSeq.get(val, from, len);
-    }
+    if(iter instanceof ValueIter) return sub((ValueIter) iter, start, len);
 
     // fast route if the size is known
     final long max = iter.size();
     if(max >= 0) {
-      final long from = Math.max(1, start) - 1;
-      final long len = Math.min(max - from, l + Math.min(0, start - 1));
-      if(from >= max || len <= 0) return Empty.SEQ;
-      final ValueBuilder vb = new ValueBuilder(Math.max((int) len, 1));
-      for(long i = 0; i < len; i++) vb.add(iter.get(from + i));
+      final long s = Math.max(1, start) - 1;
+      final long l = Math.min(max - s, len + Math.min(0, start - 1));
+      if(s >= max || l <= 0) return Empty.SEQ;
+      final ValueBuilder vb = new ValueBuilder(Math.max((int) l, 1));
+      for(long i = 0; i < l; i++) vb.add(iter.get(s + i));
       return vb.value();
     }
 
-    final long e = linf ? l : start + l;
-    final ValueBuilder build = new ValueBuilder();
-    Item i;
-    for(int c = 1; c < e && (i = iter.next()) != null; c++) {
-      if(c >= start) build.add(i);
+    final long e = len == Long.MAX_VALUE ? len : start + len;
+    final ValueBuilder vb = new ValueBuilder();
+    Item it;
+    for(int c = 1; c < e && (it = iter.next()) != null; c++) {
+      if(c >= start) vb.add(it);
     }
-    return build.value();
+    return vb.value();
+  }
+
+  /**
+   * Returns the start position and length of the requested sub sequence.
+   * @param qc query context
+   * @return range
+   * @throws QueryException query exception
+   */
+  private long[] range(final QueryContext qc) throws QueryException {
+    final double ds = toDouble(exprs[1], qc);
+    if(Double.isNaN(ds)) return null;
+
+    final long start = StrictMath.round(ds);
+    final boolean min = start == Long.MIN_VALUE;
+    long len = Long.MAX_VALUE;
+
+    if(exprs.length > 2) {
+      final double dl = toDouble(exprs[2], qc);
+      if(Double.isNaN(dl)) return null;
+      if(min && dl == Double.POSITIVE_INFINITY) return null;
+      len = StrictMath.round(dl);
+    }
+    return min ? len == Long.MAX_VALUE ? ALL : null : new long[] { start, len };
+  }
+
+
+  /**
+   * Returns a subsequence.
+   * @param iter iterator
+   * @param start start position
+   * @param len length
+   * @return sub sequence
+   */
+  private static Value sub(final ValueIter iter, final long start, final long len) {
+    final Value val = iter.value();
+    final long s = Math.max(1, start) - 1;
+    final long l = Math.min(val.size() - s, len + Math.min(0, start - 1));
+    return SubSeq.get(val, s, l);
   }
 
   @Override
