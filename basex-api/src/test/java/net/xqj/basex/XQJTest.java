345bc121f51c3e9f07c8b99e964b3630a5df6369
Christian Gruen <christian.gruen@gmail.com>
2015-01-23 14:08:12 +0100

    [MOD] XQJ: New test for client connection added

---
+++ basex-api/src/test/java/net/xqj/basex/XQJTest.java
@@ -0,0 +1,64 @@
+package net.xqj.basex;
+
+import static org.junit.Assert.*;
+
+import java.io.*;
+
+import javax.xml.namespace.*;
+import javax.xml.xquery.*;
+
+import org.basex.*;
+import org.junit.*;
+
+/**
+ * Performs simple tests on Charles Forster's XQJ client implementation for BaseX.
+ *
+ * @author BaseX Team 2005-15, BSD License
+ * @author Christian Gruen
+ */
+public final class XQJTest extends SandboxTest {
+  /** Server reference. */
+  private static BaseXServer server;
+
+  /**
+   * Starts the tests.
+   * @throws IOException I/O exception
+   */
+  @BeforeClass
+  public static void before() throws IOException {
+    server = createServer();
+  }
+
+  /**
+   * Finishes the tests.
+   * @throws Exception exception
+   */
+  @AfterClass
+  public static void after() throws Exception {
+    stopServer(server);
+  }
+
+  /**
+   * Simple query.
+   * @throws Exception exception
+   */
+  @Test
+  public void basicTest() throws Exception {
+    XQDataSource xqds = new BaseXXQDataSource();
+    xqds.setProperty("serverName", "localhost");
+    xqds.setProperty("port", "9999");
+
+    XQConnection conn = xqds.getConnection("admin", "admin");
+    try {
+      XQPreparedExpression xqpe =
+          conn.prepareExpression("declare variable $x as xs:string external; $x");
+      xqpe.bindString(new QName("x"), "Hello World!", null);
+
+      XQResultSequence rs = xqpe.executeQuery();
+      assertTrue(rs.next());
+      assertEquals(rs.getItemAsString(null), "Hello World!");
+    } finally {
+      conn.close();
+    }
+  }
+}
