b39b77daf1f90f776f034a6737d0e834e7323dda
Christian Gruen <christian.gruen@gmail.com>
2015-01-25 12:55:00 +0100

    [MOD] XQuery, Hashing Module: input is now streamed

--- basex-core/src/main/java/org/basex/query/func/hash/HashFn.java
+++ basex-core/src/main/java/org/basex/query/func/hash/HashFn.java
@@ -2,8 +2,11 @@ package org.basex.query.func.hash;
 
 import static org.basex.query.QueryError.*;
 
+import java.io.*;
 import java.security.*;
 
+import org.basex.io.*;
+import org.basex.io.in.*;
 import org.basex.query.*;
 import org.basex.query.func.*;
 import org.basex.query.value.item.*;
@@ -23,8 +26,23 @@ abstract class HashFn extends StandardFunc {
    * @throws QueryException exception
    */
   final B64 hash(final String algo, final QueryContext qc) throws QueryException {
+    final Item item = exprs[0].atomItem(qc, info);
     try {
-      return new B64(MessageDigest.getInstance(algo).digest(toBinary(exprs[0], qc)));
+      final MessageDigest md = MessageDigest.getInstance(algo);
+      if(item instanceof B64Stream) {
+        try(final BufferInput bi = item.input(info)) {
+          final byte[] tmp = new byte[IO.BLOCKSIZE];
+          do {
+            final int n = bi.read(tmp);
+            if(n == -1) return new B64(md.digest());
+            md.update(tmp, 0, n);
+          } while(true);
+        } catch(final IOException ex) {
+          throw FILE_IO_ERROR_X.get(info, ex);
+        }
+      }
+      // non-streaming item, string
+      return new B64(md.digest(toBytes(item)));
     } catch(final NoSuchAlgorithmException ex) {
       throw HASH_ALG_X.get(info, algo);
     }
