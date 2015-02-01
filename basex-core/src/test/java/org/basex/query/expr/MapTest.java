abe6fb93e88631c58762ac8076e64f6dcbbd06bf
Christian Gruen <christian.gruen@gmail.com>
2015-02-01 16:25:21 +0100

    [MIN] XQuery, test cases updated

--- basex-core/src/test/java/org/basex/query/expr/MapTest.java
+++ basex-core/src/test/java/org/basex/query/expr/MapTest.java
@@ -32,17 +32,18 @@ public final class MapTest extends AdvancedQueryTest {
     error(" map{ xs:time('01:01:01'):1, xs:time('01:01:01'):1 }", MAPDUPLKEY_X_X_X);
 
     query(_MAP_SIZE.args(" map{ xs:time('01:01:01'):1, xs:time('02:02:02'):2 }"), "2");
+    query("let $k1 := xs:time('01:01:01')"
+        + "let $k2 := xs:time('01:01:02+01:00')"
+        + "let $m := map { $k1:1 }"
+        + "return map:put(map:remove($m, $k1), $k2, 2)($k2)", "2");
+
     error(" map{ xs:time('01:01:01'):1, xs:time('01:01:02+01:00'):2 }", MAPTZ);
     error(" map{ xs:time('01:01:01'):1, xs:time('01:01:02+01:00'):2 }", MAPTZ);
 
     error("let $k1 := xs:time('01:01:01')"
         + "let $k2 := xs:dateTime('2001-01-01T01:01:01+01:00')"
         + "let $m := map { $k1:1 }"
-        + "return map:put($m, $k2, 2)($k2)", MAP_TZ);
-    query("let $k1 := xs:time('01:01:01')"
-        + "let $k2 := xs:time('01:01:02+01:00')"
-        + "let $m := map { $k1:1 }"
-        + "return map:put(map:remove($m, $k1), $k2, 2)($k2)", "2");
+        + "return map:put($m, $k2, 2)", MAP_TZ);
     error("let $k1 := xs:time('01:01:01')"
         + "let $k2 := xs:time('01:01:02')"
         + "let $k3 := xs:time('01:01:03+01:00')"
