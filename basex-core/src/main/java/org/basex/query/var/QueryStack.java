45f64b2eb9c757ce2b94696cfb1858e732b0daf7
Christian Gruen <christian.gruen@gmail.com>
2015-03-11 17:41:58 +0100

    [FIX] XQuery: prof:variables; speeding up frame access

--- basex-core/src/main/java/org/basex/query/var/QueryStack.java
+++ basex-core/src/main/java/org/basex/query/var/QueryStack.java
@@ -18,9 +18,9 @@ public final class QueryStack {
   /** The currently assigned variables. */
   private Var[] vars = new Var[INIT];
   /** The frame pointer, marking the start of the current stack frame. */
-  private int fp;
+  private int start;
   /** The stack limit, marking the end of the current stack frame. */
-  private int sl;
+  private int end;
 
   /**
    * Enters a new stack frame.
@@ -28,39 +28,41 @@ public final class QueryStack {
    * @return stack pointer of the old frame
    */
   public int enterFrame(final int size) {
-    final int nsl = sl + size;
-    ensureCapacity(nsl);
-    final int ret = fp;
-    fp = sl;
-    sl = nsl;
-    return ret;
+    final int e = end, ne = e + size;
+    ensureCapacity(ne);
+    final int s = start;
+    start = e;
+    end = ne;
+    return s;
   }
 
   /**
    * Prepares the current stack frame to be reused.
-   * @param newFrameSize new frame size
+   * @param size new frame size
    */
-  public void reuseFrame(final int newFrameSize) {
-    ensureCapacity(fp + newFrameSize);
-    while(--sl >= fp) stack[sl] = null;
-    sl = fp + newFrameSize;
+  public void reuseFrame(final int size) {
+    final int s = start;
+    ensureCapacity(s + size);
+    final Value[] stck = stack;
+    for(int e = end; --e >= s;) stck[e] = null;
+    end = s + size;
   }
 
   /**
    * Exits a stack frame and makes all bound variables eligible for garbage collection.
-   * @param fpt frame pointer of the underlying stack frame
+   * @param frame frame pointer of the underlying stack frame
    */
-  public void exitFrame(final int fpt) {
-    while(--sl >= fp) stack[sl] = null;
-    sl = fp;
-    fp = fpt;
-    if(stack.length > INIT && sl <= stack.length / 4) {
-      int len = stack.length;
-      do len /= 2; while(len > INIT && sl <= len / 4);
-      final Value[] nst = new Value[len];
-      System.arraycopy(stack, 0, nst, 0, sl);
-      stack = nst;
-    }
+  public void exitFrame(final int frame) {
+    final int s = start;
+    final Value[] stck = stack;
+    for(int en = end; --en >= s;) stck[en] = null;
+    end = s;
+    start = frame;
+
+    final int sl = stck.length;
+    int len = sl;
+    while(len > INIT && sl <= len >> 2) len >>= 1;
+    if(len != sl) resize(len);
   }
 
   /**
@@ -68,13 +70,24 @@ public final class QueryStack {
    * @param newSize required size of the stack
    */
   private void ensureCapacity(final int newSize) {
-    if(newSize > stack.length) {
-      int len = stack.length;
-      do len <<= 1; while(newSize > len);
-      final Value[] nst = new Value[len];
-      System.arraycopy(stack, 0, nst, 0, sl);
-      stack = nst;
-    }
+    final int sl = stack.length;
+    int len = sl;
+    while(newSize > len) len <<= 1;
+    if(len != sl) resize(len);
+  }
+
+  /**
+   * Resizes the stacks.
+   * @param len new size
+   */
+  private void resize(final int len) {
+    final int os = end;
+    final Value[] nst = new Value[len];
+    System.arraycopy(stack, 0, nst, 0, os);
+    stack = nst;
+    final Var[] nvr = new Var[len];
+    System.arraycopy(vars, 0, nvr, 0, os);
+    vars = nvr;
   }
 
   /**
@@ -83,8 +96,8 @@ public final class QueryStack {
    * @return position
    */
   private int pos(final Var var) {
-    final int pos = fp + var.slot;
-    if(pos < fp || sl <= pos) throw Util.notExpected(var);
+    final int pos = start + var.slot;
+    if(pos < start || end <= pos) throw Util.notExpected(var);
     return pos;
   }
 
@@ -118,9 +131,9 @@ public final class QueryStack {
    */
   public String dump() {
     final StringBuilder sb = new StringBuilder(QueryText.DEBUGLOCAL + ':');
-    for(int i = sl - 1; i >= 0; i--) {
+    for(int i = end - 1; i >= 0; i--) {
       sb.append(Prop.NL).append("  $").append(vars[i].name).append(" := ").append(stack[i]);
-      if(i == fp && i > 0) sb.append(Prop.NL).append(QueryText.DEBUGGLOBAL + ':');
+      if(i == start && i > 0) sb.append(Prop.NL).append(QueryText.DEBUGGLOBAL + ':');
     }
     return sb.toString();
   }
@@ -128,7 +141,7 @@ public final class QueryStack {
   @Override
   public String toString() {
     final StringBuilder sb = new StringBuilder(Util.className(this)).append('[');
-    for(int i = 0; i < sl; i++) sb.append(i == 0 ? "" : ", ").append(stack[i]);
+    for(int i = 0; i < end; i++) sb.append(i == 0 ? "" : ", ").append(stack[i]);
     return sb.append(']').toString();
   }
 }
