0f9179fe2920deae9be5fa1c35fce92add7e6f25
Christian Gruen <christian.gruen@gmail.com>
2015-03-26 17:14:53 +0100

    [MIN] MimeTypes readded for legacy purposes

---
+++ basex-core/src/main/java/org/basex/io/MimeTypes.java
@@ -0,0 +1,26 @@
+package org.basex.io;
+
+import org.basex.util.http.*;
+
+/**
+ * Obsolete class for retrieving mime types (new: {@link MediaType}).
+ * Will be removed in Version 8.2 or later.
+ *
+ * @author BaseX Team 2005-15, BSD License
+ * @author Christian Gruen
+ */
+public final class MimeTypes {
+  /** Private constructor. */
+  private MimeTypes() { }
+
+  /**
+   * Returns the mime type for the suffix of the specified file path.
+   * {@code application/octet-stream} is returned if no type is found.
+   * @param path path to be checked
+   * @return mime-type
+   */
+  @Deprecated
+  public static String get(final String path) {
+    return MediaType.get(path).toString();
+  }
+}
