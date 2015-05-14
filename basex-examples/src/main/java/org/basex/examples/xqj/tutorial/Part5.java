ed8703c952751bf5f113578df568296bde296a82
Christian Gruen <christian.gruen@gmail.com>
2015-05-14 14:17:54 +0200

    [MIN] Link updated.

--- basex-examples/src/main/java/org/basex/examples/xqj/tutorial/Part5.java
+++ basex-examples/src/main/java/org/basex/examples/xqj/tutorial/Part5.java
@@ -6,10 +6,9 @@ import java.util.*;
 import javax.xml.xquery.*;
 
 /**
- * XQJ Example, derived from the XQJ Tutorial
- * <a href="http://www.xquery.com/tutorials/xqj_tutorial">
- * http://www.xquery.com/tutorials/xqj_tutorial</a>
- * from Marc van Cappellen.
+ * XQJ Examples, derived from an
+ * <a href="https://www.progress.com/products/data-integration-suite/data-integration-suite-developer-center/data-integration-suite-tutorials/learning-xquery/introduction-to-the-xquery-api-for-java-xqj-">
+ * XQJ online tutorial</a>.
  *
  * Part 5: Serializing Results.
  *
