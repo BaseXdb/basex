2cc9cc31fdbd59e06cb05129a264eab67a871d31
Christian Gruen <christian.gruen@gmail.com>
2015-05-27 21:29:09 +0200

    [FIX] HTTP: splitting session and servlet listener

--- basex-api/src/main/java/org/basex/http/ServletListener.java
+++ basex-api/src/main/java/org/basex/http/ServletListener.java
@@ -1,9 +1,6 @@
 package org.basex.http;
 
-import java.util.*;
-
 import javax.servlet.*;
-import javax.servlet.http.*;
 
 /**
  * This class creates and destroys HTTP sessions and servlet contexts.
@@ -11,30 +8,7 @@ import javax.servlet.http.*;
  * @author BaseX Team 2005-15, BSD License
  * @author Christian Gruen
  */
-public final class ServletListener implements HttpSessionListener, ServletContextListener {
-  /** Sessions. */
-  private static HashMap<String, HttpSession> sessions;
-
-  @Override
-  public void sessionCreated(final HttpSessionEvent event) {
-    final HttpSession sess =  event.getSession();
-    sessions().put(sess.getId(), sess);
-  }
-
-  @Override
-  public void sessionDestroyed(final HttpSessionEvent event) {
-    sessions().remove(event.getSession().getId());
-  }
-
-  /**
-   * Initializes the HTTP context.
-   * @return context;
-   */
-  public static synchronized HashMap<String, HttpSession> sessions() {
-    if(sessions == null) sessions = new HashMap<>();
-    return sessions;
-  }
-
+public final class ServletListener implements ServletContextListener {
   @Override
   public void contextInitialized(final ServletContextEvent event) {
   }
---
+++ basex-api/src/main/java/org/basex/http/SessionListener.java
@@ -0,0 +1,36 @@
+package org.basex.http;
+
+import java.util.*;
+
+import javax.servlet.http.*;
+
+/**
+ * This class creates and destroys HTTP sessions and servlet contexts.
+ *
+ * @author BaseX Team 2005-15, BSD License
+ * @author Christian Gruen
+ */
+public final class SessionListener implements HttpSessionListener {
+  /** Sessions. */
+  private static HashMap<String, HttpSession> sessions;
+
+  @Override
+  public void sessionCreated(final HttpSessionEvent event) {
+    final HttpSession sess =  event.getSession();
+    sessions().put(sess.getId(), sess);
+  }
+
+  @Override
+  public void sessionDestroyed(final HttpSessionEvent event) {
+    sessions().remove(event.getSession().getId());
+  }
+
+  /**
+   * Initializes the HTTP context.
+   * @return context;
+   */
+  public static synchronized HashMap<String, HttpSession> sessions() {
+    if(sessions == null) sessions = new HashMap<>();
+    return sessions;
+  }
+}
