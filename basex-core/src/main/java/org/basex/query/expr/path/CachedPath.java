49351de0441d4bd8accc0312177ca398ee03b0a1
Christian Gruen <christian.gruen@gmail.com>
2015-06-18 18:56:09 +0200

    [MIN] XQuery, CachedPath: reverted (IterPath should be optimized as well)

--- basex-core/src/main/java/org/basex/query/expr/path/CachedPath.java
+++ basex-core/src/main/java/org/basex/query/expr/path/CachedPath.java
@@ -19,70 +19,52 @@ import org.basex.util.hash.*;
  * @author BaseX Team 2005-15, BSD License
  * @author Christian Gruen
  */
-public final class CachedPath extends AxisPath {
-  /** Flag for result caching. */
-  private boolean cache;
-  /** Cached results. */
-  private ANodeList clist;
-  /** Cached context value. */
-  private Value cvalue;
-
+final class CachedPath extends AxisPath {
   /**
    * Constructor.
    * @param info input info
    * @param root root expression; can be a {@code null} reference
    * @param steps axis steps
    */
-  public CachedPath(final InputInfo info, final Expr root, final Expr... steps) {
+  CachedPath(final InputInfo info, final Expr root, final Expr... steps) {
     super(info, root, steps);
-    // cache values if expression has no free variables, is deterministic and performs no updates
-    cache = root instanceof Root && !hasFreeVars() &&
-        !has(Flag.NDT) && !has(Flag.UPD) && !has(Flag.HOF);
   }
 
   @Override
   public BasicNodeIter iter(final QueryContext qc) throws QueryException {
-    final Value cv = qc.value;
-    ANodeList nl = clist;
-    if(nl == null || cv != cvalue) {
-      final long cp = qc.pos, cs = qc.size;
-      final Value r = root != null ? qc.value(root) : cv;
-      nl = new ANodeList().check();
-      try {
-        if(r != null) {
-          final Iter ir = qc.iter(r);
-          for(Item it; (it = ir.next()) != null;) {
-            // ensure that root only returns nodes
-            if(root != null && !(it instanceof ANode))
-              throw PATHNODE_X_X_X.get(info, steps[0], it.type, it);
-            qc.value = it;
-            iter(0, nl, qc);
-          }
-        } else {
-          qc.value = null;
-          iter(0, nl, qc);
+    final long cp = qc.pos, cs = qc.size;
+    final Value cv = qc.value, r = root != null ? qc.value(root) : cv;
+    final ANodeList list = new ANodeList().check();
+    try {
+      if(r != null) {
+        final Iter ir = qc.iter(r);
+        for(Item it; (it = ir.next()) != null;) {
+          // ensure that root only returns nodes
+          if(root != null && !(it instanceof ANode))
+            throw PATHNODE_X_X_X.get(info, steps[0], it.type, it);
+          qc.value = it;
+          iter(0, list, qc);
         }
-      } finally {
-        qc.value = cv;
-        qc.size = cs;
-        qc.pos = cp;
+      } else {
+        qc.value = null;
+        iter(0, list, qc);
       }
-      if(cache) {
-        clist = nl;
-        cvalue = cv;
-      }
+    } finally {
+      qc.value = cv;
+      qc.size = cs;
+      qc.pos = cp;
     }
-    return nl.iter();
+    return list.iter();
   }
 
   /**
    * Recursive step iterator.
    * @param step current step
-   * @param nl node cache
+   * @param list node cache
    * @param qc query context
    * @throws QueryException query exception
    */
-  private void iter(final int step, final ANodeList nl, final QueryContext qc)
+  private void iter(final int step, final ANodeList list, final QueryContext qc)
       throws QueryException {
 
     // cast is safe (steps will always return a {@link NodeIter} instance)
@@ -91,10 +73,10 @@ public final class CachedPath extends AxisPath {
     for(ANode node; (node = ni.next()) != null;) {
       if(more) {
         qc.value = node;
-        iter(step + 1, nl, qc);
+        iter(step + 1, list, qc);
       } else {
         qc.checkStop();
-        nl.add(node);
+        list.add(node);
       }
     }
   }
@@ -102,8 +84,6 @@ public final class CachedPath extends AxisPath {
   @Override
   public AxisPath copy(final QueryContext qc, final VarScope scp, final IntObjMap<Var> vs) {
     final Expr rt = root == null ? null : root.copy(qc, scp, vs);
-    final CachedPath ap = copyType(new CachedPath(info, rt, Arr.copyAll(qc, scp, vs, steps)));
-    ap.cache = cache;
-    return ap;
+    return copyType(new CachedPath(info, rt, Arr.copyAll(qc, scp, vs, steps)));
   }
 }
