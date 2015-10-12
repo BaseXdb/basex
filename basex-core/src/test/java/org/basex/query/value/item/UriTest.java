49a80aa19cf04e5c7c76fb5e2db2a25b37fe46db
Dimitar Popov <dimitar.georgiev.popov@gmail.com>
2015-10-12 22:40:33 +0200

    [FIX] GH-928: RFC 3986 URI parser

--- basex-core/src/test/java/org/basex/query/value/item/UriTest.java
+++ basex-core/src/test/java/org/basex/query/value/item/UriTest.java
@@ -1,8 +1,12 @@
 package org.basex.query.value.item;
 
+
 import static org.junit.Assert.*;
 
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.*;
 
 /**
  * URI tests.
@@ -10,54 +14,78 @@ import org.junit.*;
  * @author BaseX Team 2005-15, BSD License
  * @author Dimitar Popov
  */
+@RunWith(Parameterized.class)
 public class UriTest {
+
+  @Parameters(name = "{index}: \"{0}\": valid = {1}, absolute = {2}")
+  public static Object[][] sampleUris() {
+    return new Object[][] {
+      {"x:", true, true},
+      {"x", true, false},
+      {"a string", true, false},
+      {"ç.html", true, false},
+      {"http:\\\\example.com\\\\examples", true, true},
+      {"〜", true, false},
+      {" http://example.com/ foo.xml ", true, true},
+      {"http://www.w3.org/TestModules/<=>@/test", true, true},
+      {"", true, false},
+      {"//test.org:80", true, false},
+      {"//[fe80::216:ceff:fe86:3e33]", true, false},
+      {"x+y://a:b@[fe80::216:ceff:fe86:3e33]:80/p/b/c?q=1&q=2#test?123", true, true},
+      {"x+y://a:b@254.254.254.254:80/p/b254/c?q=1&q=2#test?123", true, true},
+      {"http://!$&'()*+,;=/", true, true},
+      {"http://%0Ad%E2%9C%90%F0%98%9A%A0/", true, true},
+      {"odd-scheme+1.://www.example.org/", true, true},
+      {"http://www.example.org/%0Ad%E2%9C%90%F0%98%9A%A0/", true, true},
+      {"http://www.example.org/!/$/&/'/(/)/*/+/,/;/=/", true, true},
+      {"http://www.example.org/:/@/", true, true},
+      {"http://www.example.org/?%0Ad%E2%9C%90%F0%98%9A%A0/", true, true},
+      {"http://www.example.org/?!$&'()*+,;=", true, true},
+      {"http://www.example.org/?:@", true, true},
+      {"http://www.example.org/#%0Ad%E2%9C%90%F0%98%9A%A0/", true, true},
+      {"http://www.example.org/#!$&'()*+,;=", true, true},
+      {"http://www.example.org/#:@", true, true},
+      {"ldap://[2001:db8::7]/c=GB?objectClass?one", true, true},
+      {"mailto:John.Doe@example.com", true, true},
+      {"news:comp.infosystems.www.servers.unix", true, true},
+      {"tel:+1-816-555-1212", true, true},
+      {"telnet://192.0.2.16:80/", true, true},
+      {"urn:oasis:names:specification:docbook:dtd:xml:4.1.2", true, true},
+
+      {"1:", false, false},
+      {"invalidURI%gg", false, false},
+      {"ttp:\\\\invalidURI\\someURI%gg", false, false},
+      {":/", false, false},
+      {"##invalid", false, false},
+      {"%gg", false, false},
+      {"%", false, false},
+      {"http://www.example.com/file%GF.html", false, false},
+      {":/cut.jpg", false, false},
+      {":/images/cut.png", false, false},
+      {"http:\\\\invalid%>URI\\someURI", false, false},
+      {"//z:1%40", false, false},
+      {"//x//x", true, false},
+      {"/a/../b/./c/d/.x//x", true, false},
+    };
+  }
+
+  @Parameter(0) public String uri;
+  @Parameter(1) public boolean valid;
+  @Parameter(2) public boolean absolute;
+
   /**
-   * Tests {@link Uri#isAbsolute()}.
+   * Tests for {@link Uri#isAbsolute()}.
    */
   @Test
   public void isAbsolute() {
-    assertUriIsAbsolute("x:", true);
-
-    // absolute URIs always have schema
-    assertUriIsAbsolute("x", false);
-    assertUriIsAbsolute("", false);
-    // [DP] #928
-    //assertUriIsAbsolute("//localhost:80", false);
-
-    // absolute URIs don't have fragments
-    // [DP] #928
-    //assertUriIsAbsolute("http://localhost:80/html#f", false);
+    assertEquals("Uri absolute check failed", absolute, Uri.uri(uri).isAbsolute());
   }
 
   /**
-   * Tests {@link Uri#isValid()}.
+   * Tests for {@link Uri#isValid()}.
    */
   @Test
   public void isValid() {
-    // [DP] #928
-    //assertUriIsValid("x:", true);
-    assertUriIsValid("x", true);
-    assertUriIsValid("", true);
-    assertUriIsValid("//localhost:80", true);
-  }
-
-  /**
-   * Tests if a URI is valid.
-   * @param uri uri
-   * @param expected expected value
-   */
-  private static void assertUriIsValid(final String uri, final boolean expected) {
-    assertEquals("Uri validation failed for '" + uri + '\'' + uri, expected,
-        Uri.uri(uri).isValid());
-  }
-
-  /**
-   * Tests if a URI is absolute.
-   * @param uri uri
-   * @param expected expected value
-   */
-  private static void assertUriIsAbsolute(final String uri, final boolean expected) {
-    assertEquals("Uri absolute check failed for '" + uri + '\'', expected,
-        Uri.uri(uri).isAbsolute());
+    assertEquals("Uri validation failed", valid, Uri.uri(uri).isValid());
   }
 }
