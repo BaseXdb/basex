b0602b539c106493b603303d3335da584c4fbb69
Christian Gruen <christian.gruen@gmail.com>
2016-07-26 10:28:36 +0200

    [FIX] XQuery, optimization of fulltext options. Closes #1323

---
+++ basex-core/src/test/java/org/basex/query/ft/FTQueryTest.java
@@ -0,0 +1,22 @@
+package org.basex.query.ft;
+
+import org.basex.query.*;
+import org.junit.*;
+
+/**
+ * Full-text query tests.
+ *
+ * @author BaseX Team 2005-16, BSD License
+ * @author Christian Gruen
+ */
+public final class FTQueryTest extends AdvancedQueryTest {
+  /** Checks optimizations of full-text operations. */
+  @Test
+  public void ftOptimize() {
+    query("let $a := <a>x</a> "
+        + "let $b := $a[. contains text '.*' all using wildcards] "
+        + "let $c := $b "
+        + "return $c", "<a>x</a>");
+  }
+
+}
