9a700bf1f663b18eb578acb1a8aa8bbb4d4dd707
Christian Gruen <christian.gruen@gmail.com>
2016-08-14 22:36:44 +0200

    [MOD] SQL Module: only assign autocommit if value has been assigned.

--- basex-core/src/main/java/org/basex/query/func/sql/SqlConnect.java
+++ basex-core/src/main/java/org/basex/query/func/sql/SqlConnect.java
@@ -1,6 +1,5 @@
 package org.basex.query.func.sql;
 
-import static java.sql.DriverManager.*;
 import static org.basex.query.QueryError.*;
 import static org.basex.util.Token.*;
 
@@ -21,63 +20,52 @@ import org.basex.util.options.*;
  */
 public final class SqlConnect extends SqlFn {
   /** Auto-commit mode. */
-  private static final String AUTO_COMM = "autocommit";
+  private static final String AUTOCOMMIT = "autocommit";
   /** User. */
   private static final String USER = "user";
   /** Password. */
   private static final String PASS = "password";
 
+  @SuppressWarnings("resource")
   @Override
   public Item item(final QueryContext qc, final InputInfo ii) throws QueryException {
     checkCreate(qc);
     // URL to relational database
     final String url = string(toToken(exprs[0], qc));
     final JDBCConnections jdbc = jdbc(qc);
+    final Connection conn;
     try {
       if(exprs.length > 2) {
         // credentials
         final String user = string(toToken(exprs[1], qc));
         final String pass = string(toToken(exprs[2], qc));
         if(exprs.length == 4) {
-          // connection options
-          final Options opts = toOptions(3, new Options(), qc);
-          // extract auto-commit mode from options
-          boolean ac = true;
-          final HashMap<String, String> options = opts.free();
-          final String commit = options.get(AUTO_COMM);
-          if(commit != null) {
-            ac = Strings.yes(commit);
-            options.remove(AUTO_COMM);
+          // parse connection options
+          final HashMap<String, String> options = toOptions(3, new Options(), qc).free();
+
+          // prepares connection properties
+          final Properties props = new Properties();
+          for(final Entry<String, String> entry : options.entrySet()) {
+            final String key = entry.getKey(), value = entry.getValue();
+            if(!key.equals(AUTOCOMMIT)) props.setProperty(key, value);
           }
-          // connection properties
-          final Properties props = connProps(options);
           props.setProperty(USER, user);
           props.setProperty(PASS, pass);
 
-          // open connection
-          final Connection conn = getConnection(url, props);
-          // set auto/commit mode
-          conn.setAutoCommit(ac);
-          return Int.get(jdbc.add(conn));
+          // open connection and set auto-commit mode
+          conn = DriverManager.getConnection(url, props);
+          if(options.containsKey(AUTOCOMMIT)) {
+            conn.setAutoCommit(Strings.yes(options.get(AUTOCOMMIT)));
+          }
+        } else {
+          conn = DriverManager.getConnection(url, user, pass);
         }
-        return Int.get(jdbc.add(getConnection(url, user, pass)));
+      } else {
+        conn = DriverManager.getConnection(url);
       }
-      return Int.get(jdbc.add(getConnection(url)));
+      return Int.get(jdbc.add(conn));
     } catch(final SQLException ex) {
       throw BXSQ_ERROR_X.get(info, ex);
     }
-  }
-
-  /**
-   * Parses connection options.
-   * @param options options
-   * @return connection properties
-   */
-  private static Properties connProps(final HashMap<String, String> options) {
-    final Properties props = new Properties();
-    for(final Entry<String, String> entry : options.entrySet()) {
-      props.setProperty(entry.getKey(), entry.getValue());
-    }
-    return props;
   }
 }
