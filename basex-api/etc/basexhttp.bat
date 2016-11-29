8663b929156e07b3d060a9605bd01a28fa8dabe8
Christian Gruen <christian.gruen@gmail.com>
2016-11-29 12:44:31 +0100

    [MOD] Windows start scripts. Closes #1328

--- basex-api/etc/basexhttp.bat
+++ basex-api/etc/basexhttp.bat
@@ -1,18 +1,12 @@
 @echo off
 setLocal EnableDelayedExpansion
 
-REM Path to this script
-set PWD=%~dp0
-
-REM Core and library classes
-set CP=%PWD%/../target/classes
-set LIB=%PWD%/../lib
-for /R "%LIB%" %%a in (*.jar) do set CP=!CP!;%%a
-set LIB=%PWD%/../../basex-core/lib
-for /R "%LIB%" %%a in (*.jar) do set CP=!CP!;%%a
+REM Path to core and library classes
+set MAIN=%~dp0/..
+set CP=%MAIN%/target/classes;%MAIN%/lib/*;%MAIN%/../basex-core/lib/*
 
 REM Options for virtual machine
-set BASEX_JVM=-Xmx512m %BASEX_JVM%
+set BASEX_JVM=-Xmx2g %BASEX_JVM%
 
 REM Run code
 java -cp "%CP%" %BASEX_JVM% org.basex.BaseXHTTP %*
--- basex-core/etc/basexclient.bat
+++ basex-core/etc/basexclient.bat
@@ -1,13 +1,9 @@
 @echo off
 setLocal EnableDelayedExpansion
 
-REM Path to this script
-set PWD=%~dp0
-
-REM Core and library classes
-set CP=%PWD%/../target/classes
-set LIB=%PWD%/../lib
-for /R "%LIB%" %%a in (*.jar) do set CP=!CP!;%%a
+REM Path to core and library classes
+set MAIN=%~dp0/..
+set CP=%MAIN%/target/classes;%MAIN%/lib/*
 
 REM Run code
 java -cp "%CP%" %BASEX_JVM% org.basex.BaseXClient %*
