6ff3e2548aa37d6682b72035b667a5f9a913d3a8
Christian Gruen <christian.gruen@gmail.com>
2017-01-24 12:43:50 +0100

    [MOD] XQuery, xml-to-json: Adopt indentation from module

--- basex-core/src/main/java/org/basex/query/func/fn/FnXmlToJson.java
+++ basex-core/src/main/java/org/basex/query/func/fn/FnXmlToJson.java
@@ -4,11 +4,13 @@ import static org.basex.query.QueryError.*;
 
 import org.basex.build.json.*;
 import org.basex.build.json.JsonOptions.*;
+import org.basex.io.serial.*;
 import org.basex.query.*;
 import org.basex.query.func.json.*;
 import org.basex.query.value.item.*;
 import org.basex.query.value.node.*;
 import org.basex.util.*;
+import org.basex.util.options.Options.*;
 
 /**
  * Function implementation.
@@ -22,6 +24,12 @@ public class FnXmlToJson extends FnParseJson {
     final ANode node = toEmptyNode(exprs[0], qc);
     final JsonSerialOptions jopts = toOptions(1, new JsonSerialOptions(), qc);
     jopts.set(JsonOptions.FORMAT, JsonFormat.BASIC);
+
+    // no indentation specified: adopt module indentation
+    final Boolean indent = jopts.get(JsonSerialOptions.INDENT);
+    if(indent == null) jopts.set(JsonSerialOptions.INDENT,
+        qc.serParams().get(SerializerOptions.INDENT) == YesNo.YES);
+
     return node == null ? null :
       Str.get(serialize(node.iter(), JsonSerialize.options(jopts), INVALIDOPT_X));
   }
