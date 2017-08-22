0d43004f664348dc7614897135359b5c78feef12
Christian Gruen <christian.gruen@gmail.com>
2017-08-22 12:21:43 +0200

    [DEL] java9: obsolete annotation

--- basex-core/src/main/java/org/basex/query/func/crypto/MyKeySelector.java
+++ basex-core/src/main/java/org/basex/query/func/crypto/MyKeySelector.java
@@ -47,7 +47,6 @@ final class MyKeySelector extends KeySelector {
     if(ki == null) throw new KeySelectorException("KeyInfo is null");
 
     final SignatureMethod sm = (SignatureMethod) m;
-    @SuppressWarnings("unchecked")
     final List<?> list = ki.getContent();
 
     for(final Object l : list) {
