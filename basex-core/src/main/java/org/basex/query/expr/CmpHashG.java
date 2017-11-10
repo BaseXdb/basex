090244dc945b9c02d902bfdb4139604d96f86303
Christian Gruen <christian.gruen@gmail.com>
2017-11-10 12:39:46 +0100

    [MOD] XQuery, CmpHashG: thread-safe caching

--- basex-core/src/main/java/org/basex/query/expr/CmpHashG.java
+++ basex-core/src/main/java/org/basex/query/expr/CmpHashG.java
@@ -16,10 +16,8 @@ import org.basex.util.hash.*;
  * @author Christian Gruen
  */
 public final class CmpHashG extends CmpG {
-  /** Item cache. */
-  private HashItemSet cache;
-  /** Iterator. */
-  private Iter iter2;
+  /** Thread-safe caching. */
+  private final ThreadLocal<CmpCache> caches = new ThreadLocal<>();
 
   /**
    * Constructor.
@@ -47,28 +45,31 @@ public final class CmpHashG extends CmpG {
     if(iter1.size() == 0) return Bln.FALSE;
 
     // first call: initialize hash
+    CmpCache cache = caches.get();
     if(cache == null) {
-      cache = new HashItemSet(true);
+      cache = new CmpCache();
+      caches.set(cache);
       final Iter ir2 = exprs[1].atomIter(qc, info);
       // no values: no need to cache
       if(ir2.size() == 0) return Bln.FALSE;
-      iter2 = ir2;
+      cache.iter2 = ir2;
     }
+    final HashItemSet set = cache.set;
 
     // loop through input
     for(Item it1; (it1 = iter1.next()) != null;) {
       qc.checkStop();
       // check if value has already been cached
-      if(cache.contains(it1, info)) return Bln.TRUE;
+      if(set.contains(it1, info)) return Bln.TRUE;
 
       // cache remaining values (stop after first hit)
-      if(iter2 != null) {
-        for(Item it2; (it2 = iter2.next()) != null;) {
+      if(cache.iter2 != null) {
+        for(Item it2; (it2 = cache.iter2.next()) != null;) {
           qc.checkStop();
-          cache.add(it2, ii);
-          if(cache.contains(it1, info)) return Bln.TRUE;
+          set.add(it2, ii);
+          if(set.contains(it1, info)) return Bln.TRUE;
         }
-        iter2 = null;
+        cache.iter2 = null;
       }
     }
     return Bln.FALSE;
@@ -82,5 +83,17 @@ public final class CmpHashG extends CmpG {
   @Override
   public String description() {
     return "hashed '" + op + "' operator";
+  }
+
+  /**
+   * Cached items.
+   * @author BaseX Team 2005-17, BSD License
+   * @author Christian Gruen
+   */
+  private static final class CmpCache {
+    /** cached items. */
+    final HashItemSet set = new HashItemSet(true);;
+    /** Lazy iterator. */
+    Iter iter2;
   }
 }
