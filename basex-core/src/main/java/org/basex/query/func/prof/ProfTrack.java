f3363ee6ab5ff06ca683dfadc4a701d262688b87
Christian Gruen <christian.gruen@gmail.com>
2017-11-23 19:13:00 +0100

    [MOD] XQuery, prof:track, 'value' option

--- basex-core/src/main/java/org/basex/query/func/prof/ProfTrack.java
+++ basex-core/src/main/java/org/basex/query/func/prof/ProfTrack.java
@@ -3,6 +3,7 @@ package org.basex.query.func.prof;
 import org.basex.query.*;
 import org.basex.query.expr.*;
 import org.basex.query.func.*;
+import org.basex.query.iter.*;
 import org.basex.query.value.*;
 import org.basex.query.value.item.*;
 import org.basex.query.value.map.*;
@@ -30,30 +31,45 @@ public final class ProfTrack extends StandardFunc {
   public Item item(final QueryContext qc, final InputInfo ii) throws QueryException {
     final TrackOptions opts = toOptions(1, new TrackOptions(), qc);
 
-    final boolean value = opts.get(TrackOptions.VALUE);
     Performance perf = null;
     long min = -1;
+    // include memory consumption
     if(opts.get(TrackOptions.MEMORY)) {
       Performance.gc(4);
       min = Performance.memory();
     }
+    // include execution time (called after garbage collection)
     if(opts.get(TrackOptions.TIME)) {
       perf = new Performance();
     }
+    // include resulting value
+    Value value = null;
+    if(opts.get(TrackOptions.VALUE)) {
+      // directly retrieve value
+      value = qc.value(exprs[0]);
+    } else {
+      // iterate through results; skip iteration if iterator is value-based
+      final Iter ir = exprs[0].iter(qc);
+      if(ir.value() == null) {
+        while(ir.next() != null);
+      }
+    }
 
-    final Value v = qc.value(exprs[0]);
     Map map = Map.EMPTY;
+    // resulting execution time (called before garbage collection)
     if(perf != null) {
       final long time = perf.ns();
       map = map.put(Str.get(TrackOptions.TIME.name()), Dbl.get(Performance.ms(time, 1)), info);
     }
+    // resulting memory consumption
     if(min != -1) {
       Performance.gc(2);
       final long mem = Math.max(0, Performance.memory() - min);
       map = map.put(Str.get(TrackOptions.MEMORY.name()), Int.get(mem), info);
     }
-    if(value) {
-      map = map.put(Str.get(TrackOptions.VALUE.name()), v, info);
+    // resulting value
+    if(value != null) {
+      map = map.put(Str.get(TrackOptions.VALUE.name()), value, info);
     }
     return map;
   }
