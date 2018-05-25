d94558f2ad81170713d5b3a7710db9f8e3498baa
BenEngbers <Ben.Engbers@Be-Logical.nl>
2018-05-25 11:26:14 +0200

    Add files via upload
    Updated the client, added binary support and removed errors.
    Added examples.

---
+++ basex-api/src/main/r/QueryBindExample.R
@@ -0,0 +1,25 @@
+# Basic example on how to use 'bind'.
+# Works with BaseX 8.0 and later
+#
+# Documentation: http://docs.basex.org/wiki/Clients
+#
+# (C) Ben Engbers
+
+# This example requires a running database server instance.
+# Documentation: http://docs.basex.org/wiki/Clients
+
+# @author BaseX Team 2005-18, BSD License
+
+source("RbaseXClient.R")
+
+Session <- BasexClient$new("localhost", 1984, "admin", "admin")
+
+query <- "declare variable $name external; for $i in 1 to 10 return element { $name } { $i }"
+query <- Session$query(query)$query
+query$bind("$name", "number", "")
+print(query)
+print(query$execute())
+print(query$info())
+
+query$close()
+
