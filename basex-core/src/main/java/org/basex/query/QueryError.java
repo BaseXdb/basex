50e704a32e5c705c80430681ef47069e38fe5b47
Christian Gruen <christian.gruen@gmail.com>
2018-05-29 09:15:26 +0200

    [MIN] XQuery, error message

--- basex-core/src/main/java/org/basex/query/QueryError.java
+++ basex-core/src/main/java/org/basex/query/QueryError.java
@@ -1299,7 +1299,7 @@ public enum QueryError {
   /** Error code. */
   UPMODIFY(XUST, 2, "Transformations must all be updating or return an empty sequence."),
   /** Error code. */
-  UPEXPECTF(XUST, 2, "Function body is no an updating expression."),
+  UPEXPECTF(XUST, 2, "Function body must be updating."),
   /** Error code. */
   DUPLREVAL(XUST, 3, "Duplicate 'revalidation' declaration."),
   /** Error code. */
