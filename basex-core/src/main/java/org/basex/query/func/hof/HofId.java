28d2f510669d3b075e4e5300301420cadb38ce94
Christian Gruen <christian.gruen@gmail.com>
2018-07-23 17:46:23 +0200

    [MIN] XQuery: hof:id. Obsolete implementation removed

--- basex-core/src/main/java/org/basex/query/func/hof/HofId.java
+++ basex-core/src/main/java/org/basex/query/func/hof/HofId.java
@@ -4,8 +4,6 @@ import org.basex.query.*;
 import org.basex.query.expr.*;
 import org.basex.query.func.*;
 import org.basex.query.iter.*;
-import org.basex.query.value.*;
-import org.basex.query.value.item.*;
 import org.basex.util.*;
 
 /**
@@ -17,17 +15,7 @@ import org.basex.util.*;
 public class HofId extends StandardFunc {
   @Override
   public final Iter iter(final QueryContext qc) throws QueryException {
-    return exprs[0].iter(qc);
-  }
-
-  @Override
-  public final Value value(final QueryContext qc) throws QueryException {
-    return exprs[0].value(qc);
-  }
-
-  @Override
-  public final Item item(final QueryContext qc, final InputInfo ii) throws QueryException {
-    return exprs[0].item(qc, info);
+    throw Util.notExpected();
   }
 
   @Override
