4b1600aa5fd33121619fb6784d05537d86d23f63
Christian Gruen <christian.gruen@gmail.com>
2018-10-30 15:28:49 +0100

    [MOD] Starts Scripts: change classpath order

--- basex-core/etc/basexclient.bat
+++ basex-core/etc/basexclient.bat
@@ -3,7 +3,7 @@ setLocal EnableDelayedExpansion
 
 REM Path to core and library classes
 set MAIN=%~dp0\..
-set CP=%MAIN%\target\classes;%MAIN%\lib\*;%MAIN%\lib\custom\*
+set CP=%MAIN%\target\classes;%MAIN%\lib\custom\*;%MAIN%\lib\*
 
 REM Run code
 java -cp "%CP%" %BASEX_JVM% org.basex.BaseXClient %*
--- basex-core/etc/basexgui.bat
+++ basex-core/etc/basexgui.bat
@@ -3,7 +3,7 @@ setLocal EnableDelayedExpansion
 
 REM Path to core and library classes
 set MAIN=%~dp0\..
-set CP=%MAIN%\target\classes;%MAIN%\lib\*;%MAIN%\lib\custom\*
+set CP=%MAIN%\target\classes;%MAIN%\lib\custom\*;%MAIN%\lib\*
 
 REM Options for virtual machine
 set BASEX_JVM=-Xmx1200m %BASEX_JVM%
