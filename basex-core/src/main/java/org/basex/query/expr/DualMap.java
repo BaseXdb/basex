d9b8bb24388659dfcaab870ebaefa14b1837d313
Christian Grün <cg@basex.org>
2020-10-06 11:56:12 +0200

    [MOD] XQuery, simple map: value-based implementations

--- basex-core/src/main/java/org/basex/query/expr/DualMap.java
+++ basex-core/src/main/java/org/basex/query/expr/DualMap.java
@@ -56,7 +56,20 @@ public final class DualMap extends SimpleMap {
 
   @Override
   public Value value(final QueryContext qc) throws QueryException {
-    return iter(qc).value(qc, this);
+    final QueryFocus qf = qc.focus;
+    final Value qv = qf.value;
+    try {
+      final ValueBuilder vb = new ValueBuilder(qc);
+      final Iter iter = exprs[0].iter(qc);
+      for(Item item; (item = qc.next(iter)) != null;) {
+        qf.value = item;
+        item = exprs[1].item(qc, info);
+        if(item != Empty.VALUE) vb.add(item);
+      }
+      return vb.value(this);
+    } finally {
+      qf.value = qv;
+    }
   }
 
   @Override
