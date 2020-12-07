58dc464d42d02130fd2d82da60a4cc83c2db2695
Christian Grün <cg@basex.org>
2020-12-07 17:42:49 +0100

    [MOD] XQuery, bin:join, singleton argumentsRArr

--- basex-core/src/main/java/org/basex/query/func/bin/BinJoin.java
+++ basex-core/src/main/java/org/basex/query/func/bin/BinJoin.java
@@ -1,9 +1,11 @@
 package org.basex.query.func.bin;
 
 import org.basex.query.*;
+import org.basex.query.expr.*;
 import org.basex.query.func.*;
 import org.basex.query.iter.*;
 import org.basex.query.value.item.*;
+import org.basex.query.value.seq.*;
 import org.basex.util.*;
 import org.basex.util.list.*;
 
@@ -16,9 +18,19 @@ import org.basex.util.list.*;
 public final class BinJoin extends StandardFunc {
   @Override
   public Item item(final QueryContext qc, final InputInfo ii) throws QueryException {
-    final ByteList bl = new ByteList();
-    final Iter iter = exprs[0].atomIter(qc, info);
-    for(Item item; (item = qc.next(iter)) != null;) bl.add(toB64(item, false).binary(info));
+    final Expr expr = exprs[0];
+
+    final ByteList bl;
+    if(expr instanceof SingletonSeq && ((SingletonSeq) expr).singleItem()) {
+      final byte[] bytes = toB64(((SingletonSeq) expr).itemAt(0), false).binary(info);
+      final long bs = expr.size();
+      bl = new ByteList(bs * bytes.length);
+      for(int b = 0; b < bs; b++) bl.add(bytes);
+    } else {
+      bl = new ByteList();
+      final Iter iter = expr.atomIter(qc, info);
+      for(Item item; (item = qc.next(iter)) != null;) bl.add(toB64(item, false).binary(info));
+    }
     return B64.get(bl.finish());
   }
 }
