dba88acaba69c028b792fcf8c199df2f52edb199
Christian Grün <cg@basex.org>
2021-03-25 16:49:39 +0100

    [MOD] XQuery: merge for/let clauses. #1995

--- basex-core/src/test/java/org/basex/query/expr/GFLWORTest.java
+++ basex-core/src/test/java/org/basex/query/expr/GFLWORTest.java
@@ -442,7 +442,7 @@ public final class GFLWORTest extends QueryPlanTest {
     check("for $a in (<a/> | <b/>) for $b in $a/* return <c/> ! <d>{ $b, . }</d>", "",
         count(For.class, 1));
 
-    check("for $e in (<a/>, <b/>) let $n := $e/name() order by $n return $n", "a\nb");
+    check("for $e in (<a/>, <b/>) let $n := $e/name() order by $n return $n", "a\nb", root(SORT));
     error("for $a in (1, 4, 2) let $i := (1, $a, 2) order by $i return $i + 1", SEQFOUND_X);
   }
 }
