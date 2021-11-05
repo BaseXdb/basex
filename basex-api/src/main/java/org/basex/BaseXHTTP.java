d48122467acbb35744fb69443b062c7d65e479b9
James Ball <github@jamesball.co.uk>
2021-11-05 12:28:35 +0000

    Update BaseXHTTP.java
    Override the default for which DespatchTypes will be compressed/uncompressed.
    Default is Response only. This adds Request.

--- basex-api/src/main/java/org/basex/BaseXHTTP.java
+++ basex-api/src/main/java/org/basex/BaseXHTTP.java
@@ -7,6 +7,8 @@ import java.io.*;
 import java.net.*;
 import java.util.function.*;
 
+import javax.servlet.DispatcherType;
+
 import org.basex.core.*;
 import org.basex.http.*;
 import org.basex.io.*;
@@ -83,6 +85,7 @@ public final class BaseXHTTP extends CLI {
     // enable GZIP support
     if(soptions.get(StaticOptions.GZIP)) {
       final GzipHandler gzip = new GzipHandler();
+      gzip.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.RESPONSE);
       gzip.addIncludedMethods(HttpMethod.POST.asString(), HttpMethod.PUT.asString());
       gzip.setHandler(wac);
       jetty.setHandler(gzip);
