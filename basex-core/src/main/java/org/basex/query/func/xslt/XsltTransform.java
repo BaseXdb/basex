c4ae287282a604be5701d31562a646a9f018d889
Christian Grün <cg@basex.org>
2021-11-24 14:09:36 +0100

    [ADD] xslt:transform-report

--- basex-core/src/main/java/org/basex/query/func/xslt/XsltTransform.java
+++ basex-core/src/main/java/org/basex/query/func/xslt/XsltTransform.java
@@ -4,11 +4,11 @@ import static org.basex.query.QueryError.*;
 import static org.basex.util.Token.*;
 
 import java.io.*;
-import java.util.*;
 
 import javax.xml.transform.*;
 import javax.xml.transform.stream.*;
 
+import org.basex.build.xml.*; // for CatalogWrapper
 import org.basex.io.*;
 import org.basex.io.out.*;
 import org.basex.io.serial.*;
@@ -17,7 +17,6 @@ import org.basex.query.value.item.*;
 import org.basex.query.value.node.*;
 import org.basex.util.*;
 import org.basex.util.options.*;
-import org.basex.build.xml.*; // for CatalogWrapper
 
 /**
  * Function implementation.
@@ -36,7 +35,8 @@ public class XsltTransform extends XsltFn {
   @Override
   public Item item(final QueryContext qc, final InputInfo ii) throws QueryException {
     try {
-      return new DBNode(new IOContent(transform(qc)));
+      final Str result = (Str) transform(qc, true);
+      return new DBNode(new IOContent(result.string()));
     } catch(final IOException ex) {
       throw IOERR_X.get(info, ex);
     }
@@ -45,30 +45,71 @@ public class XsltTransform extends XsltFn {
   /**
    * Performs an XSL transformation.
    * @param qc query context
-   * @return item
+   * @param simple simple processing (no report generation)
+   * @return item (map or string)
    * @throws QueryException query exception
    */
-  final byte[] transform(final QueryContext qc) throws QueryException {
+  final Item transform(final QueryContext qc, final boolean simple) throws QueryException {
     checkCreate(qc);
     final IO in = read(0, qc), xsl = read(1, qc);
     final Options opts = toOptions(2, new Options(), qc);
     final XsltOptions xopts = toOptions(3, new XsltOptions(), qc);
 
-    final PrintStream tmp = System.err;
-    final ArrayOutput ao = new ArrayOutput();
+    final ArrayOutput result = new ArrayOutput();
+    final PrintStream errPS = System.err;
+    final ArrayOutput err = new ArrayOutput();
+    final XsltReport xr = simple ? null : new XsltReport();
     try {
-      System.setErr(new PrintStream(ao));
-      return transform(in, xsl, opts.free(), xopts, qc);
-    } catch(final TransformerException ex) {
-      Util.debug(ex);
-      throw XSLT_ERROR_X.get(info, trim(utf8(ao.toArray(), Prop.ENCODING)));
+      // redirect errors
+      System.setErr(new PrintStream(err));
+      final URIResolver ur = CatalogWrapper.getURIResolver(qc.context.options);
+
+      // retrieve new or cached templates object
+      Templates templates = null;
+      final StreamSource ss = xsl.streamSource();
+      final String key = xopts.get(XsltOptions.CACHE) ? ss.getSystemId() : null;
+      if(key != null) templates = MAP.get(key);
+      if(templates == null) {
+        // no templates object cached: create new instance
+        final TransformerFactory tf = TransformerFactory.newInstance();
+        // assign catalog resolver (if defined)
+        if(ur != null) tf.setURIResolver(ur);
+        templates = tf.newTemplates(ss);
+        if(key != null) MAP.put(key, templates);
+      }
+
+      // create transformer, assign catalog resolver (if defined)
+      final Transformer tr = templates.newTransformer();
+      if(ur != null) tr.setURIResolver(ur);
+
+      // bind parameters
+      opts.free().forEach(tr::setParameter);
+
+      // do transformation and return result
+      if(simple) {
+        tr.transform(in.streamSource(), new StreamResult(result));
+        return Str.get(result.finish());
+      }
+
+      xr.register(tr);
+      tr.transform(in.streamSource(), new StreamResult(result));
+      xr.addMessage(qc);
     } catch(final IllegalArgumentException ex) {
       // Saxon raises runtime exceptions for illegal parameters
+      if(simple) throw XSLT_ERROR_X.get(info, ex);
+      xr.addError(Str.get(Util.message(ex)));
+    } catch(final TransformerException ex) {
+      // catch transformation errors, throw them again or add them to report
       Util.debug(ex);
-      throw XSLT_ERROR_X.get(info, ex.getMessage());
+      final byte[] error = trim(utf8(err.toArray(), Prop.ENCODING));
+      if(simple) throw XSLT_ERROR_X.get(info, error);
+      xr.addError(Str.get(error));
+      xr.addMessage(qc);
     } finally {
-      System.setErr(tmp);
+      System.setErr(errPS);
     }
+    xr.addResult(result.finish());
+    return xr.finish();
   }
 
   /**
@@ -91,47 +132,5 @@ public class XsltTransform extends XsltFn {
     }
     if(item.type.isStringOrUntyped()) return checkPath(toToken(item));
     throw STRNOD_X_X.get(info, item.type, item);
-  }
-
-  /**
-   * Performs an XSLT implementation.
-   * @param in input
-   * @param xsl style sheet
-   * @param params parameters
-   * @param xopts XSLT options
-   * @param qc query context
-   * @return transformed result
-   * @throws TransformerException transformer exception
-   */
-  private static byte[] transform(final IO in, final IO xsl, final HashMap<String, String> params,
-      final XsltOptions xopts, final QueryContext qc) throws TransformerException {
-
-    final URIResolver ur = CatalogWrapper.getURIResolver(qc.context.options);
-
-    // retrieve new or cached templates object
-    Templates tmp = null;
-    final StreamSource ss = xsl.streamSource();
-    final String key = xopts.get(XsltOptions.CACHE) ? ss.getSystemId() : null;
-    if(key != null) tmp = MAP.get(key);
-    if(tmp == null) {
-      // no templates object cached: create new instance
-      final TransformerFactory tf = TransformerFactory.newInstance();
-      // assign catalog resolver (if defined)
-      if(ur != null) tf.setURIResolver(ur);
-      tmp = tf.newTemplates(ss);
-      if(key != null) MAP.put(key, tmp);
-    }
-
-    // create transformer, assign catalog resolver (if defined)
-    final Transformer tr = tmp.newTransformer();
-    if(ur != null) tr.setURIResolver(ur);
-
-    // bind parameters
-    params.forEach(tr::setParameter);
-
-    // do transformation and return result
-    final ArrayOutput ao = new ArrayOutput();
-    tr.transform(in.streamSource(), new StreamResult(ao));
-    return ao.finish();
   }
 }
