39110b71d72c9c7daeb90cd40a10a3d66196fd4f
Christian Grün <cg@basex.org>
2022-03-08 17:00:15 +0100

    [MOD] XQuery: unifications

--- basex-core/src/main/java/org/basex/util/list/LongList.java
+++ basex-core/src/main/java/org/basex/util/list/LongList.java
@@ -123,11 +123,12 @@ public class LongList extends ElementList {
   }
 
   /**
-   * Removes duplicate entries.
+   * Sorts the data and removes distinct values.
    * @return self reference
    */
-  public LongList distinct() {
+  public LongList ddo() {
     if(!isEmpty()) {
+      sort();
       int i = 1;
       for(int j = 1; j < size; ++j) {
         while(j < size && list[i - 1] == list[j]) j++;
