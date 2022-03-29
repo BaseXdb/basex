3e481822dd14bcffa003c40c88fdab8cd7d27345
Christian Grün <cg@basex.org>
2022-03-29 11:50:40 +0200

    [FIX] XQuery, db:attribute-range: 3rd argument

--- basex-core/src/main/java/org/basex/query/func/db/DbAttribute.java
+++ basex-core/src/main/java/org/basex/query/func/db/DbAttribute.java
@@ -1,17 +1,10 @@
 package org.basex.query.func.db;
 
-import static org.basex.util.Token.*;
-
 import org.basex.data.*;
 import org.basex.index.*;
 import org.basex.query.*;
-import org.basex.query.expr.index.*;
-import org.basex.query.expr.path.*;
 import org.basex.query.iter.*;
 import org.basex.query.value.*;
-import org.basex.query.value.item.*;
-import org.basex.query.value.seq.*;
-import org.basex.query.value.type.*;
 
 /**
  * Function implementation.
@@ -23,29 +16,7 @@ public class DbAttribute extends DbText {
   @Override
   public final Iter iter(final QueryContext qc) throws QueryException {
     final Data data = checkData(qc);
-    final ValueAccess va = valueAccess(data, qc);
-
-    // no attribute specified: return iterator
-    if(exprs.length <= 2) return va.iter(qc);
-
-    // parse and compile the name test
-    final QNm qName = new QNm(toToken(exprs[2], qc), sc);
-    if(!qName.hasPrefix()) qName.uri(sc.ns.uri(EMPTY));
-
-    // return empty sequence if test will yield no results
-    final NameTest nt = new NameTest(qName, NamePart.FULL, NodeType.ATTRIBUTE, sc.elemNS);
-    if(nt.noMatches(data)) return Empty.ITER;
-
-    // wrap iterator with name test
-    final Iter iter = va.iter(qc);
-    return new Iter() {
-      @Override
-      public Item next() throws QueryException {
-        Item item;
-        while((item = iter.next()) != null && !nt.matches(item)) qc.checkStop();
-        return item;
-      }
-    };
+    return attribute(data, valueAccess(data, qc), qc, 2);
   }
 
   @Override
