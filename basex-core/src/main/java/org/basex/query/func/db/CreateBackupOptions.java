116f41ddd9012066b7f42fc73006dcaa07bf11f9
Christian Grün <cg@basex.org>
2022-05-13 11:52:35 +0200

    [MOD] XQuery: db:create-backup, compress vs. store. Closes #2103

---
+++ basex-core/src/main/java/org/basex/query/func/db/CreateBackupOptions.java
@@ -0,0 +1,16 @@
+package org.basex.query.func.db;
+
+import org.basex.util.options.*;
+
+/**
+ * Options for processing archives.
+ *
+ * @author BaseX Team 2005-22, BSD License
+ * @author Christian Gruen
+ */
+public final class CreateBackupOptions extends Options {
+  /** Recursive parsing. */
+  public static final StringOption COMMENT = new StringOption("comment");
+  /** Include root directory. */
+  public static final BooleanOption COMPRESS = new BooleanOption("compress", true);
+}
