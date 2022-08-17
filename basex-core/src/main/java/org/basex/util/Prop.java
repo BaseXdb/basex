d4ed7a4908a66906981fd297449aed40823a4502
Christian Grün <cg@basex.org>
2022-08-17 13:20:58 +0200

    BaseX 10.1 RC1

--- basex-core/src/main/java/org/basex/util/Prop.java
+++ basex-core/src/main/java/org/basex/util/Prop.java
@@ -23,7 +23,7 @@ public final class Prop {
   /** Project name. */
   public static final String NAME = "BaseX";
   /** Code version (may contain major, minor and optional patch number). */
-  public static final String VERSION = version("10.1 beta");
+  public static final String VERSION = version("10.1 RC1");
 
   /** Project name. */
   public static final String PROJECT = NAME.toLowerCase(Locale.ENGLISH);
