ec56dc7c0614106a45823f641db1cdf395d3244a
Christian Grün <cg@basex.org>
2022-10-08 11:31:32 +0200

    [FIX] XQuery: db:name

--- basex-core/src/main/java/org/basex/query/func/db/DbName.java
+++ basex-core/src/main/java/org/basex/query/func/db/DbName.java
@@ -1,8 +1,6 @@
 package org.basex.query.func.db;
 
-import org.basex.data.*;
 import org.basex.query.*;
-import org.basex.query.expr.*;
 import org.basex.query.value.item.*;
 import org.basex.util.*;
 
@@ -16,11 +14,5 @@ public final class DbName extends DbAccess {
   @Override
   public Item item(final QueryContext qc, final InputInfo ii) throws QueryException {
     return Str.get(toDBNode(toNode(exprs[0], qc), false).data().meta.name);
-  }
-
-  @Override
-  protected Expr opt(final CompileContext cc) {
-    final Data data = exprs[0].data();
-    return data != null ? Str.get(data.meta.name) : this;
   }
 }
