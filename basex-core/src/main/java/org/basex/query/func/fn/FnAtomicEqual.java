a678b5cb7a704e2b2800c3a47c3d632406fadc44
Christian Grün <cg@basex.org>
2023-02-09 12:40:37 +0100

    [FIX] Language files

---
+++ basex-core/src/main/java/org/basex/query/func/fn/FnAtomicEqual.java
@@ -0,0 +1,20 @@
+package org.basex.query.func.fn;
+
+import org.basex.query.*;
+import org.basex.query.func.*;
+import org.basex.query.value.item.*;
+import org.basex.util.*;
+
+/**
+ * Function implementation.
+ *
+ * @author BaseX Team 2005-23, BSD License
+ * @author Christian Gruen
+ */
+public final class FnAtomicEqual extends StandardFunc {
+  @Override
+  public Item item(final QueryContext qc, final InputInfo ii) throws QueryException {
+    final Item value1 = toItem(exprs[0], qc), value2 = toItem(exprs[1], qc);
+    return Bln.get(value1.sameKey(value2, info));
+  }
+}
