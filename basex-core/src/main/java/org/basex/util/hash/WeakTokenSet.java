72e4715a0e8dc6fd6a300870f3f66af54e8b4ea9
Gunther Rademacher <grd@gmx.net>
2023-04-24 08:01:39 +0200

    refactoring, less calls to ReferenceQueue.poll

--- basex-core/src/main/java/org/basex/util/hash/WeakTokenSet.java
+++ basex-core/src/main/java/org/basex/util/hash/WeakTokenSet.java
@@ -9,17 +9,17 @@ import org.basex.util.*;
 
 /**
  * A set for tokens that allows its elements to be garbage collected. This is achieved by holding
- * WeakRefefences to the byte arrays, making them eligible for garbage collection when there are
- * no strong references left from outside of this set. The set entry is then maintained from the
- * gc'ed reference showing up in a reference queue.
- * The first entry of the token set (offset 0) is always empty.
+ * WeakReferences to the tokens, making them eligible for garbage collection when there are no
+ * strong references left from outside of this set. The set entry is then maintained from the gc'ed
+ * reference showing up in a reference queue. The first entry of the token set (offset 0) is always
+ * empty.
  *
  * @author BaseX Team 2005-23, BSD License
  * @author Gunther Rademacher
  */
-public class WeakTokenSet extends ASet implements Iterable<byte[]> {
+public class WeakTokenSet extends ASet {
   /** Hashed keys. */
-  protected ElementRef[] keys;
+  protected WeakTokenRef[] keys;
   /** Garbage collected keys. */
   protected ReferenceQueue<byte[]> gcedKeys = new ReferenceQueue<>();
   /** Head of free id list. */
@@ -30,69 +30,74 @@ public class WeakTokenSet extends ASet implements Iterable<byte[]> {
    */
   public WeakTokenSet() {
     super(Array.INITIAL_CAPACITY);
-    keys = new ElementRef[capacity()];
+    keys = new WeakTokenRef[capacity()];
   }
 
   /**
-   * Stores the specified key and returns its id.
-   * @param key key to be added
-   * @return unique id of stored key (larger than zero)
+   * Stores the specified token, if not yet present in this set, and returns its stored equivalent.
+   * @param token token to be stored.
+   * @return token, or its equivalent that is stored in this set.
    */
-  public final byte[] put(final byte[] key) {
-    for(ElementRef gcedKey; (gcedKey = (ElementRef) gcedKeys.poll()) != null;) {
-      remove(gcedKey);
-    }
-    final int b = Token.hash(key) & capacity() - 1;
+  public final byte[] put(final byte[] token) {
+    final int b = Token.hash(token) & capacity() - 1;
     for(int id = buckets[b]; id != 0; id = next[id]) {
-      byte[] cachedKey = keys[id].get();
-      if(eq(key, cachedKey)) return cachedKey;
+      byte[] storedToken = keys[id].get();
+      if(eq(token, storedToken)) return storedToken;
     }
-    int s;
+    final int s;
     if(free != 0) {
       s = free;
       free = next[free];
     }
-    else {
-      checkSize((id, bucket) -> keys[id].bucket = bucket);
+    else if(size < capacity()) {
       s = size++;
     }
+    else {
+      cleanUp();
+      if(free != 0) {
+        s = free;
+        free = next[free];
+      }
+      else {
+        checkSize((id, bucket) -> keys[id].bucket = bucket);
+        s = size++;
+      }
+    }
     next[s] = buckets[b];
-    keys[s] = new ElementRef(key, b, gcedKeys);
+    keys[s] = new WeakTokenRef(token, b, gcedKeys);
     buckets[b] = s;
-    return key;
+    return token;
   }
 
   /**
-   * Removes the entry with the specified element reference.
-   * The deletion of keys will lead to empty entries. If {@link #size} is called after
-   * deletions, the original number of entries will be returned.
-   * @param ref the element reference to be removed.
+   * Removes garbage collected keys from the set. The deletion of keys will lead to empty entries.
+   * If {@link #size} is called after deletions, the original number of entries will be returned.
    */
-  protected void remove(final ElementRef ref) {
-    for (int i = 0; i < 2; ++i) {
-      final int b = ref.bucket;
-      for(int p = 0, id = buckets[b]; id != 0; p = id, id = next[id]) {
-        if(ref != keys[id]) continue;
+  public void cleanUp() {
+    for(WeakTokenRef key; (key = (WeakTokenRef) gcedKeys.poll()) != null;) {
+      final int b = key.bucket;
+      for(int p = 0, id = buckets[b];; p = id, id = next[id]) {
+        if(id == 0) throw Util.notExpected();
+        if(key != keys[id]) continue;
         if(p == 0) {
           buckets[b] = next[id];
         }
         else {
-          next[p] = next[next[p]];
+          next[p] = next[id];
         }
         keys[id] = null;
         next[id] = free;
         free = id;
-        return;
+        break;
       }
     }
-    throw Util.notExpected();
   }
 
   @Override
   protected int hash(final int id) {
-    byte[] key = keys[id].get();
-    return key == null ? keys[id].bucket : Token.hash(key);
-    }
+    byte[] token = keys[id].get();
+    return token == null ? keys[id].bucket : Token.hash(token);
+  }
 
   @Override
   protected void rehash(final int newSize) {
@@ -101,13 +106,10 @@ public class WeakTokenSet extends ASet implements Iterable<byte[]> {
 
   @Override
   public void clear() {
-    throw Util.notExpected();
-  }
-
-  @Override
-  public final Iterator<byte[]> iterator() {
-    throw Util.notExpected();
-//    return new ArrayIterator<>(keys, 1, size);
+    gcedKeys = new ReferenceQueue<>();
+    free = 0;
+    Arrays.fill(keys, null);
+    super.clear();
   }
 
   @Override
@@ -116,23 +118,29 @@ public class WeakTokenSet extends ASet implements Iterable<byte[]> {
   }
 
   /**
-   *  A reference to a token set element. It is represented as a WeakReference, but it also is
-   *  aware of the bucket where it is currently held in the set. This allows removal of the element
-   *  even after the referred byte array has been garbage collected (and thus its hash code cannot
-   *  be calculated any longer).
+   *  A weak reference to a token. It is aware of the bucket where it is currently held in the set.
+   *  This allows removal of the key even after the referred token has been garbage collected (and
+   *  thus its hash code cannot be calculated any longer).
    */
-  protected static class ElementRef extends WeakReference<byte[]> {
-    /** The current bucket where this reference. */
+  protected static class WeakTokenRef extends WeakReference<byte[]> {
+    /** The current bucket where this reference is in. */
     int bucket;
 
     /**
      * Constructor.
-     * @param key the key to be cached.
+     * @param token the key to be stored.
      * @param bucket the initial bucket of this entry.
-     * @param queue queue for registering this reference to.  */
-    ElementRef(final byte[] key, final int bucket, final ReferenceQueue<? super byte[]> queue) {
-      super(key, queue);
+     * @param queue queue for registering this reference to.
+     */
+    WeakTokenRef(final byte[] token, final int bucket, final ReferenceQueue<byte[]> queue) {
+      super(token, queue);
       this.bucket = bucket;
+    }
+
+    @Override
+    public String toString() {
+      final byte[] token = get();
+      return token == null ? "null" : string(token);
     }
   }
 }
