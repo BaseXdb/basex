83a115a1551308ff140f9b4f044916c77fcf4a10
Gunther Rademacher <grd@gmx.net>
2023-10-05 21:02:29 +0200

    correct mistake made in earlier merge commit

--- basex-core/src/main/java/org/basex/query/func/fn/FnInvisibleXml.java
+++ basex-core/src/main/java/org/basex/query/func/fn/FnInvisibleXml.java
@@ -32,46 +32,44 @@ public final class FnInvisibleXml extends StandardFunc {
   @Override
   public FuncItem item(final QueryContext qc, final InputInfo ii) throws QueryException {
     if(generator == null) {
-      for(final String className : Arrays.asList("de.bottlecaps.markup.Blitz")) {
+      for(final String className : Arrays.asList("de.bottlecaps.markup.Blitz",
+          "de.bottlecaps.markup.BlitzException", "de.bottlecaps.markup.BlitzParseException")) {
         if(!Reflect.available(className)) {
-          throw BASEX_CLASSPATH_X_X.get(ii, definition.local(), className);
+          throw BASEX_CLASSPATH_X_X.get(info, definition.local(), className);
         }
       }
       generator = new Generator();
     }
-    return generator.generate(qc, ii, sc, toString(arg(0), qc));
+    return generator.generate(qc, toString(arg(0), qc));
   }
 
   /**
    * Invisible XML parser generator.
    */
-  private static final class Generator {
+  private final class Generator {
     /**
      * Generate a parser from an invisible XML grammar.
      * @param qc query context
-     * @param ii input info
-     * @param sc static context
      * @param grammar the invisible XML grammar
      * @return the parsing function
      * @throws QueryException query exception
      */
-    public FuncItem generate(final QueryContext qc, final InputInfo ii, final StaticContext sc,
-        final String grammar) throws QueryException {
+    public FuncItem generate(final QueryContext qc, final String grammar) throws QueryException {
       final de.bottlecaps.markup.blitz.Parser parser;
       try {
         parser = Blitz.generate(grammar);
       }
       catch (BlitzParseException ex) {
-        throw IXML_GRM_X_X_X.get(ii, ex.getOffendingToken(), ex.getLine(), ex.getColumn());
+        throw IXML_GRM_X_X_X.get(info, ex.getOffendingToken(), ex.getLine(), ex.getColumn());
       }
       catch (BlitzException ex) {
-        throw IXML_GEN_X.get(ii, ex);
+        throw IXML_GEN_X.get(info, ex);
       }
-      final Var[] params = { new VarScope(sc).addNew(new QNm("input"), STRING_O, true, qc, ii) };
-      final Expr arg = new VarRef(ii, params[0]);
-      final ParseInvisibleXml parseFunction = new ParseInvisibleXml(ii, parser, arg);
+      final Var[] params = { new VarScope(sc).addNew(new QNm("input"), STRING_O, true, qc, info) };
+      final Expr arg = new VarRef(info, params[0]);
+      final ParseInvisibleXml parseFunction = new ParseInvisibleXml(info, parser, arg);
       final FuncType type = FuncType.get(parseFunction.seqType(), STRING_O);
-      return new FuncItem(sc, null, null, params, type, parseFunction, params.length, ii);
+      return new FuncItem(sc, null, null, params, type, parseFunction, params.length, info);
     }
   }
 
@@ -84,7 +82,7 @@ public final class FnInvisibleXml extends StandardFunc {
 
     /**
      * Constructor.
-     * @param info input info
+     * @param info input info (can be {@code null})
      * @param args function arguments
      * @param parser generated invisible XML parser
      */
@@ -105,7 +103,7 @@ public final class FnInvisibleXml extends StandardFunc {
         throw IXML_INP_X_X_X.get(ii, ex.getOffendingToken(), ex.getLine(), ex.getColumn());
       }
       catch (BlitzException | IOException ex) {
-        throw IXML_RESULT_X.get(ii, ex);
+        throw IXML_RESULT_X.get(info, ex);
       }
     }
 
