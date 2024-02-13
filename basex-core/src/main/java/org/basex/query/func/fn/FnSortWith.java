1dee783826d498824562a74394dcad7f992e66f4
Christian Grün <cg@basex.org>
2024-02-13 14:30:06 +0100

    [XQFO4] fn:sort-with: multiple comparators. qtspecs#655

--- basex-core/src/main/java/org/basex/query/func/fn/FnSortWith.java
+++ basex-core/src/main/java/org/basex/query/func/fn/FnSortWith.java
@@ -21,11 +21,19 @@ public final class FnSortWith extends StandardFunc {
   @Override
   public Value value(final QueryContext qc) throws QueryException {
     final Iter input = arg(0).iter(qc);
-    final FItem comparator = toFunction(arg(1), 2, qc);
-    final Comparator<Item> cmp = (a, b) -> {
+    final Value comparators = arg(1).value(qc);
+
+    final FItem[] cmps = new FItem[(int) comparators.size()];
+    int c = 0;
+    for(final Item item : comparators) cmps[c++] = toFunction(item, 2, qc);
+
+    final Comparator<Item> comparator = (a, b) -> {
       try {
-        final long diff = toLong(comparator.invoke(qc, info, a, b).item(qc, info));
-        return diff < 0 ? -1 : diff > 0 ? 1 : 0;
+        for(final FItem cmp : cmps) {
+          final long diff = toLong(cmp.invoke(qc, info, a, b).item(qc, info));
+          if(diff != 0) return diff < 0 ? -1 : 1;
+        }
+        return 0;
       } catch(final QueryException qe) {
         throw new QueryRTException(qe);
       }
@@ -34,7 +42,7 @@ public final class FnSortWith extends StandardFunc {
     final ItemList items = new ItemList(Seq.initialCapacity(input.size()));
     for(Item item; (item = qc.next(input)) != null;) items.add(item);
     try {
-      Arrays.sort(items.list, 0, items.size(), cmp);
+      Arrays.sort(items.list, 0, items.size(), comparator);
     } catch(final QueryRTException ex) {
       throw ex.getCause();
     }
