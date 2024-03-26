f068c23ea6fcfa85cfc4943e3aaadf766fcaa0c2
Christian Grün <cg@basex.org>
2024-03-26 09:14:20 +0100

    [MOD] ICU library: Unifications

--- basex-core/src/main/java/org/basex/query/func/fn/FnGraphemes.java
+++ basex-core/src/main/java/org/basex/query/func/fn/FnGraphemes.java
@@ -1,10 +1,13 @@
 package org.basex.query.func.fn;
 
+import java.text.*;
+
 import org.basex.query.*;
 import org.basex.query.func.*;
-import org.basex.query.util.*;
 import org.basex.query.value.*;
 import org.basex.query.value.seq.*;
+import org.basex.util.*;
+import org.basex.util.list.*;
 
 /**
  * Function implementation.
@@ -15,10 +18,18 @@ import org.basex.query.value.seq.*;
 public final class FnGraphemes extends StandardFunc {
   @Override
   public Value value(final QueryContext qc) throws QueryException {
-    if(!GraphemesOptions.AVAILABLE) throw QueryError.BASEX_ICU.get(info);
-
     final String value = toStringOrNull(arg(0), qc);
-    final GraphemesOptions options = toOptions(arg(1), new GraphemesOptions(), true, qc);
-    return value != null ? StrSeq.get(options.split(value)) : Empty.VALUE;
+
+    final TokenList list = new TokenList();
+    if(Prop.ICU) {
+      Icu.split(list, value);
+    } else {
+      final BreakIterator bi = BreakIterator.getCharacterInstance();
+      bi.setText(value);
+      for(int s = bi.first(), e = bi.next(); e != BreakIterator.DONE; s = e, e = bi.next()) {
+        list.add(value.substring(s, e));
+      }
+    }
+    return value != null ? StrSeq.get(list.finish()) : Empty.VALUE;
   }
 }
