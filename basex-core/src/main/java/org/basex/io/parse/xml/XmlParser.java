47104a8db633215994fd2bf84b0e0f7b8274d38a
Gunther Rademacher <grd@gmx.net>
2025-02-19 15:00:03 +0100

    add support for XSD validation

--- basex-core/src/main/java/org/basex/io/parse/xml/XmlParser.java
+++ basex-core/src/main/java/org/basex/io/parse/xml/XmlParser.java
@@ -2,8 +2,12 @@ package org.basex.io.parse.xml;
 
 import java.io.*;
 
+import javax.xml.*;
 import javax.xml.parsers.*;
+import javax.xml.validation.*;
 
+import org.basex.core.*;
+import org.basex.util.*;
 import org.xml.sax.*;
 import org.xml.sax.helpers.*;
 
@@ -23,7 +27,7 @@ public final class XmlParser {
    * @throws ParserConfigurationException parser configuration exception
    */
   public XmlParser() throws SAXException, ParserConfigurationException {
-    reader = reader(false, false, false);
+    reader = reader(new MainOptions());
   }
 
   /**
@@ -49,16 +53,18 @@ public final class XmlParser {
 
   /**
    * Returns an XML reader.
-   * @param dtd parse external DTDs
-   * @param dtdValidation DTD validation (implies DTD parsing)
-   * @param xinclude enable XInclude
+   * @param options XML parsing options
    * @throws SAXException SAX exception
    * @throws ParserConfigurationException parser configuration exception
    * @return reader
    */
-  public static XMLReader reader(final boolean dtd, final boolean dtdValidation,
-      final boolean xinclude) throws SAXException, ParserConfigurationException {
+  public static XMLReader reader(final MainOptions options)
+      throws SAXException, ParserConfigurationException {
 
+    final boolean dtd = options.get(MainOptions.DTD);
+    final boolean dtdValidation = options.get(MainOptions.DTDVALIDATION);
+    final boolean xinclude = options.get(MainOptions.XINCLUDE);
+    final boolean xsdValidation = MainOptions.STRICT.equals(options.get(MainOptions.XSDVALIDATION));
     final SAXParserFactory f = SAXParserFactory.newInstance();
     f.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", dtd);
     f.setFeature("http://xml.org/sax/features/external-general-entities", dtd);
@@ -68,6 +74,11 @@ public final class XmlParser {
     f.setNamespaceAware(true);
     f.setValidating(dtdValidation);
     f.setXIncludeAware(xinclude);
+    if(xsdValidation) {
+      SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+      schemaFactory.setResourceResolver(Resolver.resources(options));
+      f.setSchema(schemaFactory.newSchema());
+    }
     return f.newSAXParser().getXMLReader();
   }
 
