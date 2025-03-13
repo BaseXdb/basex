BaseXdb/basex

commit 2710fe097a66c012a0c6df15eb0702781cc0134a

[FIX] RESTXQ: Query vs. Form Parameters. Closes #2356

basex-api/src/main/java/org/basex/http/RequestContext.java
@@ -1,8 +1,6 @@
 package org.basex.http;
 
 import java.io.*;
-import java.net.*;
-import java.nio.charset.*;
 import java.util.*;
 
 import org.basex.core.*;
@@ -14,7 +12,6 @@ import org.basex.query.value.*;
 import org.basex.query.value.item.*;
 import org.basex.query.value.map.*;
 import org.basex.query.value.seq.*;
-import org.basex.query.value.type.*;
 import org.basex.util.*;
 import org.basex.util.http.*;
 import org.basex.util.list.*;
@@ -79,31 +76,28 @@ public final class RequestContext {
   /**
    * Returns the query parameters as strings.
    * @return map
-   * @throws IOException I/O exception
    */
-  public Map<String, String[]> queryStrings() throws IOException {
-    try {
-      if(strings == null) strings = request.getParameterMap();
-      return strings;
-    } catch(final RuntimeException ex) {
-      // may be caused by too large input (#884) or illegal query parameters
-      throw new IOException(ex);
+  public Map<String, String[]> queryStrings() {
+    if(strings == null) {
+      strings = new HashMap<>();
+      queryValues().forEach((key, value) -> {
+        final StringList list = new StringList(value.size());
+        for(final Item item : value) list.add(((Atm) item).toJava());
+        strings.put(key, list.finish());
+      });
     }
+    return strings;
   }
 
   /**
    * Returns query parameters as XQuery values.
-   * @return query parameters
-   * @throws IOException I/O exception
+   * @return map
    */
-  public Map<String, Value> queryValues() throws IOException {
+  public Map<String, Value> queryValues() {
     if(values == null) {
       values = new HashMap<>();
-      queryStrings().forEach((key, value) -> {
-        final ItemList items = new ItemList(value.length);
-        for(final String string : value) items.add(Atm.get(string));
-        values.put(key, items.value(AtomType.UNTYPED_ATOMIC));
-      });
+      final String string = request.getQueryString();
+      if(string != null) addParams(string, values);
     }
     return values;
   }
@@ -119,14 +113,16 @@ public final class RequestContext {
       throws QueryException, IOException {
 
     if(form == null) {
-      form = new HashMap<>();
       final MediaType mt = HTTPConnection.mediaType(request);
+      form = new HashMap<>();
       if(mt.is(MediaType.MULTIPART_FORM_DATA)) {
         // convert multipart parameters encoded in a form
-        addMultipart(mt, options, form);
+        try(InputStream is = body().inputStream()) {
+          form.putAll(new Payload(is, true, null, options).multiForm(mt));
+        }
       } else if(mt.is(MediaType.APPLICATION_X_WWW_FORM_URLENCODED)) {
         // convert URL-encoded parameters
-        addURLEncoded(form);
+        addParams(body().toString(), form);
       }
     }
     return form;
@@ -152,31 +148,15 @@ public final class RequestContext {
   // PRIVATE FUNCTIONS ============================================================================
 
   /**
-   * Adds multipart form-data from the passed on request body.
-   * @param type media type
-   * @param options main options
-   * @param map form parameters
-   * @throws QueryException query exception
-   * @throws IOException I/O exception
+   * Populates a map with URL-decoded parameters.
+   * @param params query parameters
+   * @param map to populate
    */
-  private void addMultipart(final MediaType type, final MainOptions options,
-      final Map<String, Value> map) throws QueryException, IOException {
-
-    try(InputStream is = body().inputStream()) {
-      map.putAll(new Payload(is, true, null, options).multiForm(type));
-    }
-  }
-
-  /**
-   * Adds URL-encoded parameters from the passed on request body.
-   * @param map form parameters
-   * @throws IOException I/O exception
-   */
-  private void addURLEncoded(final Map<String, Value> map) throws IOException {
-    for(final String param : Strings.split(body().toString(), '&')) {
+  private static void addParams(final String params, final Map<String, Value> map) {
+    for(final String param : Strings.split(params, '&')) {
       final String[] parts = Strings.split(param, '=', 2);
       if(parts.length == 2) {
-        final Atm atm = Atm.get(URLDecoder.decode(parts[1], StandardCharsets.UTF_8));
+        final Atm atm = Atm.get(XMLToken.decodeUri(parts[1]));
         map.merge(parts[0], atm, (value1, value2) -> {
           final ItemList items = new ItemList();
           for(final Item item : value1) items.add(item);

basex-api/src/main/java/org/basex/http/restxq/RestXqPathMatcher.java
@@ -183,14 +183,13 @@ final class RestXqPathMatcher {
       final InputInfo info) throws QueryException {
 
     if(literals.length() > 0) {
-      final byte[] path = XMLToken.decodeUri(Token.token(literals.toString()), false);
-      if(Token.contains(path, Token.REPLACEMENT))
-        throw RestXqFunction.error(info, INV_ENCODING_X, literals);
-      final TokenBuilder tb = new TokenBuilder(path.length);
-      for(final byte b : path) {
-        if(".^&!?-:<>()[]{}$=,*+|".indexOf(b) >= 0) tb.addByte((byte) '\\');
-        tb.addByte(b);
-      }
+      final String path = XMLToken.decodeUri(literals.toString());
+      if(path.contains("\uFFFD")) throw RestXqFunction.error(info, INV_ENCODING_X, literals);
+      final TokenBuilder tb = new TokenBuilder(path.length());
+      path.codePoints().forEach(cp -> {
+        if(".^&!?-:<>()[]{}$=,*+|".indexOf(cp) >= 0) tb.addByte((byte) '\\');
+        tb.add(cp);
+      });
       result.append(tb);
       literals.setLength(0);
     }

basex-api/src/main/java/org/basex/http/restxq/RestXqPerm.java
@@ -2,7 +2,6 @@ package org.basex.http.restxq;
 
 import static org.basex.http.web.WebText.*;
 
-import java.io.*;
 import java.util.*;
 
 import org.basex.http.*;
@@ -42,10 +41,9 @@ public final class RestXqPerm implements Comparable<RestXqPerm> {
    * @param conn HTTP connection
    * @return permission information
    * @throws QueryException query exception
-   * @throws IOException I/O exception
    */
   static XQMap map(final RestXqFunction func, final HTTPConnection conn)
-      throws QueryException, IOException {
+      throws QueryException {
     final MapBuilder parameters = new MapBuilder();
     for(final Map.Entry<String, Value> entry : conn.requestCtx.queryValues().entrySet()) {
       parameters.put(entry.getKey(), entry.getValue());

basex-api/src/main/java/org/basex/http/webdav/WebDAVUtils.java
@@ -1,8 +1,6 @@
 package org.basex.http.webdav;
 
 import java.io.*;
-import java.net.*;
-import java.nio.charset.StandardCharsets;
 
 import org.basex.io.*;
 import org.basex.io.in.*;
@@ -51,20 +49,12 @@ final class WebDAVUtils {
   }
 
   /**
-   * Decodes a URL. Character set must be guessed, because it cannot be derived from the request.
+   * Decodes a URL.
    * @param url url to be decoded
    * @return decoded url
    */
   static String decode(final String url) {
-    if(url.indexOf('%') != -1) {
-      try {
-        final String ud = URLDecoder.decode(url, StandardCharsets.UTF_8);
-        return ud.contains("\uFFFD") ? URLDecoder.decode(url, StandardCharsets.ISO_8859_1) : ud;
-      } catch(final Exception ex) {
-        Util.stack(ex);
-      }
-    }
-    return url;
+    return url.indexOf('%') != -1 ? XMLToken.decodeUri(url) : url;
   }
 
   /**

basex-api/src/test/java/org/basex/http/HTTPTest.java
@@ -75,64 +75,64 @@ public abstract class HTTPTest extends SandboxTest {
   /**
    * Executes the specified GET request and returns the result.
    * @param expected expected result
-   * @param request request
+   * @param path path of request
    * @param params query parameters (keys and values)
    * @throws IOException I/O exception
    */
-  protected static void get(final String expected, final String request, final Object... params)
+  protected static void get(final String expected, final String path, final Object... params)
       throws IOException {
-    assertEquals(expected, get(200, request, params));
+    assertEquals(expected, get(200, path, params));
   }
 
   /**
    * Executes the specified GET request and returns the result.
    * @param status status code to check
-   * @param request request
+   * @param path path of request
    * @param params query parameters (keys and values)
    * @return string result, or {@code null} for a failure
    * @throws IOException I/O exception
    */
-  protected static String get(final int status, final String request, final Object... params)
+  protected static String get(final int status, final String path, final Object... params)
       throws IOException {
-    return send(status, GET.name(), null, null, request, params).replaceAll("\\r", "");
+    return send(status, GET.name(), null, null, path, params).replaceAll("\\r", "");
   }
 
   /**
    * Executes the specified DELETE request.
    * @param status status code to check
-   * @param request request
+   * @param path path of request
    * @param params query parameters (keys and values)
    * @return response code
    * @throws IOException I/O exception
    */
-  protected static String delete(final int status, final String request, final Object... params)
+  protected static String delete(final int status, final String path, final Object... params)
       throws IOException {
-    return send(status, DELETE.name(), null, null, request, params);
+    return send(status, DELETE.name(), null, null, path, params);
   }
 
   /**
    * Executes the specified HEAD request and returns the result.
    * @param status status code to check
-   * @param request request
+   * @param path path of request
    * @param params query parameters (keys and values)
    * @return string result, or {@code null} for a failure
    * @throws IOException I/O exception
    */
-  protected static String head(final int status, final String request, final Object... params)
+  protected static String head(final int status, final String path, final Object... params)
       throws IOException {
-    return send(status, HEAD.name(), null, null, request, params);
+    return send(status, HEAD.name(), null, null, path, params);
   }
 
   /**
    * Executes the specified OPTIONS request and returns the result.
-   * @param request request
+   * @param path path of request
    * @param params query parameters (keys and values)
    * @return string result, or {@code null} for a failure
    * @throws IOException I/O exception
    */
-  protected static String options(final String request, final Object... params)
+  protected static String options(final String path, final Object... params)
       throws IOException {
-    return send(200, OPTIONS.name(), null, null, request, params);
+    return send(200, OPTIONS.name(), null, null, path, params);
   }
 
   /**
@@ -177,13 +177,13 @@ public abstract class HTTPTest extends SandboxTest {
    * Executes the specified PUT request.
    * @param status status code to check
    * @param is input stream (can be {@code null})
-   * @param request query
+   * @param path path of request
    * @param params query parameters (keys and values)
    * @throws IOException I/O exception
    */
-  protected static void put(final int status, final InputStream is, final String request,
+  protected static void put(final int status, final InputStream is, final String path,
       final Object... params) throws IOException {
-    send(status, Method.PUT.name(), is, null, request, params);
+    send(status, Method.PUT.name(), is, null, path, params);
   }
 
   /**
@@ -192,18 +192,18 @@ public abstract class HTTPTest extends SandboxTest {
    * @param method HTTP method
    * @param is input stream (can be {@code null})
    * @param type media type (optional, may be {@code null})
-   * @param request query
+   * @param path path of request
    * @param params query parameters (keys and values)
    * @return string result
    * @throws IOException I/O exception
    */
   protected static String send(final int status, final String method, final InputStream is,
-      final MediaType type, final String request, final Object... params) throws IOException {
+      final MediaType type, final String path, final Object... params) throws IOException {
 
     final BodyPublisher pub = is != null ? HttpRequest.BodyPublishers.ofInputStream(() -> is) :
       HttpRequest.BodyPublishers.noBody();
 
-    final StringBuilder sb = new StringBuilder(rootUrl + request);
+    final StringBuilder sb = new StringBuilder(rootUrl + path);
     final int pl = params.length;
     for(int p = 0; p < pl; p += 2) {
       sb.append(p == 0 ? '?' : '&').append(params[p]).append('=').
@@ -218,7 +218,7 @@ public abstract class HTTPTest extends SandboxTest {
       final HttpResponse<String> response = client.send(builder.build(),
           HttpResponse.BodyHandlers.ofString());
       final String body = response.body();
-      assertEquals(status, response.statusCode(), method + ' ' + request + "\nResponse: " + body);
+      assertEquals(status, response.statusCode(), method + ' ' + path + "\nResponse: " + body);
       return body;
     } catch(final InterruptedException ex) {
       throw new IOException(ex);

basex-api/src/test/java/org/basex/http/restxq/RestXqPathMatcherTest.java
@@ -131,14 +131,14 @@ public final class RestXqPathMatcherTest {
    * @param template template
    * @param path path
    * @param var variable
-   * @param val value
+   * @param value value
    * @throws Exception arbitrary exception
    */
   private static void testValues(final String template, final String path, final String var,
-      final String val) throws Exception {
+      final String value) throws Exception {
 
     final QNmMap<String> actual = RestXqPathMatcher.parse(template, null).values(path);
-    assertEquals(val, actual.get(new QNm(var)), "values differ");
+    assertEquals(value, actual.get(new QNm(var)), "values differ");
   }
 
   /**

basex-api/src/test/java/org/basex/http/restxq/RestXqPathTest.java
@@ -91,7 +91,7 @@ public final class RestXqPathTest extends RestXqTest {
     get("1", "declare %R:path('%7b') function m:f() {1};", "%7B");
     get("1", "declare %R:path('%7B') function m:f() {1};", "%7b");
     get("1", "declare %R:path('%7C') function m:f() {1};", "%7C");
-    get("1", "declare %R:path('+') function m:f() {1};", "+");
+    get("1", "declare %R:path('+') function m:f() {1};", "%20");
     get("1", "declare %R:path(' ') function m:f() {1};", "%20");
     get("1", "declare %R:path('%2b') function m:f() {1};", "+");
     get("1", "declare %R:path('%20') function m:f() {1};", "%20");

basex-api/src/test/java/org/basex/http/restxq/RestXqTest.java
@@ -38,41 +38,41 @@ public abstract class RestXqTest extends HTTPTest {
    * Executes the specified GET request and tests the result.
    * @param expected expected result
    * @param function function to test
-   * @param query request
+   * @param path path of request
    * @throws IOException I/O exception
    */
-  protected static void get(final String expected, final String function, final String query)
+  protected static void get(final String expected, final String function, final String path)
       throws IOException {
     register(function);
-    get(expected, query);
+    get(expected, path);
   }
 
   /**
    * Executes the specified GET request and tests for a status code.
    * @param status status code to check
    * @param function function to test
-   * @param query request
+   * @param path path of request
    * @throws IOException I/O exception
    */
-  protected static void get(final int status, final String function, final String query)
+  protected static void get(final int status, final String function, final String path)
       throws IOException {
     register(function);
-    get(status, query);
+    get(status, path);
   }
 
   /**
    * Executes the specified POST request and tests the result.
    * @param expected expected result
    * @param function function to test
-   * @param query request
+   * @param path path of request
    * @param payload payload
    * @param type media type
    * @throws IOException I/O exception
    */
-  protected static void post(final String expected, final String function, final String query,
+  protected static void post(final String expected, final String function, final String path,
       final String payload, final MediaType type) throws IOException {
     register(function);
-    assertEquals(expected, post(payload, type, query));
+    assertEquals(expected, post(payload, type, path));
   }
 
   /**

commit ff69fb47a4601789ea834e9193830f7e0b861e26

[FIX] RESTXQ: Forward query strings (caused by 2710fe097a66c012a0c6df15eb0702781cc0134a)

basex-api/src/main/java/org/basex/http/RequestContext.java
@@ -35,6 +35,8 @@ public final class RequestContext {
   private XQMap headers;
   /** Content body. */
   private IOContent body;
+  /** Query string. */
+  private final String query;
 
   /**
    * Returns an immutable map with all query parameters.
@@ -42,6 +44,7 @@ public final class RequestContext {
    */
   public RequestContext(final HttpServletRequest request) {
     this.request = request;
+    this.query = request.getQueryString();
   }
 
   /**
@@ -84,8 +87,13 @@ public final class RequestContext {
    */
   public XQMap queryValues() throws QueryException {
     if(values == null) {
+      // combine query parameters of previous and current request
+      final MapBuilder mb = new MapBuilder();
+      final RequestContext forward = (RequestContext) request.getAttribute(HTTPText.FORWARD);
+      if(forward != null && forward.query != null) addParams(forward.query, mb);
       final String string = request.getQueryString();
-      values = string != null ? toMap(string) : XQMap.empty();
+      if(string != null) addParams(string, mb);
+      values = mb.map();
     }
     return values;
   }
@@ -107,7 +115,9 @@ public final class RequestContext {
         }
       } else if(mt.is(MediaType.APPLICATION_X_WWW_FORM_URLENCODED)) {
         // convert URL-encoded parameters
-        form = toMap(body().toString());
+        final MapBuilder mb = new MapBuilder();
+        addParams(body().toString(), mb);
+        form = mb.map();
       } else {
         form = XQMap.empty();
       }
@@ -135,13 +145,12 @@ public final class RequestContext {
   // PRIVATE FUNCTIONS ============================================================================
 
   /**
-   * Returns a map with URL-decoded parameters.
-   * @param params query parameters
-   * @return map
+   * Adds URL-decoded parameters to the specified map.
+   * @param params parameters
+   * @param mb map builder
    * @throws QueryException query exception
    */
-  private static XQMap toMap(final String params) throws QueryException {
-    // collect values in item lists (faster)
+  private static void addParams(final String params, final MapBuilder mb) throws QueryException {
     final ItemObjMap<ItemList> map = new ItemObjMap<>();
     for(final String param : Strings.split(params, '&')) {
       final String[] parts = Strings.split(param, '=', 2);
@@ -151,8 +160,6 @@ public final class RequestContext {
       }
     }
     // create final map
-    final MapBuilder mb = new MapBuilder();
     for(final Item key : map) mb.put(key, map.get(key).value());
-    return mb.map();
   }
 }