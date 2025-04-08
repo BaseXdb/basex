de792dbdc81da35056ba983027171f0120282681
dominikik <dominikik@posteo.de>
2025-04-08 12:43:50 +0200

    [MOD] jetty: jetty-12, jetty-ee9-websocket

--- basex-api/src/main/java/org/basex/http/ws/WsResponse.java
+++ basex-api/src/main/java/org/basex/http/ws/WsResponse.java
@@ -16,7 +16,6 @@ import org.basex.query.expr.*;
 import org.basex.query.iter.*;
 import org.basex.query.value.item.*;
 import org.eclipse.jetty.ee9.websocket.api.*;
-import org.eclipse.jetty.websocket.api.*;
 /**
  * Creates WebSocket responses.
  *
