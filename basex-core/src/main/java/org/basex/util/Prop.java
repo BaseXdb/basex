023df18b0932fb8249754d0210c76b173406e0f9
Christian Grün <cg@basex.org>
2025-04-24 16:33:22 +0200

    BaseX 12.0 beta

--- basex-core/src/main/java/org/basex/util/Prop.java
+++ basex-core/src/main/java/org/basex/util/Prop.java
@@ -23,7 +23,7 @@ public final class Prop {
   /** Project name. */
   public static final String NAME = "BaseX";
   /** Current version (major and minor number, optionally followed by development tag). */
-  private static final String CURRENT_VERSION = "11.9 beta";
+  private static final String CURRENT_VERSION = "12.0 beta";
 
   /** Name of project in lower case. */
   public static final String PROJECT = NAME.toLowerCase(Locale.ENGLISH);
