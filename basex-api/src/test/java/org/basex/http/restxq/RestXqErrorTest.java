c68d08e7cdcd349770de019f6e811b7ab74e2b17
Christian Grün <cg@basex.org>
2025-06-23 12:48:53 +0200

    [FIX] JUnit tests

--- basex-api/src/test/java/org/basex/http/restxq/RestXqErrorTest.java
+++ basex-api/src/test/java/org/basex/http/restxq/RestXqErrorTest.java
@@ -118,10 +118,10 @@ public final class RestXqErrorTest extends RestXqTest {
    */
   @Test public void errorParam() throws Exception {
     // catch errors
-    get("err:FOER0000", "declare %R:path('') function m:a() { error() };" +
+    get("#err:FOER0000", "declare %R:path('') function m:a() { error() };" +
         "declare %R:error('*') %R:error-param('code', '{$x}') " +
         "function m:b($x) { $x };", "");
-    get("x", "declare %R:path('') function m:a() { error(xs:QName('x')) };" +
+    get("#x", "declare %R:path('') function m:a() { error(xs:QName('x')) };" +
         "declare %R:error('*') %R:error-param('code', '{$x}') " +
         "function m:b($x) { $x };", "");
     get("!!!", "declare %R:path('') function m:a() { error(xs:QName('x'), '!!!') };" +
