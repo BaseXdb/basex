2f24e2d36eb5a42f5a390129fec5876ffa3e9ce1
Christian Grün <cg@basex.org>
2026-03-13 11:46:49 +0100

    [FIX] Web functions: cast → coerce

--- basex-api/src/main/java/org/basex/http/web/WebFunction.java
+++ basex-api/src/main/java/org/basex/http/web/WebFunction.java
@@ -170,7 +170,7 @@ public abstract class WebFunction implements Comparable<WebFunction> {
       if(var.name.eq(name)) {
         // casts and binds the value
         final SeqType st = var.declaredType();
-        args[p] = value.seqType().instanceOf(st) ? value : st.cast(value, false, qc, null);
+        args[p] = value.seqType().instanceOf(st) ? value : st.coerce(value, null, qc, null, null);
         if(args[p] == null) throw error(ARG_TYPE_X_X_X, input, st, value);
         break;
       }
