84ad440ace0abfaaa637c0b504f3d90121b9bb15
Michael Seiferle <ms@basex.org>
2026-07-07 12:41:41 +0200

    Docker: log to standard output by default

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+# Route the log to standard output (docker logs). Prepended, so additional
+# options in $BASEX_JVM can still override it.
+export BASEX_JVM="-Dorg.basex.LOG=stdout $BASEX_JVM"
+
 # On first run, apply the admin password from $BASEX_ADMIN_PASSWORD (if provided).
 # If unset, BaseX generates a random initial password and writes it to the log.
 if [ -n "$BASEX_ADMIN_PASSWORD" ] && [ ! -e data/users.xml ]; then
