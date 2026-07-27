7ac33347a384d475bc02f05b8b79d4345834ff24
Christian Grün <cg@basex.org>
2026-07-28 00:10:13 +0200

    [MOD] GUI, Editor: Save a copy as…

--- basex-core/src/main/java/org/basex/core/Text.java
+++ basex-core/src/main/java/org/basex/core/Text.java
@@ -1015,6 +1015,8 @@ public interface Text {
   /** Command info. */
   String SAVE_AS = lang("save_as");
   /** Command info. */
+  String SAVE_COPY_AS = lang("save_copy_as");
+  /** Command info. */
   String PACKAGES = lang("packages");
   /** Command info. */
   String SHOW_HIDDEN_FILES = lang("show_hidden_files");
