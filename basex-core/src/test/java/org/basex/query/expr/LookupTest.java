4cd2430a1be0a2b5bff6021b5988b9f03883b18e
Christian Grün <cg@basex.org>
2026-08-02 15:33:12 +0200

    [MOD] XQuery: A/B?C reverted. qtspecs#2788

--- basex-core/src/test/java/org/basex/query/expr/LookupTest.java
+++ basex-core/src/test/java/org/basex/query/expr/LookupTest.java
@@ -73,18 +73,6 @@ public final class LookupTest extends SandboxTest {
     query("array:for-each([], function($i) { string($i) })!?([])", "");
   }
 
-  /** Lookup directly following an axis step (#2591). */
-  @Test public void step() {
-    query("{ 'a': 1, 'b': 2 } ! self::jnode()?b", 2);
-    query("{ 'a': 1, 'b': 2 } ! self::jnode()?*", "1\n2");
-    query("{ 'a': { 'b': 5 } } ! self::jnode()?a?b", 5);
-    // predicate before and after the lookup
-    query("{ 'a': 1 } ! self::jnode()[1]?a", 1);
-    query("{ 'a': (1, 2, 3) } ! self::jnode()?a[. > 1]", "2\n3");
-    // one lookup per context item
-    query("({ 'k': 7 }, { 'k': 8 }) ! self::jnode()?k", "7\n8");
-  }
-
   /** Rewrite lookups to map:get/array:get unless this could suppress a strict-record error. */
   @Test public void rewrite() {
     check("({ 'a': <x/> }, { 'b': <y/> })?a", "<x/>", exists(Lookup.class), empty(_MAP_GET));
